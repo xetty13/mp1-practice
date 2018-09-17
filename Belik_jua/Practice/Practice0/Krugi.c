343aa3f89846e14ee049466fa2e0ba0b01d3cbc9
Belikjulia <ybelik2015@yandex.ru>
2018-09-17 14:07:38 +0300

    PracticeKrug

--- Belik_jua/Practice/Practice0/Krugi.c
+++ Belik_jua/Practice/Practice0/Krugi.c
@@ -9,6 +9,11 @@ void main()
 	printf("x2 -? y2 -? r2 -?\n");
 	scanf("%lf %lf %lf", &x2, &y2, &r2);
 	d = sqrt((x1 - x2)*(x1 - x2) + (y1 - y2)*(y1 - y2));
+	if ((d == 0) && (r1 == r2))
+	{
+		printf("sovpadaut");
+		return;
+	}
 	if (d < fabs(r1 - r2))
 	{
 		printf("1 vnutri drugogo");
