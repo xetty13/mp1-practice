4c3540dc2a017bd8229da48fd5bcd54b627f9161
AlibekovMurad5202 <muradalibekov2000@gmail.com>
2018-09-17 14:27:23 +0300

    Practice1

--- alibekov_mr/Practice/Practice0/Main.c
+++ alibekov_mr/Practice/Practice0/Main.c
@@ -5,7 +5,8 @@ void main()
 	double r1, r2, x1, x2, y1, y2;
 	printf("Vvedite: r1, r2, x1, x2, y1, y2\n");
 	scanf_s("%lf %lf %lf %lf %lf %lf", &r1, &r2, &x1, &x2, &y1, &y2);
-	if ((x1 == x2) && (y1 == y2) && (r1 == r2)) {
+	if ((x1 == x2) && (y1 == y2) && (r1 == r2)) 
+	{
 		printf("Sovpadaut\n");
 		return;
 	}
@@ -14,13 +15,15 @@ void main()
 		printf("Ne imeut nichego obshego\n");
 		return;
 	}
-	if (((x1 - x2)*(x1 - x2) + (y1 - y2)*(y1 - y2)) == ((r1 + r2)*(r1 + r2)) || (((x1 - x2)*(x1 - x2) + (y1 - y2)*(y1 - y2)) == ((r1 - r2)*(r1 - r2)))) {
+	if (((x1 - x2)*(x1 - x2) + (y1 - y2)*(y1 - y2)) == ((r1 + r2)*(r1 + r2)) || (((x1 - x2)*(x1 - x2) + (y1 - y2)*(y1 - y2)) == ((r1 - r2)*(r1 - r2)))) 
+	{
 		printf("Kasautsya\n");
 		return;
 	}
 	//if (((x1 - x2)*(x1 - x2) + (y1 - y2)*(y1 - y2)) < ((r1 + r2)*(r1 + r2))) {
 
-		if (((x1 - x2)*(x1 - x2) + (y1 - y2)*(y1 - y2)) < ((r1 - r2)*(r1 - r2))) {
+		if (((x1 - x2)*(x1 - x2) + (y1 - y2)*(y1 - y2)) < ((r1 - r2)*(r1 - r2))) 
+		{
 			printf("Odna vnutry drugoy\n");
 		}
 		else printf("Peresekautsya\n");
