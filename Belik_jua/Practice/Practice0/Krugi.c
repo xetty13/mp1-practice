ff86ccc3abb41aa31222b9afd1a02a2733e833e3
Belikjulia <ybelik2015@yandex.ru>
2018-09-17 13:57:53 +0300

    PracticeKrug

---
+++ Belik_jua/Practice/Practice0/Krugi.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <math.h>
+
+void main()
+{
+	double x1, x2, r1, y2, y1, r2,d;
+	printf("x1 -? y1 -? r1 -?\n");
+	scanf("%lf %lf %lf", &x1, &y1, &r1);
+	printf("x2 -? y2 -? r2 -?\n");
+	scanf("%lf %lf %lf", &x2, &y2, &r2);
+	d = sqrt((x1 - x2)*(x1 - x2) + (y1 - y2)*(y1 - y2));
+	if (d < fabs(r1 - r2))
+	{
+		printf("1 vnutri drugogo");
+	}
+	if (d == fabs(r1 - r2))
+	{
+		printf("vnutrennee kasanie");
+	}
+	if ((d > fabs(r1 - r2)) && (d<(r1+r2)))
+	{
+		printf("peresekautsya");
+	}
+	if (d == (r1 + r2))
+	{
+		printf("vneshnee kasanie");
+	}
+	if (d > (r1 + r2))
+	{
+		printf("ne peresekautsya");
+	}
+}
