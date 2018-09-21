a0e43faeb364bb02337e6199dfea6a377729c644
Rustamov-A.Z <azer.rustamov.01@gmail.com>
2018-09-21 19:05:23 +0300

    All Done

--- rustamov.az/Practice/Practice0/main.c
+++ rustamov.az/Practice/Practice0/main.c
@@ -1,23 +1,50 @@
 #include <stdio.h>
 #include <math.h>
 void main() {
-	double a1, b1, a2, b2, r1, r2,x;
-	scanf("%lf %lf %lf %lf %lf %lf", a1, b1, r1, a2, b2, r2);
-	if ((a1 == a2) && (b1 == b2) && (r1 == r2)) {
-		printf("Okruzhnosti sovpadayut");
+	double x1, y1, x2, y2, r1, r2, d;
+	printf("VVedite coordinaty i radius pervoy okruzhnosti \n");
+	scanf_s("%lf %lf %lf", &x1, &y1, &r1);
+	if (r1<0)
+	{
+		printf("Radius okruzhnosty - otritsatel'noe chislo \n");
 		return;
 	}
-	x = sqrt((a1 - a2)*(a1 - a2) + (b1 - b2)*(b1 - b2));
-	if ((x - r2 < r1) || (x - r1 < r2)) {
-		printf("Odna Okruzhnost' vhodit v druguyu");
+	printf("VVedite coordinaty i radius vtoroy okruzhnosti \n");
+	scanf_s("%lf %lf %lf", &x2, &y2, &r2);
+	if (r1<0)
+	{
+		printf("Radius okruzhnosty - otritsatel'noe chislo \n");
 		return;
 	}
-	else if ((x - r2 == r1) || (x - r1 == r2)) {
-		printf("Odna okruzhnost' vnutrnne kasaets'a drugoy");
+	d = sqrt((x1 - x2)*(x1 - x2) + (y1 - y2)*(y1 - y2));
+	if (d >(r2 + r1))
+	{
+		printf("Okruzhnosty ne peresekayuts'a \n");
 		return;
 	}
-	if ()
-
-
+	if ((x1 == x2) && (y1 == y2) && (r1 == r2)) 
+	{
+		printf("Okruzhnosti sovpadayut \n");
+		return;
+	}
+	if (d == (r2 + r1))
+	{
+		printf("Okruzhnosty vneshne kasayuts'a \n");
+		return;
+	}
+	if ((d + r2 < r1) || (d + r1 < r2))
+	{
+		printf("Odna Okruzhnost' vhodit v druguyu \n");
+		return;
+	}
+	if ((d + r2 == r1) || (d + r1 == r2))
+	{
+		printf("Odna okruzhnost' vnutrnne kasaets'a drugoy \n");
+		return;
+	}
+	if ((d - r1 < r2) || (d - r2 < r1))
+	{
+		printf("Okruzhnosty peresekayuts'a \n");
+		return;
+	}
 }
-
