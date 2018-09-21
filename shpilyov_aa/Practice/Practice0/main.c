8bf068f4d01ba01b08d13687a60d0a325fe17dde
andrey-shpilyov <shpolend@yandex.ru>
2018-09-21 03:26:30 +0300

    HomeWork (Circles)

--- shpilyov_aa/Practice/Practice0/main.c
+++ shpilyov_aa/Practice/Practice0/main.c
@@ -1,22 +1,55 @@
 #include<stdio.h>
 #include<math.h>
+#include<locale.h>
 
 void main()
 {
-	float x1,x2,y1,y2,r1,r2,l;
+	setlocale(LC_ALL, "Rus");
 
-	scanf("%f%f%f%f%f%f",&x1,&x2,&y1,&y2,&r1,&r2);
+	float x1, x2, y1, y2, r1, r2, l;
 
-	l = sqrt((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2));
+	scanf("%f%f%f%f%f%f", &x1, &y1, &x2, &y2, &r1, &r2);
 
-	if(l>r1+r2)
-		printf("Circles haven't common points\n");
-	else if (l==r1+r2)
-		printf("1 points(kasaytsya)\n");
-	else if (l<r1+r2)
-		printf("2 points (peresekaytsya)\n");
-	else if (x1==x2 && y1==y2 && r1==r2)
-		printf("Circles sovpadayt");
-	else if (x1==x2 && y1==y2 && r1>r2 || r1 < r2)
-		printf("Circles kolco");
+	l = sqrt((x1 - x2)*(x1 - x2) + (y1 - y2)*(y1 - y2));
+
+    if (r1 <= 0 || r2 <= 0)
+	{
+		printf("Радиус не может быть <=0");
+		return;
+	}
+	if ((l == 0) && (r1 != r2)) 
+	{
+		printf("Концентрические окружности");
+		return;
+	}
+	if (l > r1 + r2)
+	{
+		printf("Одна окружность находится вне другой, не имеют общих точек");
+		return;
+	}
+	if (l == r1 + r2)
+	{
+		printf("Окружности касаются внешне, имеют 1 общую точку (точка касания)");
+		return;
+	}
+	if (l > r1 + r2)
+	{
+		printf("Одна окружность находится вне другой, не имеют общих точек");
+		return;
+	}
+	if ((r1 - r2 < l) && (r1 + r2 > l) || (r2 - r1 < l) && (r1 + r2 > l))
+	{
+		printf("Окружности пересекаются, имеют 2 общие точки");
+		return;
+	}
+	if ((l == r1 - r2) || (l == r2 - r1))
+	{
+		printf("Окружности имеют внтуреннее касание");
+		return;
+	}
+	if ((l < r1 - r2) || (l < r2 - r1))
+	{
+		printf("Одна окружность находится внтури другой");
+		return;
+	}
 }
