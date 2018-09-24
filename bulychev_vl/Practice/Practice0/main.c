cfdc7125bee537ccdf565a7de2f3cb743836ee41
Vladislavbul <vlad-hk.19.03@mail.ru>
2018-09-24 18:31:55 +0300

    Practice circles

---
+++ bulychev_vl/Practice/Practice0/main.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <math.h>
+
+ void main()
+ {
+	float x1, y1, r1, x2, y2, r2, d; // d-rasstoyanie mezhdu ñentrami okruzhnostej
+ 	printf("Vvedite koordinaty pervoj okruzhnosti i ee radius: ");
+	scanf("%f %f %f", &x1, &y1, &r1);
+	printf("Vvedite koordinaty vtoroj okruzhnosti i ee radius: ");
+	scanf("%f %f %f", &x2, &y2, &r2);
+	d = sqrtf(((x1 - x2)*(x1 - x2)) + ((y1 - y2)*(y1 - y2)));
+
+	if ((x1 == x2) && (y1 == y2) && (r1 == r2))
+	{
+		printf("Okruzhnosti polnost'yu ravny \n");
+		return;
+	}
+
+	if ((x1 == x2) && (y1 == y2) && (r1 < r2))
+	{
+		printf("Centry sovpadayut, pervaya okruzhnost' lezhit vo vtoroj \n");
+		return;
+	}
+
+	if ((x1 == x2) && (y1 == y2) && (r1 > r2))
+	{
+		printf("Centry sovpadayut, vtoraya okruzhnost' lezhit v pervoj \n");
+		return;
+	}
+
+	if (d >(r1 + r2))
+	{
+		printf("Okruzhnosti ne peresekayutsya i ne kasayutsya \n");
+		return;
+	}
+
+	if (d == (r1 + r2))
+	{
+		printf("Peresekayutsa v odnoy tochke \n");
+		return;
+	}
+
+	if ((d < (r1 + r2)) && (r1 < (d + r2)) && (r2 < (d + r1)))
+	{
+		printf("Okruzhnosti peresekayutsya i imeyut dve obshie tochki \n");
+		return;
+	}
+
+	if ((d + r2) == r1)
+	{
+		printf("Peresekaetsya v odnoy tochke, vtoroy vnutri pervogo \n");
+		return;
+	}
+
+	if ((d + r1) == r2)
+	{
+		printf("Peresekaetsya v odnoy tochke, perviy vnutri vtorogo \n");
+		return;
+	}
+ }
