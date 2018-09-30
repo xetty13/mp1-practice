4d1038429618711b277a5f9c0aa4bfdd82638337
AlibekovMurad5202 <muradalibekov2000@gmail.com>
2018-09-30 22:00:26 +0300

    Точно Final version

--- alibekov_mr/Practice/Practice0/Main.c
+++ alibekov_mr/Practice/Practice0/Main.c
@@ -2,11 +2,11 @@
 #include<math.h>
 void main()
 {
-	double r1, r2, x1, x2, y1, y2;
+	double r1, r2, x1, x2, y1, y2, r_megdu_centrami;
 	printf("Vvedite: r1 (r1 > 0), r2 (r2 > 0), x1, x2, y1, y2\n");
 	scanf_s("%lf %lf %lf %lf %lf %lf", &r1, &r2, &x1, &x2, &y1, &y2);
-	
-	if ((r1<=0)||(r2<=0))
+	r_megdu_centrami = sqrt(((x1 - x2)*(x1 - x2) + (y1 - y2)*(y1 - y2)));
+	if ((r1 <= 0) || (r2 <= 0))
 	{
 		printf("Ne sushestvuet(-ut)\n");
 		return;
@@ -18,25 +18,25 @@ void main()
 		return;
 	}
 
-	if (((x1 - x2)*(x1 - x2) + (y1 - y2)*(y1 - y2)) > ((r1 + r2)*(r1 + r2)))
+	if (r_megdu_centrami > (r1 + r2))
 	{
 		printf("Ne imeut nichego obshego (0 obshih tochek)\n");
 		return;
 	}
 
-	if (((x1 - x2)*(x1 - x2) + (y1 - y2)*(y1 - y2)) == ((r1 - r2)*(r1 - r2))) 
+	if (r_megdu_centrami == (r1 - r2))
 	{
 		printf("Kasautsya i odna vnutry drugoy (1 obshaya tochka)\n");
 		return;
 	}
 
-	if (((x1 - x2)*(x1 - x2) + (y1 - y2)*(y1 - y2)) == ((r1 + r2)*(r1 + r2)))
+	if (r_megdu_centrami == (r1 + r2))
 	{
 		printf("Kasautsya i odna ne vnutry drugoy (1 obshaya tochka)\n");
 		return;
 	}
 
-	if (((x1 - x2)*(x1 - x2) + (y1 - y2)*(y1 - y2)) < ((r1 - r2)*(r1 - r2))) 
+	if (r_megdu_centrami < (r1 - r2))
 	{
 		printf("Odna vnutry drugoy i ne kasautsya (0 obshih tochek)\n");
 	}
