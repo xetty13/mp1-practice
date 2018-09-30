8fbd82e8d1efb52a3992a699c77d33c1393c78a7
dasha-ludina <zxsa098721@gmail.com>
2018-09-30 22:50:11 +0300

    Circles

--- ludina-da/Practice/Practice0/main.c
+++ ludina-da/Practice/Practice0/main.c
@@ -3,31 +3,33 @@
 
 void main()
 {
-	float x1,x2,y1,y2,r1,r2,d;
-	scanf("%f%f%f%f%f%f",&x1,&x2,&y1,&y2,&r1,&r2);
-	d = sqrt((x1 - x2)*(x1 - x2) + (y1 - y2)*(y1 - y2));
+	float x1, x2, y1, y2, r1, r2, d;
+	scanf("%f %f %f %f %f %f", &x1, &x2, &y1, &y2, &r1, &r2);
+	d = sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
 	if (d == r1 + r2)
 	{
 		printf("Circles have one common point");
+		return;
 	}
 	if (d > r1 + r2)
 	{
 		printf("Circles haven't common points");
+		return;
 	}
-	if (d < r1 + r2)
-	{
-		printf("Circles have two common points");
-	}
-	if (x1==x2 && y1==y2 && r1==r2)
+	if (x1 == x2 && y1 == y2 && r1 == r2)
 	{
 		printf("Circles coincide");
+		return;
 	}
-	if (x1==x2 && y1==y2 && r1 > r2 || r1<r2)
+	if ((d < r1 - r2) || (d < r2 - r1))
 	{
 		printf("One circle is inside the other");
+		return;
 	}
-	if (d == r1 - r2)
+	if ((d == r1 - r2) || (d == r2 - r1))
 	{
 		printf("Circles touch inside");
+		return;
 	}
+		printf("Circles have two common points");
 }
