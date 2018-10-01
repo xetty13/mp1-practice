769f8243fa8b4b79f1e1b368576514f09f86cfa2
BelikJulia <ybelik2015@yandex.ru>
2018-10-01 18:17:43 +0300

    PracticeIKEA

--- belik_jua/Practice/Practice1/IKEA.c
+++ belik_jua/Practice/Practice1/IKEA.c
@@ -2,16 +2,16 @@
 
 void main()
 {
-	double h = 0, w = 0, d = 0, m, dvp, dsp, der;
+	double h = 0, w = 0, d = 0, m = 0, dvp, dsp, der;
 	int p = 0;
 
-	printf("180 < h < 200, h =/n");
+	printf("180 < h < 200, h = \n");
 	while ((h > 200) || (h < 180))
 		scanf("%lf", &h);
-	printf("80 < w < 120, w =/n");
+	printf("80 < w < 120, w = \n");
 	while ((w > 120) || (w < 80))
 		scanf("%lf", &w);
-	printf("50 < d < 90, d =/n");
+	printf("50 < d < 90, d = \n");
 	while ((d > 90) || (d < 50))
 		scanf("%lf", &d);
 
@@ -19,11 +19,13 @@ void main()
 	dsp = 1.5 * 0.00065;
 	der = 0.0008;
 	m = (dvp * h * w) + 2 * (dsp * h * d) + 2 * (dsp * w * d) + (der * h * w);
+
 	while (h > 79.0)
 	{
 		h = h - 40.0;
 		p++;
 	}
+
 	m = m + dsp * w * d * p;
-	print("massa = %lf", m);
+	printf("massa = %lf kg", m);
 }
