5f8024a21c79436b28aa2d56b7a3c035e0edbb3a
BelikJulia <ybelik2015@yandex.ru>
2018-10-01 17:45:17 +0300

    PracticeIKEA

--- belik_jua/Practice/Practice1/IKEA.c
+++ belik_jua/Practice/Practice1/IKEA.c
@@ -3,15 +3,15 @@
 void main()
 {
 	double h = 0, w = 0, d = 0, m, dvp, dsp, der;
-	int p;
+	int p = 0;
 
-	printf("h=");
+	printf("180 < h < 200, h =/n");
 	while ((h > 200) || (h < 180))
 		scanf("%lf", &h);
-	printf("w=");
+	printf("80 < w < 120, w =/n");
 	while ((w > 120) || (w < 80))
 		scanf("%lf", &w);
-	printf("d=");
+	printf("50 < d < 90, d =/n");
 	while ((d > 90) || (d < 50))
 		scanf("%lf", &d);
 
@@ -19,8 +19,7 @@ void main()
 	dsp = 1.5 * 0.00065;
 	der = 0.0008;
 	m = (dvp * h * w) + 2 * (dsp * h * d) + 2 * (dsp * w * d) + (der * h * w);
-	// расчет количества полок
-	while (h > 40.0)
+	while (h > 79.0)
 	{
 		h = h - 40.0;
 		p++;
