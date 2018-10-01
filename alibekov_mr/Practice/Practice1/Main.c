60c1b589d3e8ee62d3a62425dbce1b3650871132
AlibekovMurad5202 <muradAlibekov2000@gmail.com>
2018-10-01 14:28:12 +0300

    Practice1 version#2

--- alibekov_mr/Practice/Practice1/Main.c
+++ alibekov_mr/Practice/Practice1/Main.c
@@ -2,10 +2,12 @@
 void main() {
 	double pl_DVP, pl_DSP, pl_d, m, m_stenka, m_2bokovini, m_2krish, m_2dveri, m_polki;
 	int w, h, d;
-	printf("Vvedite: w (ot 80 do 120), h (ot 180 do 220), d (ot 50 do 90), pl_DVP, pl_DSP, pl_d");
-	scanf_s("%d %d %d %lf %lf %lf", &w, &h, &d, &pl_DVP, &pl_DSP, &pl_d);
+	printf("Vvedite (v sm): w (ot 80 do 120), h (ot 180 do 220), d (ot 50 do 90)\n");
+	scanf_s("%d %d %d", &w, &h, &d);
+	printf("Vvedite (v kg/(sm^3)): pl_DVP, pl_DSP, pl_d\n");
+	scanf_s("%lf %lf %lf", &pl_DVP, &pl_DSP, &pl_d);
 	if ((w < 80) || (w > 120) || (h < 180) || (h > 220) || (d < 50) || (d > 90) || (pl_DVP <= 0) || (pl_DSP <= 0) || (pl_d <= 0)) {
-		printf("Vvedeny nepravilnie dannnie");
+		printf("Vvedeni nepravilnie dannnie\n");
 		return;
 	}
 	m_stenka = h * w * 0.5 * pl_DVP;
@@ -14,5 +16,5 @@ void main() {
 	m_2dveri = h * w * 1 * pl_d;
 	m_polki = (h / 40) * pl_DSP * w * d * 1.5;
 	m = m_stenka + m_2bokovini + m_2krish + m_polki;
-	printf("%lf", m);
+	printf("%lf\n", m);
 }
