4a562cbaaf1d27c7a54d7b287f3cf13c235375ff
KljuevD <aloky.2000@gmail.com>
2018-10-04 15:53:13 +0300

    Updates

--- Practice/Practice1/main.c
+++ Practice/Practice1/main.c
@@ -1,28 +1,31 @@
 ﻿#include <stdio.h>
 #include <locale.h>
+#define DVP 300
+#define DSP 700
+#define WOOD 600
 
 void main()
 {
-	double w, h, d, dsp, dvp, wood;
-	double rp, side, lid, door, shelf, tm;
+	float w, h, d;
+	float rp, side, lid, door, shelf, tm;
 	setlocale(LC_ALL, "Russian");
-	printf("Высота (180 – 220 см), ширина (80 – 120 см), глубина (50 – 90 см),\nПлотность ДСП, ДВП, дерева в кг/м^3\n\n");
-	scanf("%lf %lf %lf %lf %lf %lf", &h, &w, &d, &dsp, &dvp, &wood);
-	if ((h < 180) || (h > 220) || (w < 80) || (w > 120) || (d < 50) || (d > 90) || (dsp < 0) || (dvp < 0) || (wood < 0)) {
-		printf("Некорректный ввод\n");
+	printf("Для расчёта массы шкафа введите:\nВысота (180 – 220 см), ширина (80 – 120 см), глубина (50 – 90 см)\n\n");
+	scanf("%f %f %f", &h, &w, &d);
+	if ((h < 180) || (h > 220) || (w < 80) || (w > 120) || (d < 50) || (d > 90)) {
+		printf("Неверные данные\n");
 		return;
 	}
 	h /= 100;
 	w /= 100;
 	d /= 100;
-	rp = h * w * 0.005 * dvp;
-	side = 2 * h * d * 0.015 * dsp;
-	lid = 2 * w * d * 0.015 * dsp;
-	door = w * h * 0.01 * wood;
+	rp = h * w * 0.005f * DVP;
+	side = 2 * h * d * 0.015f * DSP;
+	lid = 2 * w * d * 0.015f * DSP;
+	door = w * h * 0.01f * WOOD;
 	if (h > 200)
-		{shelf = 4 * w * d * 0.015 * dsp;}
+		{shelf = 5 * w * d * 0.015f * DSP;}
 	else
-		{shelf = 3 * w * d * 0.015 * dsp;}
+		{shelf = 4 * w * d * 0.015f * DSP;}
 	tm = rp + side + lid + door + shelf;
-	printf("\nПолная масса шкафа равна %.3lf кг\n\n", tm);
+	printf("\nПолная масса шкафа равна %.1f кг\n\n", tm);
 }
