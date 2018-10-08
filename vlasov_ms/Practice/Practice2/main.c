b930194678290622ca122057ff5ff627bf4d943c
vlasov-unn <vla2000@inbox.ru>
2018-10-08 14:12:10 +0300

    some starting lines

--- vlasov_ms/Practice/Practice2/main.c
+++ vlasov_ms/Practice/Practice2/main.c
@@ -1,9 +1,57 @@
 ﻿#include <stdio.h>
+#include <stdbool.h>
 #include <locale.h>
 
 void main() {
-	long int n, n1;
-	int bufs, cows;
+	long int n, Num1, Num2, cached_Num2, cached_2_Num2;
+	int bufs, cows, cNum2;
+	char current_number, comparing_number, flag = 0;
 	setlocale(LC_ALL, "Russian");
-	printf("Игра \"БЫКИ К КОРОВы\"");
+	do {
+		system("cls");
+		printf("Игра \"БЫКИ И КОРОВЫ\"\n");
+		printf("Введите длину загадываемого числа: ");
+		scanf("%d", &n);
+	} while (n <= 0);
+
+	/*                           *
+	 * загадывание числа сделать *
+	 *                           */
+
+	printf("Отлично, я загадал %d-значное число с неповторяющимися цифрами. Теперь попробуйте угадать его.\nВведите %d-значное число: ", n, n);
+	while (1) {
+		scanf("%d", &Num2);
+		cNum2 = 0;
+		cached_Num2 = Num2;
+		while (cached_Num2 > 0) {
+			cNum2++;
+			cached_Num2 /= 10;
+		}
+		if (cNum2 == n) {
+			cached_Num2 = Num2;
+			while (cached_Num2 > 0) {
+				current_number = cached_Num2 % 10;
+				cached_2_Num2 = Num2;
+				cached_Num2 /= 10;
+				while (cached_2_Num2 > 0) {
+					comparing_number = cached_2_Num2 % 10;
+					cached_2_Num2 /= 10;
+					if (comparing_number == current_number) {
+						flag = 1;
+					}
+				}
+				if (flag == 1) {
+					printf("Цифры в вашем числе повторяются. Повторите попытку: ");
+				}
+				else break;
+			}
+			break;
+		}
+		else 
+			printf("\aВы ввели не %d-значное число. Попробуйте еще раз: ", n);
+	}
+
+
+	printf("hello\n");
+
 }
