ef7327ef50e744bdf787618562322b60472e85ff
Vladislavbul <vlad-hk.19.03@mail.ru>
2018-10-10 22:04:07 +0300

    Practice0

--- bulychev_vl/Practice/Practice0/main.c
+++ bulychev_vl/Practice/Practice0/main.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include <math.h>
-
  void main()
  {
+
 	float x1, y1, r1, x2, y2, r2, d; // d-rasstoyanie mezhdu ñentrami okruzhnostej
- 	printf("Vvedite koordinaty pervoj okruzhnosti i ee radius: \n");
+ 	printf("Vvedite koordinaty pervoj okruzhnosti (x1, y1) i ee radius (r1): \n");
 	scanf("%f %f %f", &x1, &y1, &r1);
-	printf("Vvedite koordinaty vtoroj okruzhnosti i ee radius: \n");
+	printf("Vvedite koordinaty vtoroj okruzhnosti (x2, y2) i ee radius (r2): \n");
 	scanf("%f %f %f", &x2, &y2, &r2);
 	d = sqrtf(((x1 - x2) * (x1 - x2)) + ((y1 - y2) * (y1 - y2)));
 
@@ -51,5 +51,6 @@
 		printf("Okruzhnosti ne peresekayutsya i ne kasayutsya \n");
 		return;
 	}
+
 		printf("Okruzhnosti peresekayutsya i imeyut dve obshie tochki \n");
  }
