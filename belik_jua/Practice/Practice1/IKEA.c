7e00b64a9317c4520d420a80f21b8aea2b5ca6e0
BelikJulia <ybelik2015@yandex.ru>
2018-10-22 14:05:35 +0300

    change IKEA

--- belik_jua/Practice/Practice1/IKEA.c
+++ belik_jua/Practice/Practice1/IKEA.c
@@ -8,24 +8,25 @@ void main()
 	printf("180 < h < 200, h = \n");
 	while ((h > 200) || (h < 180))
 		scanf("%lf", &h);
+    h = h * 0.01;
 	printf("80 < w < 120, w = \n");
 	while ((w > 120) || (w < 80))
 		scanf("%lf", &w);
+    w = w * 0.01;
 	printf("50 < d < 90, d = \n");
 	while ((d > 90) || (d < 50))
 		scanf("%lf", &d);
-
-	dvp = 0.5 * 0.00082;
-	dsp = 1.5 * 0.00065;
-	der = 0.0008;
+    d = d * 0.01;
+    printf("density dvp \n");
+    scanf("%lf", &dvp);
+    printf("density dsp \n");
+    scanf("%lf", &dsp);
+    printf("density wood \n");
+    scanf("%lf", &der);
+	dvp = 0.5 * dvp;
+	dsp = 1.5 * dsp;
 	m = (dvp * h * w) + 2 * (dsp * h * d) + 2 * (dsp * w * d) + (der * h * w);
-
-	while (h > 79.0)
-	{
-		h = h - 40.0;
-		p++;
-	}
-
+    p = (int)(h * 100 / 40) - 1;
 	m = m + dsp * w * d * p;
 	printf("massa = %lf kg", m);
 }
