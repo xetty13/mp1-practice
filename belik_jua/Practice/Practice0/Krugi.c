f5c984fd5113b489fb5933f932d427f6c9cdd559
BelikJulia <ybelik2015@yandex.ru>
2018-10-22 16:35:41 +0300

    Change FunFarm

--- belik_jua/Practice/Practice0/Krugi.c
+++ belik_jua/Practice/Practice0/Krugi.c
@@ -8,7 +8,7 @@ void main()
 	scanf("%lf %lf %lf", &x1, &y1, &r1);
 	printf("x2 -? y2 -? r2 -?\n");
 	scanf("%lf %lf %lf", &x2, &y2, &r2);
-	d = sqrt((x1 - x2)*(x1 - x2) + (y1 - y2)*(y1 - y2));
+	d = sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
 	if ((d == 0) && (r1 == r2))
 	{
 		printf("sovpadaut");
