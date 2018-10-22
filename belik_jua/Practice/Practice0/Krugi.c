27beff5de7ffe504639e136ef29801ef0d36568c
BelikJulia <ybelik2015@yandex.ru>
2018-10-22 13:46:43 +0300

    change krug

--- belik_jua/Practice/Practice0/Krugi.c
+++ belik_jua/Practice/Practice0/Krugi.c
@@ -4,11 +4,11 @@
 void main()
 {
 	double x1, x2, r1, y2, y1, r2, d;
-	printf("x1 -? y1 -? r1 -?\n");
+	printf("x1 -? y1 -? r1 -? \n");
 	scanf("%lf %lf %lf", &x1, &y1, &r1);
-	printf("x2 -? y2 -? r2 -?\n");
+	printf("x2 -? y2 -? r2 -? \n");
 	scanf("%lf %lf %lf", &x2, &y2, &r2);
-	d = sqrt((x1 - x2)*(x1 - x2) + (y1 - y2)*(y1 - y2));
+	d = sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
 	if ((d == 0) && (r1 == r2))
 	{
 		printf("sovpadaut");
