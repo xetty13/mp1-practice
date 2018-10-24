20e17371567f583fc73c6164390581d4f299b2cf
ParanichevaAlyona <43447194+ParanichevaAlyona@users.noreply.github.com>
2018-10-24 19:57:09 +0300

    Update fundament.c

--- paranicheva_av/Practice/Practice1/fundament.c
+++ paranicheva_av/Practice/Practice1/fundament.c
@@ -10,13 +10,13 @@ void main()
 	{
 		if ((h >= 1,8) && (h <= 2,2) && (w >= 0,8) && (w <= 1,2) && (d >= 0,5) && (d <= 0,9))
 		{
-                         m1 = (h * w * 0,005 * pdvp);
-	                 m2 = (h * d * 0,015 * pdsp);
-	                 m3 = (w * d * 0,015 * pdsp);
-		         m4 = (h * w * 0,01 * pder);
-		         m5 = (floor(h / 0,4) * w * d * 0,015 * pdsp);
-		         M = (m1 + m2 + m3 + m4 + m5);
-	                 printf ("Massa shkapha %lf kg", M);
+                      m1 = (h * w * 0,005 * pdvp);
+	                m2 = (h * d * 0,015 * pdsp);
+	                m3 = (w * d * 0,015 * pdsp);
+		        m4 = (h * w * 0,01 * pder);
+		        m5 = (floor(h / 0,4) * w * d * 0,015 * pdsp);
+		        M = (m1 + m2 + m3 + m4 + m5);
+	                printf ("Massa shkapha %lf kg", M);
 		else 
 		{
 			printf("Vvedennye dannye ne korrektny");
