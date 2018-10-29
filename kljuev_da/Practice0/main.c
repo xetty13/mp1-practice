305a0c3afa33335a15b81ee6467e70886a3cde08
KljuevD <aloky.2000@gmail.com>
2018-10-29 23:12:44 +0300

    s

--- kljuev_da/Practice0/main.c
+++ kljuev_da/Practice0/main.c
@@ -5,7 +5,7 @@ void main()
 {
     float x1, y1, r1, x2, y2, r2, rt, dist;
     scanf("%f %f %f %f %f %f", &x1, &y1, &r1, &x2, &y2, &r2);
-    dist = (float)sqrt((x2 - x1)*(x2 - x1) + (y2 - y1)*(y2 - y1));
+    dist = sqrtf((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
     rt = r1 + r2;
     if ((r1 <= 0) || (r2 <= 0)) {
         printf("Ne suschestvujet\n");
