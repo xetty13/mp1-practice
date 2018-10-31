425aaffec05af84f5d81d88fafb6519a120813ec
BelikJulia <ybelik2015@yandex.ru>
2018-10-31 16:48:48 +0300

    Change IKEA 3

--- belik_jua/Practice/Practice1/IKEA.c
+++ belik_jua/Practice/Practice1/IKEA.c
@@ -3,19 +3,21 @@
 void main()
 {
     double h = 0, w = 0, d = 0, m = 0, dvp, dsp, der;
-    int p = 0;
 
-    printf("180 < h < 200, h = \n");
-    while ((h > 200) || (h < 180))
+    do{
+        printf("180 < h < 200, h = \n");
         scanf("%lf", &h);
+    } while ((h > 200) || (h < 180));
     h = h * 0.01;
-    printf("80 < w < 120, w = \n");
-    while ((w > 120) || (w < 80))
+    do {
+        printf("80 < w < 120, w = \n");
         scanf("%lf", &w);
+    } while ((w > 120) || (w < 80));
     w = w * 0.01;
-    printf("50 < d < 90, d = \n");
-    while ((d > 90) || (d < 50))
+    do {
+        printf("50 < d < 90, d = \n");
         scanf("%lf", &d);
+    } while ((d > 90) || (d < 50));
     d = d * 0.01;
 
     printf("density dvp \n");
@@ -28,8 +30,7 @@ void main()
     dsp = 1.5 * dsp;
     der = 1 * der;
 
-    m = (dvp * (h - 2 * 0.015) * w) + 2 * (dsp * (h - 2 * 0.015) * (d - 0.005)) + 2 * (dsp * w * d) + (der * (h - 2 * 0.015) * w); 
-    p = (int)(h * 100 / 40) - 1;
-    m = m + dsp * (w - 2 * 0.015) * (d - 0.005) * p;
+    m = dvp * h * w + 2 * dsp * h * d + 2 * dsp * w * d + der * h * w;
+    m = m + dsp * ((int)((h - 2 * 0.015) / (0.4 + 0.015))) * (w - 2 * 0.015) * (d - 0.005);
     printf("massa = %lf kg", m);
 }
