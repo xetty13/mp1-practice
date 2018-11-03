7798346be31eec5912782aff939274bff5b1ec58
KljuevD <aloky.2000@gmail.com>
2018-11-03 13:40:57 +0300

    r

--- kljuev_da/Practice/Practice0/main.c
+++ kljuev_da/Practice/Practice0/main.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+﻿#include <stdio.h>
 #include <math.h>
 
 void main()
@@ -27,8 +27,7 @@ void main()
         printf("Vkljuchena\n");
         return;
     }
-    if (((r1 > dist) && ((dist + r2) == r1)) ||
-        ((r2 > dist) && ((dist + r1) == r2))) {
+    if (((r1 > dist) && ((dist + r2) == r1)) || ((r2 > dist) && ((dist + r1) == r2))) {
         printf("Kasajutsja iznutri\n");
         return;
     }
