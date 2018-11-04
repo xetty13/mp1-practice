67b023426142f08f3a323aaa9fccdbb6803c50c9
ParanichevaAlyona <43447194+ParanichevaAlyona@users.noreply.github.com>
2018-11-04 14:22:52 +0300

    Update fundament.c

--- paranicheva_av/Practice/Practice1/fundament.c
+++ paranicheva_av/Practice/Practice1/fundament.c
@@ -8,7 +8,7 @@ void main()
 	printf ("Vvedite znacheniya plotnostey dereva, DVP i DSP"); 
 	scanf ("%lf %lf %lf", &pder, &pdvp, &pdsp);
 	{
-		if ((h >= 1,8) && (h <= 2,2) && (w >= 0,8) && (w <= 1,2) && (d >= 0,5) && (d <= 0,9))
+		if ((h >= 1.8) && (h <= 2.2) && (w >= 0.8) && (w <= 1.2) && (d >= 0.5) && (d <= 0.9))
 		{
                       m1 = (h * w * 0,005 * pdvp);
 	                m2 = (h * d * 0,015 * pdsp);
