27c365398a837540a33e067b9b59ac4360f519a5
ParanichevaAlyona <43447194+ParanichevaAlyona@users.noreply.github.com>
2018-11-05 22:37:55 +0300

    Update fundament.c

--- paranicheva_av/Practice/Practice1/fundament.c
+++ paranicheva_av/Practice/Practice1/fundament.c
@@ -5,18 +5,18 @@ void main()
 	double h, w, d, pder, pdvp, pdsp, m1, m2, m3, m4, m5, M;
 	printf ("Vvedite h, w, d");
 	scanf ("%lf %lf %lf", &h, &w, &d);
-	printf ("Vvedite znacheniya plotnostey dereva, DVP i DSP");
+	printf ("Vvedite znacheniya plotnostey dereva, DVP i DSP"); 
 	scanf ("%lf %lf %lf", &pder, &pdvp, &pdsp);
 	{
-		if ((h >= 1,8) && (h <= 2,2) && (w >= 0,8) && (w <= 1,2) && (d >= 0,5) && (d <= 0,9))
+		if ((h >= 1.8) && (h <= 2.2) && (w >= 0.8) && (w <= 1.2) && (d >= 0.5) && (d <= 0.9))
 		{
-            m1 = (h * w * 0,005 * pdvp);
-	        m2 = (h * d * 0,015 * pdsp);
-	        m3 = (w * d * 0,015 * pdsp);
-		    m4 = (h * w * 0,01 * pder);
-		    m5 = (floor(h / 0,4) * w * d * 0,015 * pdsp);
-		    M = (m1 + m2 + m3 + m4 + m5);
-			printf ("Massa shkapha %lf kg", M);
+                      m1 = (h * w * 0,005 * pdvp);
+	                m2 = (h * d * 0,015 * pdsp);
+	                m3 = (w * d * 0,015 * pdsp);
+		        m4 = (h * w * 0,01 * pder);
+		        m5 = (floor((h - 2 * 0.015) / (0.4 + 0.015)) * (w - 2 * 0.015) * (d - 0.005) * 0.01 * pdsp);
+		        M = (m1 + m2 + m3 + m4 + m5);
+	                printf ("Massa shkapha %lf kg", M);
 		}
 		else 
 		{
