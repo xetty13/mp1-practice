af69cb9b9f4b9c4486427e4e6e2242a19471e54b
valeria-vag <43135898+valeria-vag@users.noreply.github.com>
2018-11-05 22:02:42 +0300

    Исправление ошибок

--- vagina_vd/Practice/Practice1/Shkaph.c
+++ vagina_vd/Practice/Practice1/Shkaph.c
@@ -1,63 +1,65 @@
 #include <stdio.h> 
 #include <math.h> 
-void main() {
+void main()
+{
 
 	double h, d, w, p_dvp, p_dsp, p_der, t_dvp, t_dsp, t_der, M, m_zst, m_bok, m_dv, m_kr, m_p;
 
 	int k;
-	printf("Vvedite plotnost dereva v g/cm^3 \n");
-	scanf_s("%lf", &p_der);
+	printf("Vvedite plotnost dereva v kg/m^3 \n");
+	scanf("%lf", &p_der);
 	if (p_der <= 0)
 	{
 		printf("Plotnost dereva zadana neverno. Vvedennoe chislo ne yavlyaetsa polozhitelnym.\n");
 		return;
 	}
-	printf("Vvedite plotnost DSP v g/cm^3 \n");
-	scanf_s("%lf", &p_dsp);
+	printf("Vvedite plotnost DSP v kg/m^3 \n");
+	scanf("%lf", &p_dsp);
 	if (p_dsp <= 0)
 	{
 		printf("Plotnost DSP zadana neverno. Vvedennoe chislo ne yavlyaetsa polozhitelnym.\n");
 		return 0;
 	}
-	printf("Vvedite plotnost DVP v g/cm^3 \n");
-	scanf_s("%lf", &p_dvp);
+	printf("Vvedite plotnost DVP v kg/m^3 \n");
+	scanf("%lf", &p_dvp);
 	if (p_dvp <= 0)
 	{
 		printf("Plotnost DVP zadana neverno. Vvedennoe chislo ne yavlyaetsa polozhitelnym.\n");
 		return 0;
 	}
-
-	t_der = 1;
-	t_dsp = 1.5;
-	t_dvp = 0.5;
+        t_der = 0.01; //толщина в метрах
+	t_dsp = 0.015;
+	t_dvp = 0.005;
 	printf("Vvedite vysotu skapha v santimetrah (ot 180 do 220 cm) \n");
-	scanf_s("%lf", &h);
+	scanf("%lf", &h);
 	if ((h < 180) || (h > 220))
 	{
 		printf("Vysota zadana neverno. Vvedennoe chislo ne vhodit v predely ot 180 do 220 cm \n");
 		return 0;
 	}
+	h = h * 0.01; //перевод в метры
 	printf("Vvedite shirinu skapha v santimetrah  (ot 80 do 120 cm) \n ");
-	scanf_s("%lf", &w);
+	scanf("%lf", &w);
 	if ((w < 80) || (w > 120))
 	{
 		printf("Shirina zadana neverno. Vvedennoe chislo ne vhodit v predely ot 80 do 120 cm \n");
 		return 0;
 	}
+	w = w * 0.01; //перевод в метры
 	printf("Vvedite glubinu skapha v santimetrah  (ot 50 do 90 cm) \n");
-	scanf_s("%lf", &d);
+	scanf("%lf", &d);
 	if ((d < 50) || (d > 90))
 	{
 		printf("Glubina zadana neverno. Vvedennoe chislo ne vhodit v predely ot 50 do 90 cm \n");
 		return 0;
 	}
+	d = d * 0.01; //перевод в метры
 	k = h / 41;
 	m_zst = p_dvp * t_dvp * h * w;
 	m_bok = 2 * p_dsp * t_dsp * h * d;
 	m_kr = 2 * p_dsp * w * d * t_dsp;
 	m_dv = p_der * h * t_der * w;
-	m_p = k * p_dsp * (w - 3)  * d;
-	M = (m_p + m_kr + m_bok + m_zst + m_dv) / 1000;
+	m_p = k * p_dsp (h - 2 * t_dsp) / (0.4 + t_dsp) * (w - 2 * t_dsp) * (d - t_dvp);
+	M = (m_p + m_kr + m_bok + m_zst + m_dv);
 	printf("Massa shkapha ravna (v kg) %lf", M);
-
 }
