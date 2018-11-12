0b0026cc6d6481d04dec9d5a417a45ee2a514017
MaksimShagov <43129418+MaksimShagov@users.noreply.github.com>
2018-11-12 09:25:43 +0300

    Update main.c

--- shagov_ma/Practice/Practice3/main.c
+++ shagov_ma/Practice/Practice3/main.c
@@ -2,36 +2,37 @@
 #include <stdlib.h>
 #include <time.h>
 #include <locale.h>
+
 void main()
 {
 	int number,user_number,i,p;
 	char zhak;
-	long int a,b;//отрезок от а до b
-	a=0;
-	b=1000;
+	long int a, b;//отрезок от а до b
+	a = 0;
+	b = 1000;
 	setlocale(LC_ALL, "Russian");
 	srand((unsigned int) time(0));
 	do{
 		printf("Укажите режим работы\n1 - отгадать самому;\n2 - загадать компьютеру;\n3 - выйти из программы\n");
 		scanf("%d",&i);
-		if (i==1)
+		if (i == 1)
 		{
-			p=0;
-			number=rand()%1000;
-			user_number=-1;
-			while(user_number!=number)
+			p = 0;
+			number = rand()%1000;
+			user_number = -1;
+			while(user_number != number)
 			{
 				printf("\n");
 				scanf("%d", &user_number);
-				if (number>user_number)
+				if (number > user_number)
 				{
 					printf("\nЗагаданное число больше");
 				}
-				if (number<user_number)
+				if (number < user_number)
 				{
 					printf("\Загаданное число меньше");
 				}
-				if (number==user_number)
+				if (number == user_number)
 				{
 					printf("\nУгадали за %d попыток \n", p);
 				}
@@ -39,29 +40,29 @@ void main()
 			p++;
 		}
 		
-		if (i==2)
+		if (i == 2)
 		{
-			p=0;
+			p = 0;
 			do
 			{
-				number=a + rand()%(b - a + 1);
+				number = a + rand()%(b - a + 1);
 				printf("\n%d",number);
 				printf("\n Оцените число\n> если число болшьше,\n< если число меньше,\n= если число равно загаданному\n");
 				scanf("%*c%c", &zhak);//Все прекрасно работает с числами, но как только пытаешься использовать символы и тип char сразу начинается ерунда
-				if (zhak=='>')
+				if (zhak == '>')
 				{
-					a=number+1;
+					a = number+1;
 				}
-				if (zhak=='<')
+				if (zhak == '<')
 				{
-					b=number-1;
+					b = number-1;
 				}
-				if (zhak=='=')
+				if (zhak == '=')
 				{
 					printf("Число угадано за %d попыток", p);
 				}
 				p++;
-			}while(zhak!='=');
+			}while(zhak != '=');
 		}
-	}while(i!=3);
+	}while(i != 3);
 }
