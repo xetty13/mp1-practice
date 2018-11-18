60f876e318ca5934921a74bcd10dd0d10657ccef
BelikJulia <ybelik2015@yandex.ru>
2018-11-18 16:22:50 +0300

    Pyaterochka 3.0

--- belik_jua/Practice/Practice4/Pyaterochka.c
+++ belik_jua/Practice/Practice4/Pyaterochka.c
@@ -6,7 +6,7 @@
 
 void Command()
 {
-    printf("\n1 - add item \n");
+    printf("\n1 - scan item \n");
     printf("2 - display description \n");
     printf("3 - add item to check \n");
     printf("4 - form a check \n");
@@ -38,23 +38,28 @@ int Scan(int goo[], int num)
     return temp;
 }
 
-void Desciption(/*char prod[][], */int pr[], int dis[], int num)
+void Desciption(char prod[][L], int pr[], int dis[], int num)
 {
     int i;
-    printf("id %d ", num);
-    /*    for (i = 0; i < L; i++)
-            printf("%c", prod[i][num]);*/ printf(" tovar ");
-    printf("price %d dis %d \n", pr[num], dis[num]);
+    printf("id ");
+    if (num < 1000)
+        printf("0");
+    if (num < 100)
+        printf("0");
+    if (num < 10)
+        printf("0");
+    printf("%d ", num);
+        for (i = 0; i < L; i++)
+            printf("%c", prod[num][i]); 
+    printf(" price %d dis %d \n", pr[num], dis[num]);
 }
 
-void AddToCheck(int check, int total, int quan[], int pr[], int dis[], int num, int q)
+void AddToCheck(int quan[], int pr[], int dis[], int num, int q)
 {
     quan[num] += q;
-    check += (pr[num] * quan[num]);
-    total += (pr[num] * quan[num] * (100 - dis[num]) / 100);
 }
 
-void Form(int quan[],/* char prod[][], */int pr[], int dis[]/*, int check, int total*/)
+void Form(int quan[], char prod[][L], int pr[], int dis[])
 {
     int i, tov;
     int check = 0, total = 0;
@@ -62,7 +67,7 @@ void Form(int quan[],/* char prod[][], */int pr[], int dis[]/*, int check, int t
     {
         if (quan[i] > 0)
         {
-            Desciption(/*prod, */pr, dis, i);
+            Desciption(prod, pr, dis, i);
             tov = quan[i] * pr[i] * (100 - dis[i]) / 100;
             printf("quan %d total %d \n", quan[i], tov);
             check += (pr[i] * quan[i]);
@@ -71,9 +76,8 @@ void Form(int quan[],/* char prod[][], */int pr[], int dis[]/*, int check, int t
     }
     if (check > 0)
     {
-        printf("total %d \n", total);
         i = (check - total) * 100 / check;
-        printf("discount %d \n", i);
+        printf("without discount %d discount %d total %d \n", check, i, total);
     }
     else
         printf("No products added");
@@ -81,9 +85,9 @@ void Form(int quan[],/* char prod[][], */int pr[], int dis[]/*, int check, int t
 
 void main()
 {
-    int num = 0, i, total = 0, check = 0, q;
+    int num = 0, i, q;
     int quan[N] = {0}, pr[N], dis[N], goo[N];
- //   char prod[L][N] = {'M', 'i', 'l', 'k',' ', 'B', 'r', 'e', 'a', 'd', 'E', 'g', 'g', ' ', ' ', 'F', 'l', 'o', 'u', 'r'};
+    char prod[N][L] = {'M', 'i', 'l', 'k',' ', 'B', 'r', 'e', 'a', 'd', 'E', 'g', 'g', ' ', ' ', 'F', 'l', 'o', 'u', 'r'};
     Gen(pr, N, 1000.0);
     Gen(dis, N, 50.0);
     q = Scan(goo, num);
@@ -97,17 +101,17 @@ void main()
             q = Scan(goo, num);
             break;
         case 2:
-            Desciption(/*prod, */pr, dis, goo[num]);
+            Desciption(prod, pr, dis, goo[num]);
             break;
         case 3: 
-            AddToCheck(check, total, quan, pr, dis, goo[num], q);
+            AddToCheck(quan, pr, dis, goo[num], q);
             num++;
             break;
         case 4:
-            Form(quan,/* prod, */pr, dis/*, check, total*/);
+            Form(quan, prod, pr, dis);
             break;
         case 5:
-            Form(quan,/* prod, */pr, dis/*, check, total*/);
+            Form(quan, prod, pr, dis);
             break;
         }
     } while (i != 5);
