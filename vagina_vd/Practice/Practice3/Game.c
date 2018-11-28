a6e7b5e60310ca7fe7faabde93789eb65087aa80
valeria-vag <43135898+valeria-vag@users.noreply.github.com>
2018-11-28 23:45:41 +0300

    Update Game.c

--- vagina_vd/Practice/Practice3/Game.c
+++ vagina_vd/Practice/Practice3/Game.c
@@ -3,7 +3,7 @@
 #include <time.h>
 void main()
 {
-	int r, ch, ch_pl, k, n1 = 1, n2 = 1000;
+	int r, ch, ch_pl = 0, k = 0, n1 = 1, n2 = 1000;
 	char ssign;
 
 	printf("HELLO! WELCOME TO THE GAME!\n");
@@ -12,51 +12,54 @@ void main()
 	printf("2 - the computer guesses the number \n");
 	do
 	{
-		printf("Choose the game mode (1 or 2) \n"); //выбор режима с проверкой
+		printf("Choose the game mode (1 or 2) \n"); //РІС‹Р±РѕСЂ СЂРµР¶РёРјР° СЃ РїСЂРѕРІРµСЂРєРѕР№
 		scanf("%d", &r);
-	} while ((r != 1) && (r != 2));
+	} while (r != 1 && r != 2);
 	srand((unsigned int)time(0));
-	k = 0;
-	if (r == 1)   //режим 1  
+	if (r == 1)   //СЂРµР¶РёРј 1  
 	{
 		ch = rand() % (n2 - n1 + 1) + n1;
 		printf("The computer made a number from 1 to 1000. Let's guess!\n");
 		do
 		{
-			while ((ch_pl > 0) && (ch_pl <= 1000))
+			do 
+			{ 
 				scanf("%d", &ch_pl);
+			} while (ch_pl <= 0 && ch_pl > 1000);
+				
 			if (ch_pl < ch)
 				printf("This number %d is smaller than the number made by the computer. Try again \n", ch_pl);
 			else
 				printf("This number %d is bigger than the number made by the computer. Try again \n", ch_pl);
-				k++; // счетчик попыток
+				k++; // СЃС‡РµС‚С‡РёРє РїРѕРїС‹С‚РѕРє
 		} while (ch != ch_pl);
 		printf("You guessed the number! It's %d. Number of attempts: %d \n", ch, k);
 		return;
 	}
 
-	else       //режим 2
+	else       //СЂРµР¶РёРј 2
 	{
-		printf("HINT: if the computer has entered a number larger, enter '>' else '<' /n  ");
-		printf("If the computer has entered a number equal to done, enter '=' ");
-		while ((ch_pl > 0) && (ch_pl <= 1000))
+		printf("HINT: If the computer has entered a number larger, enter '>' else '<' \n  ");
+		printf("    If the computer has entered a number equal to done, enter '=' \n ");
+		printf("\n");
+		do
 		{
 			printf("Make a number from 1 to 1000\n");
 			scanf("%d", &ch_pl);
-		}
+		} while (ch_pl <= 0 && ch_pl > 1000);
 		do
 		{
 			ch = rand() % (n2 - n1 + 1) + n1;
-			printf("The computer made a number %d /n", ch);
+			printf("The computer made a number %d\n", ch);
 
-			while ((ssign == '=') || (ssign == '<') || (ssign == '>'))
+			do 
 			{
-				printf("Enter the sign /n");
-				scanf("%с", &ssign);
-			}
-			if (ssign == '<')
-				n2 = ch - 1;      // "сокращение" отрезка
+				printf("Enter the sign\n");
+				scanf("%1s", &ssign);
+			} while (ssign != '=' && ssign != '<' && ssign != '>');
 			if (ssign == '>')
+				n2 = ch - 1;      // "СЃРѕРєСЂР°С‰РµРЅРёРµ" РѕС‚СЂРµР·РєР°
+			if (ssign == '<')
 				n1 = ch + 1;
 			k++;
 		} while (ssign != '=');
