be30b477ec753e1b3a3305ea717d8003cb5e1bbf
Бакаева Мария Игоревна <bakaeva.m@ccam.cmc.unn.net>
2018-12-03 14:19:14 +0300

    new_sort

--- bakaeva_mi/Practice/Practice5/Sorting.c
+++ bakaeva_mi/Practice/Practice5/Sorting.c
@@ -2,9 +2,11 @@
 #include <time.h>
 #include <stdlib.h>
 #define N 7
-#define k 15
+#define k 6
 #define n1 0
 #define n2 5
+int arr[N] = { 0 };
+
 
 void input(int a[], int n)
 {
@@ -25,91 +27,142 @@ void output(int a[], int n)
 void choose_sort(int a[], int n)
 {
     int i, j, min, minidx;
+    int copy[N];
+
+    for (i = 0; i < n; i++)
+        copy[i] = a[i];
+
     for (i = 0; i < n; i++)
     {
-        min = a[i];
+        min = copy[i];
         minidx = i;
         for (j = i + 1; j < n; j++)
-            if (a[j] < min)
+            if (copy[j] < min)
             {
-                min = a[j];
+                min = copy[j];
                 minidx = j;
             }
-        a[minidx] = a[i];
-        a[i] = min;
+        copy[minidx] = copy[i];
+        copy[i] = min;
     }
+    output(copy, N);
+    printf("\n");
 }
 
 void insert_sort(int a[], int n)
 {
     int i, j, tmp;
+    int copy[N];
+
+    for (i = 0; i < n; i++)
+        copy[i] = a[i];
+
     for (i = 1; i < n; i++)
     {
-        tmp = a[i];
+        tmp = copy[i];
         j = i - 1;
-        while ((j >= 0) && (a[j] > tmp))
+        while ((j >= 0) && (copy[j] > tmp))
         {
-            a[j + 1] = a[j];
-            a[j] = tmp;
+            copy[j + 1] = copy[j];
+            copy[j] = tmp;
             j--;
         }
     }
+    output(copy, N);
+    printf("\n");
 }
 
 void bubble_sort(int a[], int n)
 {
     int i, j, tmp;
+    int copy[N];
+
+    for (i = 0; i < n; i++)
+        copy[i] = a[i];
     for (i = 0; i < n; i++)
         for (j = 1; j < n; j++)
-            if (a[j - 1] > a[j])
+            if (copy[j - 1] > copy[j])
             {
-                tmp = a[j];
-                a[j] = a[j - 1];
-                a[j - 1] = tmp;
+                tmp = copy[j];
+                copy[j] = copy[j - 1];
+                copy[j - 1] = tmp;
             }
+    output(copy, N);
+    printf("\n");
 }
 
 void count_sort(int a[], int n)
 {
-    int i, j, idx;
+    int i, j, idx, min, max;
     int count[k] = { 0 };
+    int copy[N];
+
+    for (i = 0; i < n; i++)
+        copy[i] = a[i];
+
+    min = copy[0];
+    max = copy[0];
     for (i = 0; i < n; i++)
     {
-        count[a[i]]++;
-        idx = 0;
-        for (i = 0; i < k; i++)
-            for (j = 0; j < count[i]; j++)
-                a[idx++] = i;
+        if (min > copy[i]) min = copy[i];
+        if (max < copy[i]) max = copy[i];
     }
+    idx = 0;
+    for (i = 0; i < n; i++)
+    {
+        count[copy[i]]++;
+        
+        for (i = min; i < max + 1; i++)
+            for (j = 0; j < count[i]; j++)
+                copy[idx++] = i;
+    }
+    output(copy, N);
+    printf("\n");
+}
+
+void quick_split(int a[], int *i, int *j, int p)
+{
+    int tmp;
+    do
+    {
+        while (a[*i] < p) *i++;
+        while (a[*j] > p) *j--;
+        if (*i <= *j)
+        {
+            tmp = a[*i];
+            a[*i] = a[*j];
+            a[*j] = tmp;
+        }
+    } while (*i < *j);
+}
+
+void quick_sort(int a[], int s1, int s2)
+{
+    int m = (s1 + s2) / 2;
+    int i = s1, j = s2;
+    int copy[N];
+
+    for (i = 0; i < N; i++)
+        copy[i] = a[i];
+    quick_split(a, &i, &j, m);
+    if (i > s1) quick_sort(copy, i, j);
+    if (j < s2) quick_sort(copy, j, s2);
+    output(copy, N);
+    printf("\n");
 }
 
 void main()
 {
-    int a[N] = { 0 };
+    input(arr, N);
+    output(arr, N);
 
-    input(a, N);
-    output(a, N);
-    choose_sort(a, N);
-    output(a, N);
-    printf("\n");
+    choose_sort(arr, N);
 
-    input(a, N);
-    output(a, N);
-    insert_sort(a, N);
-    output(a, N);
-    printf("\n");
+    insert_sort(arr, N);
 
-    input(a, N);
-    output(a, N);
-    bubble_sort(a, N);
-    output(a, N);
-    printf("\n");
+    bubble_sort(arr, N);
 
-    input(a, N);
-    output(a, N);
-    count_sort(a, N);
-    output(a, N);
-    printf("\n");
+    count_sort(arr, N);
 
     system("pause");
 
