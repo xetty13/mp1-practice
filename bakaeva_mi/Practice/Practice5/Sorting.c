10086d63bbe917c75408a7eff4954f53e555d5cb
Maria Bakaeva <mary396969@gmail.com>
2018-12-21 21:41:39 +0300

    change2

--- bakaeva_mi/Practice/Practice5/Sorting.c
+++ bakaeva_mi/Practice/Practice5/Sorting.c
@@ -4,77 +4,110 @@
 #include <time.h>
 #include <locale.h>
 #include <string.h>
-
-//#define MAX_PATH 800 //Количество файлов в папке
-#define N 50 //Размер массива
 #define BUFFER 1000 //Размер буффера
 
 void input(wchar_t **path) //Массив куда будет записана директория
 {
-    char *str; //Создадим указатель
+    char *str; //Сюда принимаем строку в char
     int k = 0, i = 0;
 
-    *path = (wchar_t*)malloc(BUFFER * sizeof(wchar_t)); //выделение памяти для пути
+    *path = (wchar_t*)malloc(BUFFER * sizeof(wchar_t));
     str = (char*)malloc(BUFFER * sizeof(char));
-    printf("Enter the path");
-    fgets(str, BUFFER, stdin);   //СПРОСИТЬ МАКСИМА
-   // str[strlen(str) - 1] = '\0';
-    swprintf(*path, BUFFER, L"%hs", str);
+    printf("Enter the path  ");
+    fgets(str, BUFFER, stdin);   //Ввод строки до enter
+    str[strlen(str) - 1] = '\0'; //Удаление перевода строки в конце
+    swprintf(*path, BUFFER, L"%hs", str); //Преобразовываем строку char  в wchar_t 
 }
 
-int ListDirectoryContetns(const wchar_t *sDir, int **fNames)
+int ListDirectoryContetns(const wchar_t *sDir, wchar_t ***fNames, ULONGLONG **fSizes)
 {
     WIN32_FIND_DATA fdFile;
-    HANDLE hFind = FindFirstFile("C:\\*", &fdFile);
+    HANDLE hFind = NULL;
     int i = 0;
-
-    wchar_t sPath[2048];
-
-    wsprintf(sPath, L"%s\\*.*", sDir); //sPath - куда попала преобразованная строка, маска, указатель на введенную строку
+    wchar_t *sPath;
+    sPath = (wchar_t*)malloc(BUFFER * sizeof(wchar_t));
 
     hFind = FindFirstFile(sPath, &fdFile);
-
+    wsprintf(sPath, L"%s\\*.*", sDir);
     //Проверка на существование директории
-    if (hFind == INVALID_HANDLE_VALUE)
+    if ((hFind = FindFirstFile(sPath, &fdFile)) == INVALID_HANDLE_VALUE)
+        return -1; //Path not found
+
+    do
     {
-        wprintf(L"Path not found: [%s]\n", sDir);
-        return -1;
-    }
+        if (wcscmp(fdFile.cFileName, L".") != 0 && wcscmp(fdFile.cFileName, L"..") != 0)
+            i++;
+    } while (FindNextFile(hFind, &fdFile));
+
+    *fNames = (wchar_t**)malloc(i * sizeof(wchar_t*));
+    *fSizes = (ULONGLONG*)malloc(i * sizeof(ULONGLONG));
 
     do
     {
         //Найденный первый файл будет возвращать "."
         // Последующие файлы будут возвращать ".."
-        /*if (wcscmp(fdFile.cFileName, L".") != 0 && wcscmp(fdFile.cFileName, L"..") != 0)
+        if (wcscmp(fdFile.cFileName, L".") != 0 && wcscmp(fdFile.cFileName, L"..") != 0)
         {
-            //Создадим путь к файлуб используя переданный в [sDir] и имя файла, которое мы только что нашли
-            wsprintf(sPath, L"%s\\%s", sDir, fdFile.cFileName);
-            wsprintf(fNames[i], L"%s", sPath);
-            //Является ли объект файлом или папкой?
-            if (fdFile.dwFileAttributes &FILE_ATTRIBUTE_DIRECTORY)
-            {
-                wprintf(L"Directory: %s\n", sPath);
-                ListDirectoryContetns(sPath, fNames); //Рекурсия
-            }
-            else
-            {
-                wprintf(L"File: %s\n", sPath);
-            }*/
+            ULONGLONG fSize = fdFile.nFileSizeHigh;
+            fSize <<= sizeof(fdFile.nFileSizeHigh) * 8;
+            fSize |= fdFile.nFileSizeLow;
 
-        strcpy(fNames[i], fdFile.cFileName);
-        puts(fNames[i]);
+            (*fNames)[i] = (wchar_t*)malloc(BUFFER * sizeof(wchar_t));
+            (*fSizes)[i] = fSize;
+            wsprintf(sPath, L"%s\\%s", sDir, fdFile.cFileName);
+            wsprintf((*fNames)[i], L"%s", sPath);
             i++;
-    }while (FindNextFile(hFind, &fdFile)); //Поиск следующего файла
-    FindClose(hFind);
+        }
+    } while (FindNextFile(hFind, &fdFile)); //Поиск следующего файла
     return i;
     FindClose(hFind);
+    free(sPath);
+}
 
+void choose_method(int *method)
+{
+    printf("\nChoose a sorting\n");
+    printf("  1) Choose sorting\n");
+    printf("  2) Insert sorting\n");
+    printf("  3) Bubble sorting\n");
+    printf("  4) Count sorting\n");
+    printf("  5) Quick sorting\n");
+    printf("  6) Merge sorting\n\n");
+    scanf("%d", method);
+}
+
+void output(int *ind, int count_files, wchar_t **fNames, ULONGLONG *size)
+{
+    int i, j;
+    WIN32_FIND_DATA fdFile;
+    for (i = 0; i < count_files; i++)
+        printf(L"%s        %lld\n", fNames[ind[i]], *(size + ind[i]));
+    printf("\n\n");
+}
+
+void choose_sort(int count_files, ULONGLONG *size, int *ind)
+{
+    int i = 0, j = 0, k = 0, t = 0;
+    long min;
+
+    for (i = 0; i < count_files; i++)
+    {
+        k = i;
+        min = size[i];
+        for (j = i + 1; j < count_files; j++)
+            if (size[j] < min)
+            {
+                k = j;
+                min = size[j];
+            }
+        t = ind[k];
+        ind[k] = ind[i];
+        ind[i] = t;
+    }
 }
 
 void start()
 {
-    //choose_method();
-
    /* switch ()
     {
     case(1):
@@ -88,46 +121,41 @@ int main()
     char answer;
     wchar_t *path;
     wchar_t **fNames;
-    long *fSizes;
+    ULONGLONG *fSizes;
     int count_files = 0;
+    int method, i = 0;
+    int *index;
+    setlocale(LC_ALL, "Russian");
 
     fNames = (wchar_t**)malloc(MAX_PATH * sizeof(wchar_t*));
-    fSizes = (long*)malloc(MAX_PATH * sizeof(long));
+    fSizes = (ULONGLONG*)malloc(MAX_PATH * sizeof(ULONGLONG));
 
     printf("\tFILE MANAGER\n\n");
     // start(&path, path, fNames, fSizes);
 
+    do
+    {
      //Ввод пути с клавиатуры
     input(&path);
-    //Передаем в функцию записи имен и рзмеров введенную директорию и массив с именами и размерами
-    //count_files = ListDirectoryContents(&(path[0]), &fNames);
+    count_files = ListDirectoryContetns(path, &fNames, &fSizes);
 
-    WIN32_FIND_DATA fdFile;
-    HANDLE hFind = FindFirstFile("C:\\*", &fdFile);
-    int i = 0;
+    printf("Files found:  %d\n\n", count_files); //Вывод количества найденных файлов
+    index = (int*)malloc(BUFFER * sizeof(int));
 
-    //wchar_t sPath[2048];
+    for (i = 0; i < count_files; i++)
+        index[i] = i;
 
-    //wsprintf(sPath, L"%s\\*.*", path); //sPath - куда попала преобразованная строка, маска, указатель на введенную строку
+    choose_method(&method);
 
-    //hFind = FindFirstFile(sPath, &fdFile);
-
-    //Проверка на существование директории
-    if (hFind == INVALID_HANDLE_VALUE)
+    switch (method)
     {
-       puts(L"Path not found");
-        do
-        {
-            strcpy(fNames[i], fdFile.cFileName);
-           // puts(fNames[i]);
-            i++;
-        } while (FindNextFile(hFind, &fdFile)); //Поиск следующего файла
-        FindClose(hFind);
+    case(1):
+    {
+        choose_sort(count_files, fSizes, index);
+        output(index, count_files, fNames, fSizes);
+    }
+    }
 
-        printf("%d", i); //Вывод количества найденных файлов
-
-        do
-        {
             printf("Start over? ( y or n )");
             scanf("%c", &answer);
             switch (answer)
@@ -138,6 +166,4 @@ int main()
         } while (answer != 'n');
 
         system("pause");
-
-    }
 }
