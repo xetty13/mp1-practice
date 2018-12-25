f403eeddce2521630d6d305a84cef62bda805151
BelikJulia <ybelik2015@yandex.ru>
2018-12-25 18:11:55 +0300

    строчный код

--- belik_jua/Practice/Practice4/Pyaterochka.c
+++ belik_jua/Practice/Practice4/Pyaterochka.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#define N 10
-#define L 5
+#define N 10000
+#define L 10
 
 void Command()
 {
@@ -23,32 +23,31 @@ void Gen(int a[], int n, double m)
 
 int Scan(int goo[], int num)
 {
-    int temp;
+    char tov[4];
+    int tmp, i;
     do
     {
         printf("Enter barcode \n");
-        scanf("%d", &temp);
-    } while ((temp < 0) || (temp > N));
-    goo[num] = temp;
+        scanf("%s", &tov);
+        tmp = tov[3] - '0' + (tov[2] - '0') * 10 + (tov[1] - '0') * 100 + (tov[0] - '0') * 1000; 
+    } 
+    while ((tmp < 0) || (tmp > N)); 
+    goo[num] = tmp; 
+    for (i = 0; i < 4; i++)
+        tov[i] = NULL;
     do
     {
         printf("Enter the quantity of goods \n");
-        scanf("%d", &temp);
-    } while ((temp < 0) || (temp > 1000));
-    return temp;
+        scanf("%d", &tmp); 
+    } 
+    while ((tmp < 0) || (tmp > 1000));
+    return tmp;
 }
 
 void Desciption(char prod[][L], int pr[], int dis[], int num)
 {
     int i;
     printf("id ");
-    if (num < 1000)
-        printf("0");
-    if (num < 100)
-        printf("0");
-    if (num < 10)
-        printf("0");
-    printf("%d ", num);
         for (i = 0; i < L; i++)
             printf("%c", prod[num][i]); 
     printf(" price %d dis %d \n", pr[num], dis[num]);
@@ -85,12 +84,21 @@ void Form(int quan[], char prod[][L], int pr[], int dis[])
 
 void main()
 {
-    int num = 0, i, q;
+    int num = 0, i, j, q;
     int quan[N] = {0}, pr[N], dis[N], goo[N];
-    char prod[N][L] = { "     ", "Milk ", "Bread", "Egg  ", "Flour", "Apple", "Meat ", "Fish " };
+    char prod[N][L] = { "0000 Pack ", "0001 Milk ", "0002 Bread", "0003 Egg  ", "0004 Flour", "0005 Apple", "0006 Meat ", "0007 Fish " }; 
+    for (i = 8; i < N; i++)
+    {
+        prod[i][0] = (i / 1000) + '0';
+        prod[i][1] = (i / 100) % 10 + '0';
+        prod[i][2] = (i / 10) % 10 + '0';
+        prod[i][3] = (i % 10) + '0';
+        for (j = 4; j < L; j++)
+            prod[i][j] = prod[i % 8][j];
+    }
     Gen(pr, N, 1000.0); 
     Gen(dis, N, 50.0);
-    q = Scan(goo, num);
+    q = Scan(goo, num); 
     do
     {
         Command();
@@ -114,5 +122,6 @@ void main()
             Form(quan, prod, pr, dis);
             break;
         }
-    } while (i != 5);
+    } 
+    while (i != 5);
 }
