f01653790e4159f4b67d79af9809788ee2f2ff65
BelikJulia <ybelik2015@yandex.ru>
2019-04-09 11:45:29 +0300

    One too many

---
+++ belik_jua/Practice/Practice7/Vector.h
@@ -0,0 +1,33 @@
+#pragma once
+
+class Vector
+{
+private:
+    int size;
+    double *arr;
+public:
+    Vector();
+    Vector(int x);
+    Vector(const Vector& vec);
+    ~Vector();
+
+    Vector operator+ (const Vector & tmp) const;
+    Vector operator- (const Vector & tmp) const;
+    double operator* (const Vector & tmp) const;
+    Vector operator+ (double tmp) const;
+    Vector operator- (double tmp) const;
+    Vector operator* (double tmp) const;
+    Vector operator= (const Vector & tmp);
+    double* operator[] (int i) const;
+    double Lenght() const;
+    Vector operator+= (const Vector & tmp);
+    Vector operator-= (const Vector & tmp);
+    Vector operator+= (double tmp);
+    Vector operator-= (double tmp);
+    Vector operator*= (double tmp);
+    void *operator new[](size_t size);
+    void operator delete[] (void *p);
+
+    void PrintVector() const;
+    void Fill();
+};
