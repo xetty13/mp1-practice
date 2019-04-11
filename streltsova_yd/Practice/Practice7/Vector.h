b8c25373077e71bec6e69720409e9815405a3498
streltsova-yana <yanastreltsova03@gmail.com>
2019-04-11 22:27:35 +0300

    Practice7

---
+++ streltsova_yd/Practice/Practice7/Vector.h
@@ -0,0 +1,29 @@
+struct Vector
+{
+private:
+    int n;
+public:
+    double *x;
+    Vector(); //конструктор по умолчанию
+    Vector(int _n); // конструктор с параметром
+    Vector(const Vector& a); // конструктор копирования
+    ~Vector(); // деструктор
+    Vector operator+(const Vector& a) const; // покомпонентное сложение
+    Vector operator-(const Vector& a) const; // покомпонентное вычитание
+    Vector operator+(double a) const; // прибавление константы
+    Vector operator-(double a) const; // вычитание константы
+    Vector operator*(double a) const; // умножение на константу
+    double operator*(const Vector& a) const; // скалярное произведение
+    Vector operator+=(const Vector& a); // покомпонентное сложение
+    Vector operator-=(const Vector& a); // покомпонентное вычитание
+    Vector operator+=(double a); // прибавление константы
+    Vector operator-=(double a); // вычитание константы
+    Vector operator*=(double a); // умножение на константу
+    Vector& operator=(const Vector& a); // присваивание
+    double* operator[](int i) const; // запись и чтение
+    double Length() const; // функция получения дины вектора
+    void *operator new[](size_t n); //выделение памяти
+    void operator delete[](void *p); // освобождение памяти
+    void Add(); // добавление вектора
+    void Print() const;// печать вектора
+};
