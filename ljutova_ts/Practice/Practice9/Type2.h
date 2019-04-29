3181e613dbca942bd4bb43011381be9ebe0df49e
lutovatanya <lutovat20@gmail.com>
2019-04-30 01:47:28 +0300

    +Type1 Type2 Task

--- ljutova_ts/Practice/Practice9/Type2.h
+++ ljutova_ts/Practice/Practice9/Type2.h
@@ -1,1 +1,16 @@
-#pragma once
+#ifndef _TYPE2_H_
+#define _TYPE2_H_
+#include "Task.h"
+
+class Type2 : public Task
+{
+	Time t1, t2;
+public:
+	Type2();
+	Type2(string, Date, Time, Time);
+	~Type2();
+
+	void Print();
+};
+
+#endif
