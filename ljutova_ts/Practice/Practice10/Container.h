c7d250b7efbc40f8a6c019e31f3cea59ea6c70c2
lutovatanya <lutovat20@gmail.com>
2019-05-15 01:08:51 +0300

    .

---
+++ ljutova_ts/Practice/Practice10/Container.h
@@ -0,0 +1,273 @@
+﻿#ifndef _CONTAINER_H_ 
+#define _CONTAINER_H_ 
+
+#include <iostream> 
+#include "Exception.h" 
+
+using namespace std;
+
+template <typename T, int maxsize>
+class Container
+{
+	T* arr;
+	int n;
+
+public:
+	Container();
+	Container(T*, int);
+	Container(const Container &x);
+	~Container();
+	bool isFull()const;
+	bool isEmpty()const;
+	int Find(T a)const;
+	void Add(T a);
+	void Delete(T a);
+	void Delete1(int);
+	T& operator[](int);
+	const T& operator[](int) const;
+	friend std::ostream& operator << (std::ostream & o, const Container& x)
+	{
+		for (int i = 0; i < x.n; i++)
+		{
+			cout « x[i] « " ";
+		}
+		return o;
+	}
+};
+
+template <typename T, int maxsize>
+Container <T, maxsize>::Container()
+{
+	n = 0;
+	arr = new T[maxsize];
+};
+
+template <typename T, int maxsize>
+Container <T, maxsize>::Container(const Container <T, maxsize> &c)
+{
+	n = c.n;
+	arr = new T[n];
+	for (int i = 0; i < n; i++)
+		arr[i] = c.arr[i];
+};
+
+template <typename T, int maxsize>
+Container <T, maxsize>::Container(T* arr1, int x)
+{
+	n = x;
+	arr = new T[maxsize];
+	for (int i = 0; i < n; i++)
+		arr[i] = arr1[i];
+};
+
+template <typename T, int maxsize>
+Container <T, maxsize>::~Container()
+{
+	n = 0;
+	delete[] arr;
+};
+
+template <typename T, int maxsize>
+bool Container <T, maxsize>::isFull() const
+{
+	return n == maxsize;
+};
+
+template <typename T, int maxsize>
+bool Container <T, maxsize>::isEmpty() const
+{
+	return n == 0;
+};
+
+template <typename T, int maxsize>
+int Container <T, maxsize>::Find(T a) const
+{
+
+	for (int i = 0; i < n; i++)
+		if (arr[i] == a)
+			return i;
+	return -1;
+};
+
+template <typename T, int maxsize>
+void Container <T, maxsize>::Add(T a)
+{
+	if (isFull())
+	{
+		throw Exception("Your container is full");
+	}
+	arr[n] = a;
+	n++;
+};
+
+template <typename T, int maxsize>
+void Container <T, maxsize>::Delete1(int idx)
+{
+	if (isEmpty())
+	{
+		throw Exception("Your container is empty");
+	}
+	arr[idx] = arr[n - 1];
+	n--;
+};
+
+template <typename T, int maxsize>
+void Container <T, maxsize>::Delete(T a)
+{
+	Delete1(Find(a));
+}
+
+template<typename T, int maxsize>
+T& Container<T, maxsize>::operator[](int i)
+{
+	if (i > maxsize)
+	{
+		throw Exception("Array bounds");
+	}
+	return arr[i];
+};
+
+template<typename T, int maxsize>
+const T& Container<T, maxsize>::operator[](int i) const
+{
+	if (i > maxsize)
+	{
+		throw Exception("Array bounds");
+	}
+	return arr[i];
+};
+
+//Специализация 
+template <typename T, int maxsize>
+class Container <T*, maxsize>
+{
+	T** arr;
+	int n;
+
+public:
+	Container();
+	Container(T**, int);
+	Container(const Container <T*, maxsize> &x);
+	~Container();
+	bool isFull()const;
+	bool isEmpty()const;
+	int Find(T* a)const;
+	void Add(T* a);
+	void Delete(T* a);
+	void Delete1(int);
+	T& operator[](int);
+	const T& operator[](int) const;
+
+	friend std::ostream& operator << (std::ostream & o, const Container& x)
+	{
+		for (int i = 0; i < x.n; i++)
+		{
+			cout « x[i] « " ";
+		}
+		return o;
+	}
+};
+
+template <typename T, int maxsize>
+Container <T*, maxsize>::Container()
+{
+	n = 0;
+	arr = new T*[maxsize];
+};
+
+template <typename T, int maxsize>
+Container <T*, maxsize>::Container(T** arr1, int x)
+{
+	n = x;
+	arr = new T*[maxsize];
+	for (int i = 0; i < n; i++)
+		arr[i] = new T(*(arr1[i]));
+};
+
+template <typename T, int maxsize>
+Container <T*, maxsize>::Container(const Container <T*, maxsize> &c)
+{
+	n = c.n;
+	arr = new T*[n];
+	for (int i = 0; i < n; i++)
+		arr[i] = new T(*(c.arr[i]));
+};
+
+template <typename T, int maxsize>
+Container <T*, maxsize>::~Container()
+{
+	n = 0;
+	delete[] arr;
+};
+
+template <typename T, int maxsize>
+bool Container <T*, maxsize>::isFull() const
+{
+	return n == maxsize;
+};
+
+template <typename T, int maxsize>
+bool Container <T*, maxsize>::isEmpty() const
+{
+	return n == 0;
+};
+
+template <typename T, int maxsize>
+int Container <T*, maxsize>::Find(T* a) const
+{
+	for (int i = 0; i < n; i++)
+		if (arr[i] == a)
+			return i;
+	return -1;
+};
+
+template <typename T, int maxsize>
+
+void Container <T*, maxsize>::Add(T* a)
+{
+	if (isFull())
+	{
+		throw Exception("Your container is full");
+	}
+	arr[n] = a;
+	n++;
+};
+
+template <typename T, int maxsize>
+void Container <T*, maxsize>::Delete1(int idx)
+{
+	if (isEmpty())
+	{
+		throw Exception("Your container is empty");
+	}
+	arr[idx] = arr[n - 1];
+	n--;
+};
+
+template <typename T, int maxsize>
+void Container <T*, maxsize>::Delete(T* a)
+{
+	Delete1(Find(a));
+}
+
+template<typename T, int maxsize>
+T& Container<T*, maxsize>::operator[](int i)
+{
+	if (i > maxsize)
+	{
+		throw Exception("Array bounds");
+	}
+	return *arr[i];
+};
+
+template<typename T, int maxsize>
+const T& Container<T*, maxsize>::operator[](int i) const
+{
+	if (i > maxsize)
+	{
+		throw Exception("Array bounds");
+	}
+	return *arr[i];
+};
+
+#endif
---
+++ ljutova_ts/Practice/Practice10/main.cpp
@@ -0,0 +1,102 @@
+﻿#include "Container.h" 
+#include "Exception.h" 
+
+void main()
+{
+	int arr1[5] = { 1, 2, 4, 7, 8 };
+	double arr2[7] = { 1.1, 2.2, 3.3, 4.4, 5.5, 6.6, 7.7 };
+
+	Container<int, 5> Cont1(arr1, 5);
+	Container<double, 7> Cont2(arr2, 7);
+	try
+	{
+		cout << "Container 1:" << Cont1 << endl;
+		cout << "Container 2:" << Cont2 << endl;
+	}
+	catch (Exception e)
+	{
+		e.Print();
+	}
+
+	try
+	{
+		Cont1.Delete(2);
+		cout << "Container 1 - num 2: " << Cont1 << endl;
+
+		Cont2.Delete(2.2);
+		cout << "Container 2 - num 2.2: " << Cont2 << endl;
+	}
+	catch (Exception e)
+	{
+		e.Print();
+	}
+
+	try
+	{
+		Cont1.Add(3);
+		cout << "Container 1 + num 3: " << Cont1 << endl;
+
+		Cont2.Add(9.9);
+		cout << "Container 2 + num 9.9: " << Cont2 << endl;
+
+	}
+	catch (Exception e)
+	{
+		e.Print();
+	}
+
+	int *arr11[5];
+	double *arr22[7];
+
+	for (int i = 0; i < 5; i++)
+		arr11[i] = &arr1[i];
+
+	for (int i = 0; i < 7; i++)
+		arr22[i] = &arr2[i];
+
+	Container<int*, 5> Cont11(arr11, 5);
+	Container<double*, 7> Cont22(arr22, 7);
+	try
+	{
+		cout << "Container 1`:" << Cont11 << endl;
+		cout << "Container 2`:" << Cont22 << endl;
+	}
+	catch (Exception e)
+	{
+		e.Print();
+	}
+
+	try
+	{
+		double a = 0.1;
+		double *a1 = &a;
+
+		Cont22.Add(a1);
+		cout << "Container 2 + num 9.9: " << Cont22 << endl;
+
+		int b = 9;
+		int *b1 = &b;
+		Cont11.Add(b1);
+		cout << "Container 1 + num 3: " << Cont11 << endl;
+	}
+	catch (Exception e)
+	{
+		e.Print();
+	}
+
+	try
+	{
+
+		Cont11.Delete1(1);
+		cout <<  "Container 1 - element with Index 1: " << Cont11 << endl;
+
+
+		Cont22.Delete1(1);
+		cout << "Container 2 - element ith Index 1: " << Cont22 << endl;
+	}
+	catch (Exception e)
+	{
+		e.Print();
+	}
+
+}
