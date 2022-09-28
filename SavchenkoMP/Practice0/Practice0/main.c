5ce278c62d0a5f4d6f7637f80599fd52fdef2992
MaxikGuy <S_Maksssim@mail.ru>
2022-09-28 17:49:05 +0300

    Practice0

---
+++ SavchenkoMP/Practice0/Practice0/main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <math.h>
+#include <clocale>
+
+void main() {
+
+	setlocale(LC_TYPE, "Russian");
+	float x1, y1, r1;
+	float x2, y2, r2;
+	float sqr_x, sqr_y;
+	
+	// Вводим x1, y1, r1 и x2, y2, r2:
+	scanf_s("%f %f %f %f %f %f", &x1, &y1, &r1, &x2, &y2, &r2);
+	sqr_x = (x1 - x2) * (x1 - x2);
+	sqr_y = (y1 - y2) * (x1 - y2);
+
+
+	if (x1 == x2 && y1 == y2 && r1 == r2) 
+		printf("1 Круги дгур на друге");
+	if (sqrt(sqr_x + sqr_y) == (r1 + r2))
+		printf("2 Круги пересекаются в 1 точке");
+	if (sqrt(sqr_x + sqr_y) >= (r1 + r2))
+		printf("3 Круги не пересекаются");
+
+}
