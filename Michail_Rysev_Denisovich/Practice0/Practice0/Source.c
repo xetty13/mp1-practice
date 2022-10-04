8cc7d003579a87b4d353c7c8682201eeaf341c1b
MishaRysev <r0botbob@yandex.ru>
2022-10-04 21:45:14 +0300

    translated to english

--- Michail_Rysev_Denisovich/Practice0/Practice0/Source.c
+++ Michail_Rysev_Denisovich/Practice0/Practice0/Source.c
@@ -9,32 +9,34 @@ int main() {
 	float x2, y2, r2;
 	float d;
 	
-	printf("Введите координаты центра и радиус первой окржности: ");
+	printf("Enter the coordinates of the center and the radius of the first circle: ");
 	scanf("%f %f %f", &x1, &y1, &r1);
-	printf("Введите координаты центра и радиус второй окружности: ");
+	printf("Enter the coordinates of the center and the radius of the second circle: ");
 	scanf("%f %f %f", &x2, &y2, &r2);
 	
 	d = sqrt(pow(x1 - x2, 2) + pow(y1 - y2, 2));
 
 	if (d == 0) {
-		if (r2 == r1) printf("Окружности совпадают ");
-		else printf("Одна окружность лежит внутри другой ");
+		if (r2 == r1) 
+			printf("Circles match ");
+		else 
+			printf("One circle lies inside another ");
 	}
 
 	else if (d < abs(r1 - r2))
-		printf("Одна окружность лежит внутри другой ");
+		printf("One circle lies inside another ");
 
 	else if (d == abs(r1 - r2))
-		printf("Окружности касаются друг друга изнутри ");
+		printf("The circles touch each other from the inside ");
 
 	else if (d > abs(r1 - r2) && d < r1 + r2)
-		printf("Окружности пересекаются ");
+		printf("Circles intersect ");
 
 	else if (d == r1 + r2)
-		printf("Окружности касаются друг друга снаружи ");
+		printf("Circles touch each other on the outside ");
 
 	else
-		printf("Окружности лежат разделно друг от друга ");
+		printf("The circles lie apart from each other ");
 
 	return 0;
 }
