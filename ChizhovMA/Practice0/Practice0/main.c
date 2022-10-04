b38a4d40e37c40ff0bd98c111bfb3a14172e1838
MaximChizhov <maxim070205@gmail.com>
2022-10-04 16:17:12 +0300

    Circles

--- ChizhovMA/Practice0/Practice0/main.c
+++ ChizhovMA/Practice0/Practice0/main.c
@@ -5,32 +5,43 @@
 
 int main()
 {
+	setlocale(LC_ALL, "Rus");
 	float x1, y1, r1, x2, y2, r2;
 	printf("Введите координаты и радиус первой окружности");
 	scanf("%f%f%f", &x1, &y1, &r1);
 	printf("Введите координаты и радиус второй окружности");
 	scanf("%f%f%f", &x2, &y2, &r2);
-	setlocale(LC_CTYPE, "Russian");
+
 
 	if ((x1 == x2) && (y1 == y2) && (r1 == r2))
 	{
 		printf("Окружности равны");
+		return 1;
 	}
 	if (x1 == x2 && y1 == y2 && r1 > r2)
 	{
 		printf("Вторая окружность вписана впервую");
+		return 1;
 	}
 	if (x1 == x2 && y1 == y2 && r1 < r2)
 	{
 		printf("Первая окружность вписана в первую");
+		return 1;
 	}
 	if (sqrtf((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2)) == (r1 + r2))
 	{
 		printf("Окружности имеют одну общую точку");
+		return 1;
 	}
-	if (sqrtf((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2 )) >= (r1 + r2))
+	if (sqrtf((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2)) >= (r1 + r2))
 	{
 		printf("Окружности не имеют общих точек");
+		return 1;
+	}
+	else
+	{
+		printf("Окружности пресекаются в двух точках");
+		return 1;
 	}
 	return 0;
 }
