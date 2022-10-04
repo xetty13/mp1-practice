ccb79cc912b7ae33bf02218370a3ca6fbb9b8305
Arseny-Korobeinikov <arsenikorob@gmail.com>
2022-10-04 19:48:08 +0300

    English

--- KorobeinikovAP/Project0/Project0/Source.c
+++ KorobeinikovAP/Project0/Project0/Source.c
@@ -1,46 +1,44 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <math.h>
-#include <locale.h>
 
 int main()
 {
-	setlocale(LC_ALL, "Rus"); 
-	float x1, x2, y1, y2, R1, R2, S, tmp;
-	printf("Введите координату x1 ");
+	float x1, x2, y1, y2, R1, R2, S;
+	printf("Input x1 ");
 	scanf("%f", &x1);
-	printf("Введите координату y1 ");
+	printf("Input y1 ");
 	scanf("%f", &y1);
-	printf("Введите R1 ");
+	printf("Input R1 ");
 	scanf("%f", &R1);
-	printf("Введите координату x2 ");
+	printf("Input x2 ");
 	scanf("%f", &x2);
-	printf("Введите координату y2 ");
+	printf("Input y2 ");
 	scanf("%f", &y2);
-	printf("Введите R2 ");
+	printf("Input R2 ");
 	scanf("%f", &R2);
 	S = sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
 	if (S > R1 + R2) {
-		printf("Окружности не пересекаются ");
+		printf("The circle does not intersect ");
 		return 0;
 	}
 	if (S == R1 + R2) {
-		printf("Внешнее касание окружностей ");
+		printf("External touch of circles ");
 		return 0;
 	}
 	if ((S < R1 + R2) && (S > abs(R1 - R2))) {
-		printf("Окружности пересекаются ");
+		printf("The circles intersect ");
 		return 0;
 	}
 	if ((S == abs(R1 - R2)) && (x1 != x2)) {
-		printf("Внутреннее касание окружностей ");
+		printf("Inner touch of circles ");
 		return 0;
 	}
 	if ((S == 0) && (R1 == R2)) {
-		printf("Окружности совпадают ");
+		printf("The circles are equal ");
 		return 0;
 	}
-	if (R1>R2) printf("Вторая окружность лежит внутри первой ");
-	else printf("Первая окружности лежит внутри второй ");
+	if (R1>R2) printf("The second circle is inside the first one");
+	else printf("The first circle is inside the second ");
 	return 0;
 }
