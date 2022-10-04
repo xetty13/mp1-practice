af71232736fb5e80ab07049f953a3f234bded8ae
Tanya-Yasakova <yasakova_04@mail.ru>
2022-10-04 16:08:39 +0300

    fix file

--- YasakovaTE/Practice0/Practice0/Source.c
+++ YasakovaTE/Practice0/Practice0/Source.c
@@ -1,5 +1,39 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-void main()
+#include <math.h>
+#include <locale.h>
+int main()
 {
-
+	setlocale(LC_ALL, "Russian");
+	float x1, y1, r1, x2, y2, r2, d;
+	printf("Введите координаты центра и радиус окружности номер 1: ");
+	scanf(" %f %f %f", &x1, &y1, &r1);
+	printf("Введите координаты центра и радиус окружности номер 2: ");
+	scanf(" %f %f %f", &x2, &y2, &r2);
+	d = sqrt(x2 * x2 - 2 * x2 * x1 + x1 * x1 + y2 * y2 - 2 * y1 * y2 + y1 * y1);
+	if (d > r1 + r2)
+	{
+		printf("Окружности не касаются.");
+	}
+	else if ((r1 == r2) && (d == 0))
+	{
+		printf("Окружности совпадают.");
+	}
+	else if (d < abs(r1 - r2))
+	{
+		printf("Окружность с меньшим радиусом лежит внутри большой.");
+	}
+	else if (d == abs(r1 - r2))
+	{
+		printf("Внутреннее касание окружностей.");
+	}
+	else if (d == r1 + r2)
+	{
+		printf("Внешнее касание окружностей.");
+	}
+	else if ((abs(r1 - r2) < d) && (d < r1 + r2))
+	{
+		printf("Окружности пересекаются.");
+	}
+	return 0;
 }
