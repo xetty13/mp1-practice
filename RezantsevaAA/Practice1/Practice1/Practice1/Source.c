af6ecdc9f181110eeee3a3f2a2bed14f75a6c6c6
AnastasiaRezantseva <rezantseva.nastya49@gmail.com>
2022-10-05 17:48:59 +0300

    new faile

--- RezantsevaAA/Practice1/Practice1/Practice1/Source.c
+++ RezantsevaAA/Practice1/Practice1/Practice1/Source.c
@@ -1,4 +1,33 @@
-void main()
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <math.h>
+int main()
 {
+	float h, w, d; 
+	float density_wood = 640;
+	float density_DSP = 800;
+	float density_DVP = 750;
 
+	printf("¬ведите высоту шкафа h");
+	scanf(" %f",&h);
+	if ((h < 180) || (h > 220))
+	{
+		printf("некорректные данные, допустипые значени€ 180 mm < h < 220 mm");
+		return 0;
+	}
+	printf("¬ведите ширину шкафа w");
+	scanf(" %f",&w);
+	if ((w < 80) || (w > 120))
+	{
+	printf("некорректные данные, допустипые значени€ 80 mm < w < 120 mm");
+	return 0;
+	}
+	printf("¬ведите глубину шкафа d");
+	scanf(" %f", &d);
+	if ((d < 50) || (d > 90))
+	{
+	 printf("некорректные данные, допустипые значени€ 50 mm < d < 90 mm");
+	 return 0;
+	}
+	
 }
