5633851f35a694ca8732b4d8b8b77530c093d5c8
Tanya-Yasakova <yasakova_04@mail.ru>
2022-10-05 15:03:18 +0300

    fix file

--- YasakovaTE/Practice0/Practice0/Source.c
+++ YasakovaTE/Practice0/Practice0/Source.c
@@ -1,39 +1,37 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <math.h>
-#include <locale.h>
 int main()
 {
-	setlocale(LC_ALL, "Russian");
 	float x1, y1, r1, x2, y2, r2, d;
-	printf("Введите координаты центра и радиус окружности номер 1: ");
+	printf("Enter the center coordinates and radius of the first circle: ");
 	scanf(" %f %f %f", &x1, &y1, &r1);
-	printf("Введите координаты центра и радиус окружности номер 2: ");
+	printf("Enter the center coordinates and radius of the second circle: ");
 	scanf(" %f %f %f", &x2, &y2, &r2);
 	d = sqrt(x2 * x2 - 2 * x2 * x1 + x1 * x1 + y2 * y2 - 2 * y1 * y2 + y1 * y1);
 	if (d > r1 + r2)
 	{
-		printf("Окружности не касаются.");
+		printf("The circles do not intersect.");
 	}
 	else if ((r1 == r2) && (d == 0))
 	{
-		printf("Окружности совпадают.");
+		printf("The circles match.");
 	}
 	else if (d < abs(r1 - r2))
 	{
-		printf("Окружность с меньшим радиусом лежит внутри большой.");
+		printf("A circle with a small radius lies in a larger circle.");
 	}
 	else if (d == abs(r1 - r2))
 	{
-		printf("Внутреннее касание окружностей.");
+		printf("Internal tangency of circles.");
 	}
 	else if (d == r1 + r2)
 	{
-		printf("Внешнее касание окружностей.");
+		printf("External tangency of circles.");
 	}
 	else if ((abs(r1 - r2) < d) && (d < r1 + r2))
 	{
-		printf("Окружности пересекаются.");
+		printf("The circles intersect.");
 	}
 	return 0;
 }
