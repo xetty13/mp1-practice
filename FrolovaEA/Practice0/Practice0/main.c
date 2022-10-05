f7f93015750e48acf5f5c995ecba34a525cdce9a
ElizavetaFrolova <liza1827frol@gmail.com>
2022-10-06 01:43:48 +0300

    practice

--- FrolovaEA/Practice0/Practice0/main.c
+++ FrolovaEA/Practice0/Practice0/main.c
@@ -8,57 +8,57 @@ int main() {
 
 	float x1, x2, y1, y2, r1, r2, dl;
 
-	printf("¬ведите x1: ");
+	printf("Enter x1: ");
 	scanf_s("%f", &x1);
 
-	printf("¬ведите x2: ");
+	printf("Enter x2: ");
 	scanf_s("%f", &x2);
 
-	printf("¬ведите y1: ");
+	printf("Enter y1: ");
 	scanf_s("%f", &y1);
 
-	printf("¬ведите y2: ");
+	printf("Enter y2: ");
 	scanf_s("%f", &y2);
 
-	printf("¬ведите r1: ");
+	printf("Enter r1: ");
 	scanf_s("%f", &r1);
 
-	printf("¬ведите r2: ");
+	printf("Enter r2: ");
 	scanf_s("%f", &r2);
 
 	dl = sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
 
 	if (dl == r1 + r2)
 	{
-		printf("ќкружности касаютс€ друг друга");
+		printf("The circles touch each other (external touch)");
 		return 0;
 	}
 
 	if (dl > r1 + r2)
 	{
-		printf("ќкружности не касаютс€ друг друга (внешнее касание)");
+		printf("The circles do not touch each other ");
 		return 0;
 	}
 
 	if ((x1 == x2) && (y1 == y2) && (r1 == r2))
 	{
-		printf("ќкружности совпадают");
+		printf("The circles match");
 		return 0;
 	}
 
 	if ((dl + r1 == r2) || (dl + r2 == r1))
 	{
-		printf("ќкружности касаютс€ (внутреннее касание)");
+		printf("The circles touch each other (inner touch)");
 		return 0;
 	}
 
 	if (abs(x1 - x2) < r1 || abs(x1 - x2) < r2)
 	{
-		printf("ќдна окружность внутри другой");
+		printf("One circle inside another");
 		return 0;
 	}
 
-	printf("ќкружности имеют две общие точки");
+	printf("Circles have two points in common");
 	return 0;
 
 
