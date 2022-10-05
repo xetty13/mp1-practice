201ab78999ff988f01c12a5853f70eda64e2a58f
Kirius257 <holin.kirill@yandex.ru>
2022-10-05 22:52:07 +0300

    Initial structure

--- KholinKI/Practice0/Project/Circles.c
+++ KholinKI/Practice0/Project/Circles.c
@@ -2,52 +2,54 @@
 #include <stdio.h>
 #include <math.h>
 #include <conio.h>
-#include <clocale>
+#include <locale.h>
 
 
 int main()
 {
-	SetLocale(LC_CTYPE, "Russian");
-	float xc1, yc1, xc2, yc2, xr1, yr1,xr2,yr2,s,r1,r2;
+	setlocale(LC_CTYPE, "Russian");
+	float xc1, yc1, xc2, yc2, xr1, yr1,xr2,yr2,r1,r2,sc,d;
 	printf("Введите координаты центра первой окружности: ");
-	scanf("&f &f", &xc1, &yc1);
+	scanf("%f %f", &xc1, &yc1);
 	printf("Введите координаты центра второй окружности: ");
-	scanf("&f &f", &xc2, &yc2);
+	scanf("%f %f", &xc2, &yc2);
 	printf("Введите координаты радиуса первой окружности: ");
-	scanf("&f &f", &xc1, &yc1);
+	scanf("%f %f", &xr1, &yr1);
 	printf("Введите координаты радиуса второй окружности: ");
-	scanf("&f &f", &xc2, &yc2);
-	s = sqrt(pow(xr2 - xr1, 2) + pow(yr2 - yr1, 2));
+	scanf("%f %f", &xr2, &yr2);
 	r1 = sqrt(pow(xr1 - xc1, 2) + pow(yc1 - yr1, 2));
 	r2 = sqrt(pow(xr2 - xc2, 2) + pow(yr2 - yc2, 2));
+	sc = sqrt(pow(xc2 - xc1, 2) + pow(yc2 - yc1, 2));
+	d = sc - (r1 + r2);
 
-	if (xc1 == xc2 && yc1 == yc2)
+	if (sc == 0 && r1 == r2)
 	{
 		printf("Окружности совпадают");
 	}
 
-	if (s == 0)
+	else if (d== 0)
 	{
 		printf("Окружности соприкасаются");
 	}
 
-	if (s > 0)
+	else if (d > 0)
 	{
 		printf("Окружности удалены друг от друга");
 	}
 
-	if (xc1 == xc2 && yc1 == yc2 && abs(r1 - r2) > 0)
+	else if (sc < abs(r1 - r2))
 	{
 		printf("Одна окружность внутри другой");
 	}
 
-	if ()
+	else if (sc == abs(r1-r2))
 	{
 		printf("Окружности имеют одну общую точку");
 	}
-	else
+	else 
 	{
 		printf("Окружности пересекаются в двух точках");
 	}
+	getchar();
 	return 0;
 }
