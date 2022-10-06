e6aad2b84cd1583b66b83cb111c17d601647a409
sonya-shulakova <sofyashulakova22@gmail.com>
2022-10-06 17:52:13 +0300

    Initial structure

---
+++ ShulakovaSD/Practice1/Practice1/mass of wardrobe.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+int main()
+{
+	double h, w, d, bw, sw, uw, shelves, doors, m;
+	int n = 0;
+	printf("enter height, width, depth (in cm): ");
+	scanf("%lf %lf %lf", &h, &w, &d);
+	if ((h < 180) || (h > 220) || (w < 80) || (w > 120) || (d < 50) || (d > 90))
+	{
+		printf("error");
+		return 1;
+	}
+	while (h >= 40)
+	{
+		h = h - 40;
+		n++;
+	}
+	h = h * 0.01;
+	w = w * 0.01;
+	d = d * 0.01;
+	bw = h * w * 0.005 * 800;
+	sw = h * d * 0.015 * 650 * 2;
+	uw = w * d * 0.015 * 650 * 2;
+	doors = h * w * 0.01 * 550;
+	shelves = d * (w - 0.015 - 0.015) * 0.015 * 650 * n;
+	m = bw + sw + uw + doors + shelves;
+	printf("mass of wardrobe is %lf", "kg\n");
+	return 0;
+}
