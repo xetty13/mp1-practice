7e29cedc86503ea6c86af101ff5e233e52d81205
Kirius257 <holin.kirill@yandex.ru>
2022-10-07 08:16:08 +0300

    Initial structure

--- KholinKI/Practice0/Project/Circles.c
+++ KholinKI/Practice0/Project/Circles.c
@@ -9,13 +9,13 @@ int main()
 {
 	setlocale(LC_CTYPE, "Russian");
 	float xc1, yc1, xc2, yc2, xr1, yr1,xr2,yr2,r1,r2,sc,d;
-	printf("Введите координаты центра первой окружности: ");
+	printf("Enter the coordinates of the center of the first circle: ");
 	scanf("%f %f", &xc1, &yc1);
-	printf("Введите координаты центра второй окружности: ");
+	printf("Enter the coordinates of the center of the second circle: ");
 	scanf("%f %f", &xc2, &yc2);
-	printf("Введите координаты радиуса первой окружности: ");
+	printf("Enter the coordinates of the radius of the first circle: ");
 	scanf("%f %f", &xr1, &yr1);
-	printf("Введите координаты радиуса второй окружности: ");
+	printf("Enter the coordinates of the radius of the second circle: ");
 	scanf("%f %f", &xr2, &yr2);
 	r1 = sqrt(pow(xr1 - xc1, 2) + pow(yc1 - yr1, 2));
 	r2 = sqrt(pow(xr2 - xc2, 2) + pow(yr2 - yc2, 2));
@@ -24,31 +24,31 @@ int main()
 
 	if (sc == 0 && r1 == r2)
 	{
-		printf("Окружности совпадают");
+		printf("the circles are congruent");
 	}
 
 	else if (d== 0)
 	{
-		printf("Окружности соприкасаются");
+		printf("the circles are touching");
 	}
 
 	else if (d > 0)
 	{
-		printf("Окружности удалены друг от друга");
+		printf("The circles are distant from each other");
 	}
 
 	else if (sc < abs(r1 - r2))
 	{
-		printf("Одна окружность внутри другой");
+		printf("One circle inside another");
 	}
 
 	else if (sc == abs(r1-r2))
 	{
-		printf("Окружности имеют одну общую точку");
+		printf("The circles have one common point");
 	}
 	else 
 	{
-		printf("Окружности пересекаются в двух точках");
+		printf("The circles intersect at two points");
 	}
 	getchar();
 	return 0;
