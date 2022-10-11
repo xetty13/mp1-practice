7b662cdf376ab3b445606236bbf9a661684508fa
L4vanda <113257135+MishaRysev@users.noreply.github.com>
2022-10-11 19:40:31 +0300

    fix

--- Michail_Rysev_Denisovich/Practice1/Practice1/Source.c
+++ Michail_Rysev_Denisovich/Practice1/Practice1/Source.c
@@ -2,7 +2,7 @@
 
 int main() {
 	float h, d, w;
-	float dvp = 0.5, dsp = 1.5; // 800 kg/m3 550 kg/m3
+	float dvp = 0.5, dsp = 1.5;
 
 	printf("Enter the height, width and depth of the cabinet ");
 	scanf_s("%f %f %f", &h, &w, &d);
@@ -13,16 +13,16 @@ int main() {
 		return 0;
 	}
 
-	int k = h / 40; // êîë-âî ïîëîê
-	float m1 = k * d * w * dsp * 0.55; //ìàññà âñåõ ïîëîê
+	int k = h / 40;
+	float m1 = k * d * w * dsp * 0.55;
 
-	float m2 = h * w * dvp * 0.8; //ìàññà çàäíåé ñòåíêè
+	float m2 = h * w * dvp * 0.8;
 
-	float m3 = 2 * h * d * dsp * 0.55; //ìàññà áîêîâèí
+	float m3 = 2 * h * d * dsp * 0.55;
 
-	float m4 = 2 * w * d * dsp * 0.55; //ìàññà âåðõíåé è íèæíåé íàêëàäîê
+	float m4 = 2 * w * d * dsp * 0.55;
 
-	float m5 = w * h * 1.5; //äîïóñòèì ïëîòíîñòü äðåâèñèíà ðàâíà 1500 êã/ì3
+	float m5 = w * h * 1.5;
 
 	float m = (m1 + m2 + m3 + m4 + m5) / 1000;
 
