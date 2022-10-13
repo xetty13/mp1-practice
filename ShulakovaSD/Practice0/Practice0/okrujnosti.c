eeb0a070aa4b6aa2661e81d488e2114e8cf229d1
sonya-shulakova <sofyashulakova22@gmail.com>
2022-10-13 22:18:27 +0300

    Fixed

--- ShulakovaSD/Practice0/Practice0/okrujnosti.c
+++ ShulakovaSD/Practice0/Practice0/okrujnosti.c
@@ -1,17 +1,35 @@
 #include<stdio.h>
 #include<math.h>
+
 void main()
 {
-	double x1, x2, y1, y2, r1, r2, d;
-printf("Enter first circle characteristics: ");
-scanf_s("%lf %lf %lf", &x1, &y1, &r1);
-printf("Enter second circle characteristics: ");
-scanf_s("%f %f %f", &x2, &y2, &r2);
-d = sqrt(x2 * x2 - 2 * x2 * x1 + x1 * x1 + y2 * y2 - 2 * y1 * y2 + y1 * y1);
-if (d > (r1 + r2)) printf("Circles don't have common points");
-if (d == (r1 + r2)) printf("Circles touch externally");
-if (d == fabs(r1 - r2)) printf("Circle2 is inside circle1 with a touch");
-if ((d < fabs(r1 - r2)) && (d > 0)) printf("Circle2 is inside circle1 without a touch");
-if ((d < (r1 + r2)) && (d > (r1 - r2))) printf("Circles touch at two points");
-if ((r1 == r2) && (d == 0)) printf("Circles are equal");
+	float x1, x2, y1, y2, r1, r2, d;
+	printf("Enter first circle characteristics: ");
+	scanf_s("%f %f %f", &x1, &y1, &r1);
+	printf("Enter second circle characteristics: ");
+	scanf_s("%f %f %f", &x2, &y2, &r2);
+	d = sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
+	if (d == r1 + r2){
+		printf("Circles touch externally");
+		return 0;
+	}
+	if (d > r1 + r2) {
+		printf("Circles don't have common points");
+		return 0;
+	}
+	if ((d + r1 == r2) || (d + r2 == r1)) {
+		printf("Circle is inside circle with a touch");
+		return 0;
+	}
+	if (abs(x1-x2) < r1 || abs(x1-x2) < r2) {
+		printf("One circle inside another");
+		return 0;
+	}
+	if ((r1 == r2) && (y1 == y2) && (r1 == r2)) {
+		printf("Circles are equal");
+		return 0;
+	}
+	printf("Circles have two points in common");
+	return 0;
+
 }
