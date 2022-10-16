153a3841386ac39ead9ec934d55da79d45f9b8bd
maxim-haruspexxx-mikhaylov <maximmikhaylov00@gmail.com>
2022-10-16 21:00:53 +0300

    first_mode_v2

--- MikhaylovMA/Practice2/Practice2/pr2.c
+++ MikhaylovMA/Practice2/Practice2/pr2.c
@@ -6,7 +6,7 @@
 int main() {
 	char mode;
 	do {
-		printf("Choose game mode: \n1 - you guess number\n2 - you choose number\n");
+		printf("Choose game mode: \n1 - you guess number\n2 - you choose number \n");
 		scanf("%c", &mode);
 	} while (mode != '1' && mode != '2');
 
@@ -15,18 +15,26 @@ int main() {
 		srand((unsigned int)time(NULL));
 		answer = rand() % 1000;
 
-		printf("I chose the number. Try to guess it! \n");
-		printf("(%hd)\n", answer);
+		printf("I chose the number. Try to guess it! (type '-11' for answer) \n");
 
 		while (1) {
 			printf("What's your guess? \n");
 			scanf("%hd", &cur_ans);
 
-			if (cur_ans < 0 || cur_ans > 1000) {
+			if (cur_ans == -11) {
+				printf("%hd \n", answer);
+			}
+			else if (cur_ans < 0 || cur_ans > 1000) {
 				printf("Wrong input. \n");
 			}
-			else if (cur_ans != answer) {
-				printf("That's not my number. Try again. (Tries: %hd) \n", try_count);
+			else if (cur_ans < answer) {
+
+				printf("This number < my number. Try again. (Tries: %hd) \n", try_count);
+				try_count++;
+			}
+			else if (cur_ans > answer) {
+
+				printf("This number > my number. Try again. (Tries: %hd) \n", try_count);
 				try_count++;
 			}
 			else if (cur_ans == answer) {
@@ -34,6 +42,7 @@ int main() {
 				return 0;
 			}
 		}
+
 	}
 	return 0;
 }
