caa6226a54ecc9eaffd5694d8d09b710bf43e234
Fest1veNapkin <arseniy.mironov.01@mail.ru>
2022-10-16 22:25:07 +0300

    Practice0

--- MironovAI/Practice0/Practice0/pr_0.c
+++ MironovAI/Practice0/Practice0/pr_0.c
@@ -6,13 +6,13 @@ int main()
 {
 	double x1, dl, x2, y1, y2, r1, r2;
 
-	setlocale(LC_ALL, "rus");
+	setlocale(LC_ALL, "Russian");
 	printf("Input x1: ");
 	scanf_s("%lf", &x1);
 
 	printf("Input y1: ");
 	scanf_s("%lf", &y1);
-		
+
 	printf("Input r1: ");
 	scanf_s("%lf", &r1);
 
@@ -46,18 +46,24 @@ int main()
 		return;
 	}
 
-	if (abs(x1 - x2) < r1 || abs(x1 - x2) < r2);
-	{
-		printf("Одна окружность внутри другой");
-		return;
+	if ((sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2)) < (r1 - r2)) || (sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2)) > (r1 - r2))) {
+
+		printf("Одна окружность внутри второй, не пересекаются");
+		return 0;
+	}
+	if (sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2)) == (r2 - r1)) {
+		printf("Одна окружность касается внутри второй");
+		return 0;
+	}
+	if (sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2)) > (r1 + r2)) {
+		printf("Окружности не пересекаются");
+		return 0;
+	}
+	if (sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2)) < (r2 + r1) && (sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2)) > (r1 - r2))){
+		printf("Окружности пересекаются в 2 точках");
+		return 0;
 	}
 
-	if (((r1 > r2) && (abs(x1 - x2) + r2 == r2)));
-	{
-		printf("Одна окружность касается внутри другой");
-		return;
-	}
-
-	printf("Окружности имеют 2 точки пресечения");
+	printf("Вы нашли баг в программе, напишите нам пожалуйста :( ");
 	return 0;
 }
