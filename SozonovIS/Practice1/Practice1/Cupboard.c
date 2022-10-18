905f626a9e599b55def27fec869767c1ac73b434
Sozonov_Ilushka <113029719+sozozzya@users.noreply.github.com>
2022-10-18 17:42:53 +0300

    Update Cupboard.c

--- SozonovIS/Practice1/Practice1/Cupboard.c
+++ SozonovIS/Practice1/Practice1/Cupboard.c
@@ -1,28 +1,28 @@
 #include <stdio.h>
 int main()
 {
-	double h, w, d, m_z_st, m_bk, m_kr, m_dvr, h_plk, m_plk = 0, m;
-	printf("Введите высоту задней стенки h(см.)=");
-	scanf_s("%lf", &h);
-	printf("Введите ширину задней стенки w(см.)=");
-	scanf_s("%lf", &w);
-	printf("Введите глубину боковины d(см.)=");
-	scanf_s("%lf", &d);
+	double h, w, d, backwall, sidewall, lids, doors, shelves_height, shelves = 0, m;
+	printf("Enter the back wall height h(sm.)=");
+	scanf("%lf", &h);
+	printf("Enter back wall width w(sm.)=");
+	scanf("%lf", &w);
+	printf("Enter sidewall depth d(sm.)=");
+	scanf("%lf", &d);
 	if ((h < 180) || (h > 220) || (w < 80) || (w > 120) || (d < 50) || (d > 90))
 	{
-		printf("Неверно введенные данные");
+		printf("Invalid data entered");
 		return 1;
 	}
-	m_z_st = h * w * 0.5 * 0.8;
-	m_bk = 2 * h * d * 1.5 * 0.65;
-	m_kr = 2 * w * d * 1.5 * 0.65;
-	m_dvr = h * w * 0.55;
-	h_plk = 41.5;
-	while (h_plk < h)
+	backwall = h * w * 0.5 * 0.8;
+	sidewall = 2 * h * d * 1.5 * 0.65;
+	lids = 2 * w * d * 1.5 * 0.65;
+	doors = h * w * 0.55;
+	shelves_height = 41.5;
+	while (shelves_height < h)
 	{
-		m_plk = m_plk + d * (w - 3) * 1.5 * 0.65;
-		h_plk = h_plk + 41.5;
+		shelves += d * (w - 3) * 1.5 * 0.65;
+		shelves_height += 41.5;
 	}
-	printf("Общая масса шкафа(кг.)=%lf", (m_z_st + m_bk + m_kr + m_dvr + m_plk) / 1000);
+	printf("Total cabinet weight(kg.)=%lf", (backwall + sidewall + lids + doors + shelves) / 1000);
 	return 0;
 }
