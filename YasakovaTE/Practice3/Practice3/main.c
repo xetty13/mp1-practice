5e287c319ad07d908c899548d719de5721d3733c
Tanya-Yasakova <yasakova_04@mail.ru>
2022-10-19 17:51:01 +0300

    fix file

--- YasakovaTE/Practice3/Practice3/main.c
+++ YasakovaTE/Practice3/Practice3/main.c
@@ -1,5 +1,29 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-void main()
+#include <locale.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+int main()
 {
-
+	int len, i, a, number;
+    float g, k;
+    g = 10;
+    srand((unsigned int)time(NULL));
+	printf("Choose the length of the number (from 2 to 5): ");
+    do {
+        scanf(" %d", &len);
+        if ((len > 5) || (len < 2)) {
+            printf("Incorrect number entered. Try again: ");
+        }
+    } while ((len > 5) || (len < 2));
+    k = pow(g, len);
+    printf("%f", k);
+    number = 0;
+    for (i = 1; i < len; i++)
+    {
+        a = 1 + rand() % (1000000);
+        number = number + a * k;
+    }
+    printf("%d", number);
 }
