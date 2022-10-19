07437dd4cab1549898e2efdbf80f16c7e6881388
AnastasiaRezantseva <rezantseva.nastya49@gmail.com>
2022-10-19 17:52:54 +0300

    file

--- RezantsevaAA/Practice3/Practice3/Source.c
+++ RezantsevaAA/Practice3/Practice3/Source.c
@@ -1,7 +1,22 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+#include <time.h>
 #include <math.h>
 void main()
 {
+
+    srand(time(NULL));
+    int number[] = 0;
+	int n, element;
+	printf("Enter the number of digits in the number from 2 to 5 ");
+    do {
+        scanf("%d", &n);
+        if ((n > 5) || (n < 2)) {
+            printf("Invalid number entered\n");
+        }
+    } while ((n > 5) || (n< 2));
+    printf("%d", n);
+    number[1] = 1 + rand() % (10);
+    number[2] = rand() % (10)
 
 }
