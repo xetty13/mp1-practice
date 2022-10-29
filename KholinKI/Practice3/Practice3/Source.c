21d9e75415e1e3ccbb98ad0576412a9fb1a99d81
Kirius257 <holin.kirill@yandex.ru>
2022-10-29 03:02:29 +0300

    Changed

--- KholinKI/Practice3/Practice3/Source.c
+++ KholinKI/Practice3/Practice3/Source.c
@@ -1,28 +1,35 @@
+#define CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 #include <conio.h>
+#pragma warning(suppress : 4996)
+#pragma warning(suppress : 4996)
 #define N 5
-
+int mass[N];
 
 
 
 int main() {
-	int l, i, n = 0, multi, k, q, j = 0, m[N], mass[N],p=0,k1,k1_old= 0,unique=1;
+	int l, i, n = 0, multi, k, q, j, m[N], p = 0, unique=1,old_n;
 	srand(time(NULL));
 	printf("Input length of riddled number: \n");
 	scanf("%d", &l);
-	do{
+	do {
 		for (i = 2, q = 9.9, multi = 10; i <= 5; i++, multi *= 10) {
 			if (l == i) {
 				n = multi + rand() % (q * multi - multi + 1);
 				break;
 			}
 		}
+		old_n = n;
 		i = 0;
 		while (n != 0) {
 			k = n % 10;
 			mass[i] = k;
+			if (mass[i] < 0) {
+				mass[i] == 0;
+			}
 			n = n / 10;
 			i++;
 		}
@@ -38,12 +45,13 @@ int main() {
 			mass[i] = m[j];
 			j = j - 1;
 		}
+		unique = 1;
 		for (i = 0; i < N; i++) {
-			for (j = i + 1; j < N; j++) {
-				if (mass[0] == 0) {
+			for (j = i+1; j < N; j++) {
+				if (mass[i] == 0) {
 					break;
 				}
-				else if (mass[i] == mass[j]){
+				else if (mass[i] == mass[j]) {
 					unique = 0;
 					break;
 				}
@@ -52,13 +60,16 @@ int main() {
 				break;
 			}
 		}
-	} while (unique != 1);
+	} while (unique == 0);
 	printf("\n");
+
 	for (i = 0; i < N; i++) {
 			p = 10 * p + mass[i];
 		}
 	printf("Computer`s number: %d\n", p);
+
 	return 0;
 }
+
 
 
