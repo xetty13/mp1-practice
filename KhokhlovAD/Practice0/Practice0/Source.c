6c79650360a336399385f733bded399aa2b47a8d
pipidaster2004 <chebureknabite@gmail.com>
2022-10-30 12:20:48 +0300

    teper bez oshibok

--- KhokhlovAD/Practice0/Practice0/Source.c
+++ KhokhlovAD/Practice0/Practice0/Source.c
@@ -1,51 +1,27 @@
 #include <stdio.h>
 #include <math.h>
-int main()
-{
-	int x1, x2, y1, y2, r1, r2, max, min;
-	float d;
-	printf("x1=");
-	scanf_s("%d", &x1);
-	printf("y1=");
-	scanf_s("%d", &y1);
-	printf("r1=");
-	scanf_s("%d", &r1);
-	printf("x2=");
-	scanf_s("%d", &x2);
-	printf("y2=");
-	scanf_s("%d", &y2);
-	printf("r2=");
-	scanf_s("%d", &r2);
-	if (r1>r2)
-	{
-		max = r1;
-		min = r2;
-	}
-	else
-	{
-		max = r2;
-		min = r1;
-	}
-	d = sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
-	if (d>(r1+r2))
-	{
-		printf("ne_peresekautsa");   //не пересекаются
-	}
-	else if (d == (r1+r2))
-	{
-		printf("vneshnee kasanie");   //внешнее косание
-	}
-	else if ((d<(r1+r2))&&(d>max))
-	{
-		printf("peresekautsa");   //пересекаются
-	}
-	else if (d==(max-min))
-	{
-		printf("vnutrennee kasanie");   //внутреннее косание
-	}
-	else if (d<(max-min))
-	{
-		printf("1 okruj vnutri drugoi");  // одна окружность внутри другой и не косаются
-	}
-	else	printf("okrujnosti sovpadaut"); //окружности совпадают
+#include <locale.h>
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+	int x1, y1, r1, x2, y2, r2;
+	double distance;
+	printf("Введите координаты центра и радиус первой окружности: ");
+	scanf_s("%i%i%i", &x1, &y1, &r1);
+	printf("Введите координаты центра и радиус второй окружности: ");
+	scanf_s("%i%i%i", &x2, &y2, &r2);
+	distance = sqrt(pow(x1 - x2, 2) + pow(y1 - y2, 2));
+	if (x1 == x2 && y1 == y2 && r1 == r2)
+		printf("Совпадают полностью");
+	else if (distance < r1 - r2 || distance < r2 - r1)
+		printf("одна окружность внутри другой");
+	else if (distance == r1 + r2)
+		printf("Внешнее касание");
+	else if (distance == r1 - r2 || distance == r2 - r1)
+		printf("Внутреннее касание");
+	else if (distance > r1 + r2)
+		printf("Не пересекаются");
+	else if (distance < r1 + r2)
+		printf("Пересекаются в 2 точках");
+	return 0;
 }
