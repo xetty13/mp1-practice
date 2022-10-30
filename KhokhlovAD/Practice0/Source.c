38bd9e15dbac3c24b003dd74a09acc4794cbe014
pipidaster2004 <chebureknabite@gmail.com>
2022-10-30 12:27:47 +0300

    teper bez oshibok1

--- KhokhlovAD/Practice0/Source.c
+++ KhokhlovAD/Practice0/Source.c
@@ -7,14 +7,14 @@ int main() {
 	int x1, y1, r1, x2, y2, r2;
 	double distance;
 	printf("Введите координаты центра и радиус первой окружности: ");
-	scanf_s("%i%i%i", &x1, &y1, &r1);
+	scanf("%i%i%i", &x1, &y1, &r1);
 	printf("Введите координаты центра и радиус второй окружности: ");
-	scanf_s("%i%i%i", &x2, &y2, &r2);
+	scanf("%i%i%i", &x2, &y2, &r2);
 	distance = sqrt(pow(x1 - x2, 2) + pow(y1 - y2, 2));
 	if (x1 == x2 && y1 == y2 && r1 == r2)
 		printf("Совпадают полностью");
 	else if (distance < r1 - r2 || distance < r2 - r1)
-		printf("одна окружность внутри другой");
+		printf("Не пересекаются");
 	else if (distance == r1 + r2)
 		printf("Внешнее касание");
 	else if (distance == r1 - r2 || distance == r2 - r1)
