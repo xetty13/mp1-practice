fb91dd751458972a50f4be8e58ddac3132e9911c
Kirius257 <holin.kirill@yandex.ru>
2022-10-31 20:41:00 +0300

    Done Practice3

--- KholinKI/Practice3/Practice3/Source.c
+++ KholinKI/Practice3/Practice3/Source.c
@@ -72,55 +72,104 @@ int unique_number()
 	return p;
 }
 
-int Shchitar_of_cows_and_bulls(int pc_n, int user_n) {
-	int k, pc_mass[N], m_pc[N], i = 0, j, m_user[N], cows = 0, bulls = 0, user_mass[N], k1;
-	while (pc_n != 0 && user_n != 0) {
-		k = pc_n % 10;
-		k1 = user_n % 10;
-		pc_mass[i] = k;
-		user_mass[i] = k1;
-		user_n = user_n / 10;
-		pc_n = pc_n / 10;
-		i++;
-	}
-	for (i = 0; i < N; i++) {
-		m_pc[i] = pc_mass[i];
-		m_user[i] = user_mass[i];
-	}
-	for (i = 0, j = 4; i < 5; i++) {
-		pc_mass[i] = m_pc[j];
-		user_mass[i] = m_user[j];
-		j = j - 1;
-	}
-	for (i = 0; i < N; i++) {
-		for (j = 0; j < N; j++) {
-			if (pc_mass[i] < 0 && user_mass[j] < 0) {
-				break;
-			}
-			if (user_mass[j] == pc_mass[i] && j == i) {
-				cows += 1;
-			}
-			if ((user_mass[j]== pc_mass[i]) && j != i){
-				bulls += 1;
-			}
-		}
-	}
-	printf("%d ", cows);
-	printf("%d", bulls);
-	return cows, bulls;
-}
-
 int main() {
 	srand(time(NULL));
-	int pc_n,user_n,cows1=0,bulls=0;
+	int pc_n,user_n,cows=0,bulls=0, k, pc_mass[N], m_pc[N], i = 0, j, m_user[N], user_mass[N], k1,signal=1,kolvo=0,saved_pc_n,flag=0,i_saved=0,mark1=0,significanse=2,id=0;		
 	pc_n = unique_number();
-	printf("Computer`s number: %d\n", pc_n);
-	user_n = unique_number();
-	printf("User number: %d\n", user_n);
-	printf("\n");
-	cows1,bulls= Shchitar_of_cows_and_bulls(pc_n, user_n);
-	printf("Cows count: %d\n", cows1);
-	printf("Bulls count: %d\n", bulls);
+	printf("PC %d\n", pc_n);
+	do{ //*pobedniy do...while
+		do{ //* unikalnost chisla
+			printf("Input unique number: ");
+			scanf_s("%d", &user_n);
+			printf("\n");
+			i = 0;
+			saved_pc_n = pc_n;
+			kolvo = 0;
+			while (pc_n != 0 && user_n != 0) {
+				k = pc_n % 10;
+				k1 = user_n % 10;
+				pc_mass[i] = k;
+				user_mass[i] = k1;
+				user_n = user_n / 10;
+				pc_n = pc_n / 10;
+				i++;
+				kolvo++;
+				if ((pc_n != 0 && user_n == 0) || (pc_n == 0 && user_n != 0)) {
+					printf("Game exited with error 1");
+					return 1;
+				}
+			}
+			pc_n = saved_pc_n;
+			for (i = 0; i < N; i++) {
+				m_pc[i] = pc_mass[i];
+				m_user[i] = user_mass[i];
+			}
+			for (i = 0, j = 4; i < 5; i++) {
+				pc_mass[i] = m_pc[j];
+				user_mass[i] = m_user[j];
+				j = j - 1;
+			}
+			signal = 1;
+			for (i = 0; i < N; i++) {
+				for (j = i + 1; j < N; j++) {
+					if (user_mass[i] <= 0) {
+						break;
+					}
+					else if (user_mass[i] == user_mass[j]) {
+						signal = 0;
+						break;
+					}
+				}
+				if (signal == 0) {
+					for (i = 0; i < N; i++) {
+						user_mass[i] = 0;
+					}
+					break;
+				}
+			}
+		} while (signal == 0);
+		flag = 0;
+		for (i = 0; i < N; i++) {
+			for (j = 0; j < N; j++) {
+				if ((pc_mass[i] <= 0 && user_mass[j] <= 0 && (i == 0 && j == 0 || i == 1 && j==0 || i==2 && j==-0))) {
+					if (flag == 0) {
+						break;
+					}
+				}
+				if (pc_mass[i] >= 0 && user_mass[j] <= 0) {
+					if (flag == 0) {
+						j = i;
+						i_saved = j;
+						flag = 1;
+					}
+					if (mark1 == 0) {
+						j = i_saved;
+						mark1 = 1;
+					}
+				}
+				if (user_mass[j] == pc_mass[i] && j == i) {
+					bulls += 1;
+				}
+				if ((user_mass[j] == pc_mass[i]) && j != i) {
+					cows += 1;
+				}
+			}
+			mark1 = 0;
+		}
+		printf("Cows count: %d\n", cows);
+		printf("Bulls count: %d\n", bulls);
+		if (bulls != kolvo) {
+			cows = 0;
+			bulls = 0;
+			for (i = 0; i < N; i++) {
+				user_mass[i] = 0;
+			}
+			for (i = 0; i < N; i++) {
+				pc_mass[i] = 0;
+			}
+		}
+	} while (bulls != kolvo);
+	printf("Congratulations! You won!");
 	return 0;
 }
 
