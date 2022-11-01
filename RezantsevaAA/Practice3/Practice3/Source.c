428cd6543de9c61fb00245ad3552a0aea70aa03e
AnastasiaRezantseva <rezantseva.nastya49@gmail.com>
2022-11-01 17:41:48 +0300

    fix file

--- RezantsevaAA/Practice3/Practice3/Source.c
+++ RezantsevaAA/Practice3/Practice3/Source.c
@@ -2,21 +2,87 @@
 #include <stdio.h>
 #include <time.h>
 #include <math.h>
+#define N 5
 void main()
 {
-
     srand(time(NULL));
-    int number[] = 0;
-	int n, element;
+    int number[N] = { 0 };
+    int arr[N] = { 0 }; 
+    int tries = 0;
+    int n, i, raz, flag, j, r, p_num, cow,bull, proverka;
 	printf("Enter the number of digits in the number from 2 to 5 ");
     do {
         scanf("%d", &n);
         if ((n > 5) || (n < 2)) {
-            printf("Invalid number entered\n");
+            printf("Invalid number entered. Try again\n");
         }
     } while ((n > 5) || (n< 2));
-    printf("%d", n);
-    number[1] = 1 + rand() % (10);
-    number[2] = rand() % (10)
-
+    number[0] = 1 + rand() % (9);
+    r = 0;
+    flag = 1;
+    for (i = 1; i < n; i++)
+    {
+        flag = 1;
+        while (flag == 1) {
+            r = rand() % 10;
+            flag = 1;
+            for (j = 0; j < i; j++)
+            {
+                if (number[j] == r)
+                {
+                    flag = 0;
+                    break;
+                }
+            }
+            if (flag == 1)
+            {
+                number[i] = r;
+            }
+            break;
+        }
+     
+    }
+    cow = 0;
+    bull = 0;
+    proverka = pow(10, n - 1);
+    while(bull != n)
+    {
+        printf("Try to guess the number: ");
+        do {
+            scanf(" %d", &p_num);
+            if ((p_num < proverka) || (p_num >= proverka * 10)) {
+                printf("Incorrect number entered. Try again: ");
+            }
+        } while ((p_num < proverka) || (p_num >= proverka * 10));
+        i = n - 1;
+        while (p_num > 0)
+        {
+            arr[i] = p_num % 10;
+            p_num = p_num / 10;
+            i--;
+        }
+        bull = 0;
+        cow = 0;
+        for (i = 0; i < n; i++)
+        {
+            if (number[i] == arr[i])
+            {
+                bull++;
+            }
+        }
+        for (i = 0; i < n; i++)
+        {
+            for (j = 0; j < n; j++)
+            {
+                if (number[i] == arr[j])
+                {
+                    if (i != j)
+                        cow++;
+                }
+            }
+            tries++;
+        }
+        printf("Number of bulls: %d. Number of cows: %d\n", bull, cow);
+    }
+    printf("Congratulations! You guessed the number in %d attempts", tries);
 }
