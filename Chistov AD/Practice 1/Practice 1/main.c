5b81910c671a630274f7b3123dbc2751668eff86
Alexey2013 <112825972+Alexey2013@users.noreply.github.com>
2022-11-01 15:06:26 +0300

    Update main.c

--- Chistov AD/Practice 1/Practice 1/main.c
+++ Chistov AD/Practice 1/Practice 1/main.c
@@ -3,7 +3,7 @@
 
 int main()
 {
-	double h, w, d, m1, m2, m3, m4, m5, q, k = 0, M;
+	double h, w, d, back_wall, side_wall, upper_n_lower_wall, shelves, doors, q, k = 0, M;
 	printf("Enter cabinet height in cm ");
 	scanf("%lf", &h);
 	if ((h < 180) || (h > 220))
@@ -18,26 +18,25 @@ int main()
 		printf("Cabinet width is out of range ");
 		return 1;
 	}
-	printf("Enter cabinet depth in cm ");
+	printf("Enter cabinet depth in m ");
 	scanf("%lf", &d);
-	if ((d < 50) || (d > 90))
+	if ((d < 50) || (d >90))
 	{
 		printf("Cabinet depth value out of range");
 		return 1;
 	}
-	q = h-2;	//k=(int)(h/40.);
+	q = h - 2;	//k=(int)(h/40.);
 	while (q > 40)
 	{
 		q -= 40;
 		k++;
 	}
-	
-	m1 = (h * w * 0.5) * 0.8;
-	m2 = (h * d * 1.5) * 0.65;
-	m3 = (w * d * 1.5) * 0.65;
-	m4 = (h * w * 0.5 * 2) * 0.55;
-	m5 = ((w-3) *  1.5 * (d-0.5) * k) * 0.65;
-	M = m1 + 2 * m2 + 2 * m3 + 2 * m4 + m5;
-	printf("The weight of the cabinet is: %.3lf  kg", M/1000.);
+	back_wall = h * w * 0.005 * 800;
+	side_wall = (h - 3) * d * 0.015 * 650 * 2;
+	upper_n_lower_wall = w * d * 0.015 * 650 * 2;
+	shelves = d * (w - 3) * 0.015 * 650 * k;
+	doors = h * w * 0.01 * 550;
+	M = back_wall + side_wall + upper_n_lower_wall + shelves + doors;
+	printf("The weight of the cabinet is: %.3lf  kg", M/10000);
 	return 0;
 }
