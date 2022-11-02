7bc62c0c0e9c3ec5efec198f6b23f3ce9f53851b
Резанцева Анастасия Алексеевна <s22380341@unn.ru>
2022-11-02 17:48:48 +0300

    practice4

--- RezantsevaAA/Practice4/Practice4/Source.c
+++ RezantsevaAA/Practice4/Practice4/Source.c
@@ -1,4 +1,9 @@
-void main()
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <time.h>
+#include <math.h>
+int main()
 {
-
+	char barcode[10][4] = { "1001","1002","1003","1004","1005","1006","1007","1008","1009","1010" };
+	char products[10][15] = { "eggs","milk", "meat","tea","cheese","bread","pen","bottle of water", "crisps","honey" };
 }
