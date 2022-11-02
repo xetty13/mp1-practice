fbe3c2c9ed56798ff6d02a4affa18915a853759a
Arseny-Korobeinikov <arsenikorob@gmail.com>
2022-11-02 17:48:53 +0300

    File modification

--- KorobeinikovAP/Practice4/Practice4/Source.c
+++ KorobeinikovAP/Practice4/Practice4/Source.c
@@ -1,10 +1,9 @@
 
-
-
 #include <stdio.h>
 #include <string.h>
 
-
+char codes[10][4] = { "1234","2345","3456","4567","5678","6789","7891","8912","9123","9999" };
+char names[10][20] = { "salt","sugar","bread", "milk","eggs","tea","cheese","meat","honey","oil" };
 int main() {
 
 
