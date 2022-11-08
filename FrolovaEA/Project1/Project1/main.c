43d7bc69a995fb3543e77d73c9ba9e732701bd54
ElizavetaFrolova <liza1827frol@gmail.com>
2022-11-09 01:31:55 +0300

    structure

--- FrolovaEA/Project1/Project1/main.c
+++ FrolovaEA/Project1/Project1/main.c
@@ -1,86 +1,83 @@
-#define _CRT_SECURE_NO_WARNINGS
-#include <conio.h>
-#include <stdio.h>
-#include <ctype.h>
-#include <stdlib.h>
-#include <time.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+#include<conio.h>
+#include<ctype.h>
+#define N 5
+#define B 5
 
 
-/* функция проверяет на совпадение вводимые цифр(массив, длина массива, символ который надо сравнить)
+int main() {
+
+	int a[N], mycomp[B];
+	int b, size = 0, cows = 0, bulls = 0, digits = 0, buff = 0, no_equal = 0;
+
+	printf("enter the number of digits of the number(from 2 to 5) or q for quit ");
+	scanf_s("%d", &size);
+	//	printf("size= %d", size);
+	while ((size < 2) || (size > 5))
+	{
+		printf("enter the number of digits of the number");
+		scanf_s("%d", &size);
+		//
+	}
+	//	printf("size= %d", size);
 
 
-*/
-
-int is_valid(char a[5], int size, char c)
-{
-	int no = size, i;
-	for (i = 0; i < size; i++)
-		if (a[i] != c)
-			no--;
-	if (no == 0)
-		a[size] = c;
-	//	printf("no = %d, c = %c\n", no, c);
-	return !no;
-}
-int main()
-{
-	char my[5];
-	int bulls = 0, size = 0, cows = 0, digits = 0;
-	char numbers[10] = "0123456789", ch;
-	char enter[5], c = ' ';
-
-
-	// контроль ввода данных 
-	do {
-		printf("Enter digit number (from 2 to 5): ");
-		scanf("%d", &size);
-	} while (size < 2 || size > 5);
-
-
-	//	scanf_s("%d", &size);
-	//	printf("\n%c, %d ", c, size);
-
-	printf("\nEnter %d digits or q for quit\n", size);
-
-	// заполняем массив рандомными цифрами
-	srand((unsigned)time(NULL));
+	srand((unsigned int)time(NULL));
+	mycomp[0] = 1 + rand() % 9;
+	//	printf("%d\n", array[0]);
+	digits++;
 	while (digits < size) {
-		ch = numbers[rand() % 10];
-		if (is_valid(my, digits, ch)) {
+		buff = rand() % 10;
+		//		printf("buff=%d, digits=%d\n", buff, digits);
+		no_equal = digits;
+
+		for (int i = 0; i < digits; i++)
+			if (buff != mycomp[i]) no_equal--;
+
+		if (no_equal == 0) {
+			mycomp[digits] = buff;
 			digits++;
-			//			printf("\t%c", ch);
 		}
 	}
-	/*	printf("my number is ");
-		for (int i = 0; i < size; i++)
-			printf("%c", my[i]);
-		printf("\n");*/
-		//	printf("my number is %c%c%c%c%c%c\n",my[0],my[1],my[2],my[3],my[4],my[5]);
-	bulls = cows = digits = 0;
 
-	while (c != 'q' && c != 'Q')
+	//printf("\nEnter %d digits or q for quit\n", size);
+	for (int i = 0; i < size; i++)
+		printf("%d", mycomp[i]);
+	printf("\n");
+	printf("\nEnter %d digits or q for quit\n", size);
+	bulls = cows = digits = 0;
+	char c = ' ';
+
+	while (c != 'q' && c != 'Q') {
 		if (isdigit(c = _getch())) // контроль ввода данных isdigit (является ли десятичной цифрой)
-			if (is_valid(enter, digits, c))
-			{
-				printf("%c", c);
-				for (int i = 0; i < size; i++)
-					if (my[i] == c)
-						if (i == digits)
-							bulls++;
-						else
-							cows++;
-				digits++;
-				if (digits == size)
-				{
-					printf("\t\tbulls=%d cows=%d\n", bulls, cows);
-					if (bulls == size) {
-						printf("YOU WON!");
-						break;
-					}
-					bulls = cows = digits = 0;
-				}
+			//		if (is_valid(enter, digits, c))
+			//		{
+			printf("%c", c);
+
+		for (int i = 0; i < size; i++) {
+
+			if (mycomp[i] == c - 48) {
+				if (i == digits)
+					bulls++;
+				else
+					cows++;
 			}
-	printf("\npress any key\n");
-	_getch();
+		}
+		digits++;
+		if (digits == size)
+		{
+			printf("\t\tbulls=%d cows=%d\n", bulls, cows);
+			if (bulls == size) {
+				printf("YOU WON!");
+				break;
+			}
+			bulls = cows = digits = 0;
+		}
+	}
+
 	return 0;
 }
+
+
