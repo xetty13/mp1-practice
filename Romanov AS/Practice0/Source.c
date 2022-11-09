9a3352a4f0cdbb038c3e6ffaf87943da3ab20a97
andreyromanov04 <113029493+andreyromanov04@users.noreply.github.com>
2022-11-09 15:06:58 +0300

    okruzhnost

--- Romanov AS/Practice0/Source.c
+++ Romanov AS/Practice0/Source.c
@@ -8,11 +8,6 @@ int main()
 	scanf("%f %f %f", &x1, &y1, &r1);
 	printf("Vvedite koordinats x, y b radius vtoroy okruzhosti");
 	scanf("%f %f %f", &x2, &y2, &r2);
-	xmax = fmax(x1, x2);
-	xmin = fmin(x1, x2);
-	rmax = fmax(r1, r2);
-	ymin = fmin(y1, y2);
-	ymax = fmax(y1, y2);
 	rmin = fmin(r1, r2);
 	rmax = fmax(r1, r2);
 	if ((x1 == x2) && (y1 == y2) && (r1 == r2))
@@ -20,80 +15,7 @@ int main()
 		printf("okruzhosti sovpadayut");
 		return 0;
 	}
-	if ((x1 == x2) && (y1 != y2))
-	{
-		if ((y1 > 0) && (y2 > 0))
-		{
-			s = abs(y1 - y2);
-		}
-		if ((y1 < 0) && (y2 > 0))
-		{
-			s = (y2 + abs(y1));
-		}
-		if ((y1 > 0) && (y2 < 0))
-		{
-			s = (y1 + abs(y2));
-		}
-		if ((y1 < 0) && (y2 < 0))
-		{
-			s = abs(abs(y1) - abs(y2));
-		}
-	}
-	if ((y1 == y2) && (x1 != x2))
-	{
-		if ((x1 > 0) && (x2 > 0))
-		{
-			s = abs(x1 - x2);
-		}
-		if ((x1 < 0) && (x2 > 0))
-		{
-			s = (x2 + abs(x1));
-		}
-		if ((x1 > 0) && (x2 < 0))
-		{
-			s = (x1 + abs(x2));
-		}
-		if ((x1 < 0) && (x2 < 0))
-		{
-			s = abs(abs(x1) - abs(x2));
-		}
-	}
-	if ((x1 != x2) && (y1 != y2))
-	{
-		if ((y1 > 0) && (y2 > 0))
-		{
-			b = ymax - ymin;
-		}
-		if ((y1 > 0) && (y2 < 0))
-		{
-			b = y1 + abs(y2);
-		}
-		if ((y1 < 0) && (y2 > 0))
-		{
-			b = y2 + abs(y1);
-		}
-		if ((y1 < 0) && (y2 < 0))
-		{
-			b = abs(ymin) - abs(ymax);
-		}
-		if ((x1 > 0) && (x2 > 0))
-		{
-			a = xmax - xmin;
-		}
-		if ((x1 < 0) && (x2 > 0))
-		{
-			a = x2 + abs(x1);
-		}
-		if ((x1 > 0) && (x2 < 0))
-		{
-			a = x1 + abs(x2);
-		}
-		if ((x1 < 0) && (x2 < 0))
-		{
-			a = abs(xmin) - abs(xmax);
-		}
-		s = sqrt((a * a) + (b * b));
-	}
+	s = sqrt((x1 -x2)* (x1 - x2) + (y1 - y2)*(y1 - y2));
 	if (s == (r1 + r2))
 	{
 		printf("okruzhnosti kasautsya snaruzhi");
@@ -116,16 +38,7 @@ int main()
 			printf("okruzhnost soderzhitsya v drugoi");
 			return 0;
 		}
-		if (rmax < s)
-		{
-			printf("okruzhnosti peresekayutsya v dvuh tochkah snaruzhi");
-			return 0;
-		}
-		if ((rmax > s) && (rmin > (rmax - s)))
-		{
-			printf("okruzhosti peresekautsya v dvuh tochkah vnutri");
-			return 0;
-		}
+		printf("okruzhnosti peresekayutsya v dvuh tochkah");
 	}
 	return 0;
 }
