b1746042e4128959387a45432ebf191af67be35f
Kirius257 <holin.kirill@yandex.ru>
2022-11-15 15:27:24 +0300

    Corrected Practice3

--- KholinKI/Practice3/Practice3/Source.c
+++ KholinKI/Practice3/Practice3/Source.c
@@ -10,83 +10,63 @@
 
 int unique_number()
 {
-	int l, i, n = 0,q=9.9, multi, k, j, m[N], p = 0, unique = 1, old_n,mass[N],digit;
+	int l, i, n = 0, p = 0,mass[N],digit,g,count=0;
 	printf("Input length of riddled number: \n");
 	do {
 		scanf("%d", &l);
 	} while (l < 2 || l > 5);
-	do {
-		for (i = 1; i <= l; i++) {
+		for (i = 0; i < l; i++) {
 			digit = 0 + rand() % (9 - 0 + 1);		//generator sifr
-			n = 10 * n + digit;
+			mass[i] = digit;
+			for (g = 0; g < l; g++) {
+				if (mass[i] == mass[g]) {
+					count += 1;
+				}
+				if (count >= 2) {
+					digit = 0 + rand() % (9 - 0 + 1);
+					mass[i] = digit;
+					count = 0;
+					g = -1;
+				}
+			}
+			count = 0;
 		}
-		old_n = n;
 		i = 0;
-		while (n != 0) {
-			k = n % 10;
-			mass[i] = k;
-			if (mass[i] < 0) {
-				mass[i] = 0;
-			}
-			n = n / 10;					//obrabotka massiva...
-			i++;
-		}
 		for(i = 0; i < N; i++) {
-			if (mass[i] < 0) {
+			if (mass[i] < 0) {	//ochistka musora
 				mass[i] = 0;
 			}
 		}
-		for (i = 0; i < N; i++) {
-			m[i] = mass[i];
-		}
-		for (i = 0, j = 4; i < 5; i++) {
-			mass[i] = m[j];
-			j = j - 1;
-		}
-		unique = 1;
-		for (i = 0; i < N; i++) {
-			for (j = i + 1; j < N; j++) {
-				if (mass[i] == 0) { 
-					break;
-				}
-				else if (mass[i] == mass[j]) {
-					unique = 0;						//proverka na unikalnost
-					break;
-				}
-			}
-			if (unique == 0) {
-				for (i = 0; i < N; i++) {
-					mass[i] = 0;
-				}
-				break;
-			}
-		}
-	} while (unique == 0);
 	printf("\n");
-
 	for (i = 0; i < N; i++) {
-		p = 10 * p + mass[i]; //preobrazovanie massiva v chislo
+		//preobrazovanie massiva v chislo
+		if (mass[i] == 0) {
+			i = i;
+		}
+		else
+		p = 10 * p + mass[i];
 	}
-	return p;
+		return p;
 }
 
 int main() {
 	srand(time(NULL));
 	int pc_n,user_n,cows=0,bulls=0, k, pc_mass[N], m_pc[N], i = 0, j, m_user[N], user_mass[N], k1,signal=1,kolvo=0,saved_pc_n,flag=0,i_saved=0,mark1=0,significanse=2,id=0;		
 	pc_n = unique_number();
+	printf("%d\n", pc_n);
 	do{ //
 		do{ //* unikalnost chisla
-			printf("Input unique number: ");
-			scanf_s("%d", &user_n);
+			printf("Input unique number: \n");
+			scanf_s("%d", &user_n);	//polzovatel vvodit chislo
 			printf("\n");
 			i = 0;
-			saved_pc_n = pc_n;
-			kolvo = 0;
+			saved_pc_n = pc_n; // soxranenie chisla kompuktera
+			kolvo = 0; //soxranenie dlini chisla
 			while (pc_n != 0 && user_n != 0) {
 				k = pc_n % 10;
 				k1 = user_n % 10;
 				pc_mass[i] = k;
-				user_mass[i] = k1;
+				user_mass[i] = k1;			// pomechenie sift polzovatela i kompuktera v massivi
 				user_n = user_n / 10;
 				pc_n = pc_n / 10;
 				i++;
@@ -96,37 +76,40 @@ int main() {
 					return 1;
 				}
 			}
-			pc_n = saved_pc_n;
+			pc_n = saved_pc_n; //soxranennoe chislo kompuktera vosstanavlivau
 			for (i = 0; i < N; i++) {
 				m_pc[i] = pc_mass[i];
-				m_user[i] = user_mass[i];
+				m_user[i] = user_mass[i];	//vvodim vspomogatelnie massivi
 			}
 			for (i = 0, j = 4; i < 5; i++) {
 				pc_mass[i] = m_pc[j];
-				user_mass[i] = m_user[j];
+				user_mass[i] = m_user[j];	//perevorot massivov
 				j = j - 1;
 			}
-			signal = 1;
+			signal = 1;													//||||
 			for (i = 0; i < N; i++) {
 				for (j = i + 1; j < N; j++) {
 					if (user_mass[i] <= 0) {
 						break;
 					}
-					else if (user_mass[i] == user_mass[j]) {
+					else if (user_mass[i] == user_mass[j]) {	
 						signal = 0;
 						break;
-					}
+					}															//proverka unikalnosti chisla polzovatela
 				}
 				if (signal == 0) {
 					for (i = 0; i < N; i++) {
 						user_mass[i] = 0;
+						m_pc[i] = 0;
+						m_user[i] = 0;
+						pc_mass[i] = 0;
 					}
 					break;
 				}
 			}
-		} while (signal == 0);
+		} while (signal == 0);											//||||
 		flag = 0;
-		for (i = 0; i < N; i++) {
+		for (i = 0; i < N; i++) {															//|||||||||||||||||||||||||||
 			for (j = 0; j < N; j++) {
 				if ((pc_mass[i] <= 0 && user_mass[j] <= 0 && (i == 0 && j == 0 || i == 1 && j==0 || i==2 && j==-0))) {
 					if (flag == 0) {
@@ -146,13 +129,13 @@ int main() {
 				}
 				if (user_mass[j] == pc_mass[i] && j == i) {
 					bulls += 1;
-				}
+				}																		//METHOD KHOLIN KIRILL`S: Avoiding nulls
 				if ((user_mass[j] == pc_mass[i]) && j != i) {
 					cows += 1;
 				}
 			}
 			mark1 = 0;
-		}
+		}																					//|||||||||||||||||||||||||||
 		printf("Cows count: %d\n", cows);
 		printf("Bulls count: %d\n", bulls);
 		if (bulls != kolvo) {
@@ -160,7 +143,7 @@ int main() {
 			bulls = 0;
 			for (i = 0; i < N; i++) {
 				user_mass[i] = 0;
-			}
+			}																//Podvedenit itogov i umivanie ruk
 			for (i = 0; i < N; i++) {
 				pc_mass[i] = 0;
 				m_pc[i] = 0;
@@ -168,7 +151,7 @@ int main() {
 			}
 		}
 	} while (bulls != kolvo);
-	printf("Congratulations! You won!");
+	printf("Congratulations! You won!");					//pobeda!
 	return 0;
 }
 
