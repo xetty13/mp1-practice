827e56be350bc413622f72b51cf93a2943aa107c
Tanya-Yasakova <yasakova_04@mail.ru>
2022-11-15 23:20:05 +0300

    fix file

--- YasakovaTE/Practice4/Practice4/Source.c
+++ YasakovaTE/Practice4/Practice4/Source.c
@@ -5,36 +5,53 @@
 #include <time.h>
 #include <math.h>
 #include <string.h>
-char codes[10][4] = { "0001", "0002", "0003", "0004", "0005", "0006", "0007", "0008", "0009", "0010", };
-char name[10][10] = { "carrot", "potato", "tomato", "chips", "chocolate", "juice", "lemonade", "banana", "milk", "sour cream" };
-int cost[10][3] = { "20", "10", "15", "80", "90", "150", "100", "40", "60", "70" };
-int discount[10][2] = { "0.05", "0.08", "0.07", "0.3", "0.25", "0.35", "0.15", "0.04", "0.5", "0.12" };
-int main()
-{
-	char code;
-	int count = 10;
-	int i, fl;
-	while (1)
-	{
-		printf("Hello! Scan the product: ");
-		scanf(" %c", code);
-		fl = 0;
-		for (i = 1; i < count; i++)
-		{
-			if (codes[i] == code)
+#define N 10
+
+char* barcodes[N] = { "0101", "0202", "0303", "1212", "1414", "2323", "4545", "5236", "5632", "7845" };
+char* products[N] = { "Milk", "Bread", "Butter", "Cheese", "Sugar", "Juice", "Chicken", "Jam", "Beer", "Pie" };
+int cost[10] = { 20, 10, 15, 80, 90, 150, 100, 40, 60, 70 };
+float discount[10] = { 0.05, 0.03, 0.04, 0.15, 0.19, 0.10, 0.18, 0.03, 0.04, 0.09 };
+
+void scan(int* a) {
+	char b[N];
+	int count[10] = { 0 };
+	int out=1;
+	int get = 1;
+	printf("Enter codes: \n");
+	while (out==1) {
+		gets(b);
+		for (int i = 0; i < N; i++) {
+			if (strcmp(barcodes[i], b) == 0)
 			{
-				fl = 1;
+				printf("%s,   price: %.2d rubles,   diccount: %.2f percent\n", products[i], cost[i], discount[i]);
+				a[i] += 1;
+				printf("If there are no more products in the basket, then enter - 0, if there is - 1:  \n");
+				scanf(" %d", &get);
+				if (get == 0)
+					out = 0;
+				else printf("Enter codes: \n");
 				break;
 			}
 		}
-		if (fl == 0)
-		{
-			printf("An incorrect product code has been entered. Enter another one: ");
-			
-		}
 	}
+}
 
+void bill(int* a) {
+	float totalCost = 0;
+	printf("List of products:\n");
+	for (int i = 0; i < N; i++) {
+		if (a[i] == 0)
+			continue;
+		printf("%s, cost per unit: %d, count: %d,  price including discount: %.2f\n", products[i], cost[i], a[i], (cost[i] - discount[i] * cost[i]) * a[i]);
+		totalCost += (cost[i] - discount[i] * cost[i]) * a[i];
+	}
+	printf("Total amount - %.2f", totalCost);
+}
 
-
+int main() {
+	int counts[N] = { 0 };
+	scan(counts);
+	bill(counts);
 	return 0;
 }
+
