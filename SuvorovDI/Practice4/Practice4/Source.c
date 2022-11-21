1b137c99738b56819ec1267f57a35bb60d59bfed
suvorovDm-1 <dim-suv@mail.ru>
2022-11-21 12:50:38 +0300

    'ModifiedVersion'

--- SuvorovDI/Practice4/Practice4/Source.c
+++ SuvorovDI/Practice4/Practice4/Source.c
@@ -1,57 +1,109 @@
 #include <stdio.h>
+#include <string.h>
 
-#define C_PR 12
+#define TOTAL_N 12
+
+char* names[TOTAL_N] = { "Milk 1L", "Bread", "Butter", "Chokolate", "Cheese", "Ice-cream", "Chicken 800g", "Beef 1kg", "Crisps", "Juice 1L", "Sugar 1kg", "Salt 1kg" };
+int prices[TOTAL_N] = { 86, 45, 110, 114, 120, 65, 220, 300, 82, 99, 50, 45 };
+int discounts[TOTAL_N] = { 5, 3, 20, 25, 0, 5, 10, 5, 5, 5, 0, 5 };
+char barcodes[TOTAL_N][5] = { "0001", "00010", "0011", "0100", "0101", "0110", "0111", "1000", "1001", "1010", "1011", "1100" };
+
+
+int check_exists_bar(char curr[]) {
+	int i, result = -1, n = TOTAL_N;
+	for (i = 0; i < n; i++) {
+		if (strcmp(curr, barcodes[i]) == 0) {
+			result = i;
+			break;
+		}
+	}
+	return result;
+}
+
+void print_cheque(int count[], double curr_price_whoutDisc, double t_curr_price) {
+	int i, n = TOTAL_N;
+	double economy = curr_price_whoutDisc - t_curr_price, perc_economy = (economy / curr_price_whoutDisc) * 100;
+	printf("------------------------------------------------------------------------------\n");
+	printf("------------------------------------------------------------------------------\n\n");
+	for (i = 0; i < n; i++) {
+		if (count[i] > 0) {
+			double price_disc = prices[i] * (1 - (double)discounts[i] / 100.0);
+			double total_pr = price_disc * count[i];
+			printf("Product: %s   ||   Price with discount: %.*lf   ||   Count: %d   ||   Total price is  %.*lf\n\n", names[i], 2, price_disc, count[i], 2, total_pr);
+		}
+	}
+	printf("------------------------------------------------------------------------------\n");
+	printf("\nTotal cost without discount:  %.*lf ,  BUT in general you have saved %.*lf rubles (i.e. %.*lf percent)\n", 2, curr_price_whoutDisc, 2, economy, 2, perc_economy);
+	printf("To be paid:  %.*lf  reubles\n", 2, t_curr_price);
+}
+
+// calculates the count of each product and the total price
+void product_processing(int id, int count[], double* cost_without, double* total_cost) {
+	double price_disc = prices[id] * (1 - (double)discounts[id] / 100.0);
+	count[id]++;
+	*(cost_without) += prices[id];
+	*(total_cost) += price_disc;
+}
+
+void print_prod_info(int bar) {
+	double price_disc = prices[bar] * (1 - (double)discounts[bar] / 100.0);
+	printf("Product:  %s  ||  Price per piece:  %d rubles  ||   Discount:  %d%%  ||   price with discount:   %.*lf rubles\n\n", names[bar], prices[bar], discounts[bar], 2, price_disc);
+}
+
+int get_and_check_barcode() {
+	char curr_bar[5], clean, count_of_ch = 0, c_extra_ch = 0, ind_prod = -1;
+	int i, n = TOTAL_N;
+	printf("Enter barcode or 'C'\n");
+	fgets(curr_bar, 5, stdin);
+	while ((clean = getchar()) != '\n') {
+		c_extra_ch++;
+	}
+
+	// User wants to get cheque
+	if (curr_bar[0] == 'C' && curr_bar[1] == '\n') {
+		return -2;
+	}
+
+	// check the barcode by 3 parameters:
+
+	for (i = 0; i < 4; i++) { // check for invalid characters
+		if (!((curr_bar[i] == '0') || (curr_bar[i] == '1'))) {
+			printf("The barcodes in our supermarket consist only of '0' and '1'\n\n");
+			return -1;
+		}
+	}
+
+	if (c_extra_ch > 0) { // the barcode is too long -- it's bad
+		printf("You have been entered wrong-length barcode, you should enter barcode which length is 4!!!\n\n");
+		return -1;
+	}
+
+	ind_prod = check_exists_bar(curr_bar); // -1 if such a barcode doesn't exist
+	if (ind_prod == -1) {
+		printf("We haven't a product with such barcode!!! Try again\n\n");
+		return -1;
+	}
+
+	return ind_prod;
+}
 
 int main() {
-	char barcodes[C_PR][5] = { "0000", "0001", "0010", "0011", "0100", "1000", "1010", "1011", "1111", "0110", "0111", "0101" };
-	char names[C_PR][30] = { "Milk Prostokvashino", "Bread", "Cola 1l", "Sour cream", "Butter", "Chocolate Milka", "Cheese", "Porridge", "Eggs", "Rice", "Sugar 1kg", "baking soda" };
-	int priñes[C_PR] = { 80, 40, 110, 80, 107, 120, 170, 50, 55, 60, 59, 250 };
-	double disc[C_PR] = { 10.0, 0.0, 5.0, 15.0, 15.0, 20.0, 5.0, 0.0, 5.0, 0.0, 5.0, 15.0 }; // percentage discount
-	int hmuch[C_PR] = { 0 };
-
+	int count[12] = { 0 }, res_id;
+	double curr_price_whoutDisc = 0, t_curr_price = 0;
+	printf("Welcome to our supermarket!\nThere is a self-service ticket office\nEnter a barcode of each product, or enter 'C' and 2 'ENTER' to get cheque)\n\n");
 
 	while (1) {
-		char curr_bar[5], c;
-		char code_ch[4] = "CHEK";
-		int i, j, flag, ind_pr;
-		printf("Enter a barcode of your product\n");
-		printf("If you have already scanned all the products, enter: 'CHEK'\n");
-		fgets(curr_bar, 5, stdin);
-		while ((c = getchar()) != '\n' && c != EOF); // clean up in stream
+		res_id = get_and_check_barcode(); // returns: -1, if sth goes wrong; index of product, if It's OK prod; -2, if CHECK
 
-		flag = 1;
-		for (j = 0; j < 4; j++) {
-			if (curr_bar[j] != code_ch[j]) {
-				flag = 0;
-				break;
-			}
+		if (res_id != -1 && res_id != -2) {
+			print_prod_info(res_id);
+			product_processing(res_id, count, &curr_price_whoutDisc, &t_curr_price);
 		}
-		if (flag) {
-			int i;
-
-			for (i = 0; i < C_PR; i++) {
-				if (hmuch[i] != 0)
-					printf("Product: %s\tPrice: %d\tDiscount: %d%%\t total price is: %.*lf\n", names[i], priñes[i], (int)disc[i], 2, (priñes[i] * (1 - disc[i] / 100)));
-			}
-
+		if (res_id == -2) {
+			print_cheque(count, curr_price_whoutDisc, t_curr_price);
 			break;
 		}
 
-		for (i = 0; i < C_PR; i++) {
-			flag = 1;
-			for (j = 0; j < 4; j++) {
-				if (curr_bar[j] != barcodes[i][j]) {
-					flag = 0;
-					break;
-				}
-			}
-
-			if (flag) {
-				hmuch[i] += 1;
-				printf("Product: %s\tPrice: %d\tDiscount: %d%%\t total price is: %.*lf\n", names[i], priñes[i], (int)disc[i], 2, (priñes[i] * (1 - disc[i] / 100)));
-				break;
-			}
-		}
 	}
 
 	return 0;
