b39f396173cbd841f9542755c6265940b2a10c79
sonya-shulakova <sofyashulakova22@gmail.com>
2022-11-21 17:04:50 +0300

    Fixed

--- ShulakovaSD/Practice3/Practice3/bulls and cows.c
+++ ShulakovaSD/Practice3/Practice3/bulls and cows.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include <time.h>
 #include <math.h>
-int main()													
+int main()
+{
 	{
-		int a[5] = { 0 }, b[5] = { 0 }, n = 0, i = 0, tries = 0, k, l, f = 1, n1, n2, bull = 0, cow = 0;
+		int a[5] = { 0 }, b[5] = { 0 }, n = 0, i = 0, tries = 0, k, l, f = 1, n1, n2, bull = 0, cow = 0, tmp = 0;
 		time_t t;
 		srand((unsigned)time(&t));
 		printf("Enter the length of the number (from 2 to 5): ");
@@ -28,17 +29,29 @@ int main()
 			f *= 10;
 		}
 		do {
-			printf("Enter your guess: ");
+				printf("Enter your guess: ");
 
-			do {											
-				scanf_s("%d", &l);
-			} while ((l < f) && (l > 10 * f - 1));
-			for (i = n - 1; i >= 0; i--) {
-				b[i] = l % 10;
-				l /= 10;
-			}
+				do {											
+					scanf_s("%d", &l);
+					for (i = n - 1; i >= 0; i--) {
+						b[i] = l % 10;
+						l /= 10;
+					}
+					tmp = 0;
+					for (i = 0; i < n; i++) {
+						for (int kk = i + 1; kk < n; kk++) {
+							if ((b[i] == b[kk]) || (b[kk] == 0)) {
+									tmp = 1;
+									break;
+							}
+						}
+					}
+					if (tmp == 1) {
+						printf("you've entered a wrong number");
+					}
+			} while (tmp == 1);
 			tries++;
-			for (n1 = 0; n1 < n; n1++) {					//checking if there are any bulls or cows
+			for (n1 = 0; n1 < n; n1++) {					
 				for (n2 = 0; n2 < n; n2++) {
 					if ((n1 == n2) && (a[n1] == b[n2])) {
 						bull++;
@@ -51,10 +64,11 @@ int main()
 			printf("bulls: %d\n", bull);
 			printf("cows: %d\n", cow);
 			if (bull == n) {
-				printf("Congrats! Total attemts count: %d", tries);
-				return 0;
+					printf("Congrats! Total attemts count: %d", tries);
+					return 0;
 			}
 			cow = 0;
 			bull = 0;
-		} while (1);
-	}
+		}while (1);
+	} 
+}
