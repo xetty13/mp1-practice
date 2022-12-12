f51dfc69d116b79cb8183e43ef4e4b853bd1dcb8
Tanya-Yasakova <yasakova_04@mail.ru>
2022-12-12 21:12:10 +0300

    fix file

--- YasakovaTE/Practice5/Practice5/Source.c
+++ YasakovaTE/Practice5/Practice5/Source.c
@@ -1,4 +1,248 @@
-void main()
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <tchar.h>
+#include <windows.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+#include <time.h>
+#include <string.h>
+#include <time.h>
+#include<tchar.h>
+
+WIN32_FIND_DATA names[MAX_PATH];
+WIN32_FIND_DATA File;
+HANDLE hfile;
+
+
+void Choice(int a[], wchar_t** b[], int N);
+void Insert(int a[], wchar_t** b[], int N);
+void merge_sort(int a[], wchar_t** name[], int N);
+void output(int size[], wchar_t** name[], int N);
+void merge_sort_lr(int a[], wchar_t** name[], int l, int r);
+void merge(int get[], wchar_t** name[], int l, int mid, int r);
+
+
+
+int main()
+{
+	printf("19  \n");
+	clock_t start, end;
+	double elapsed;
+	int i = 0, N = 0, number, flag = 1;
+	char* a = (char*)malloc(MAX_PATH);
+	int* size = (int*)malloc(MAX_PATH * sizeof(int));
+	wchar_t** name = (wchar_t**)malloc(MAX_PATH * sizeof(wchar_t*));
+	wchar_t* path = (wchar_t*)malloc(MAX_PATH * sizeof(wchar_t));
+	printf("Enter the path to the directory (Example: D:\\photo\\1\)  \n");
+	
+	scanf("%s", a);
+	strcat(a, "\\*.*");
+	mbstowcs(path, a, strlen(a) + 1);
+	hfile = FindFirstFileW(path, &File);
+
+	FindNextFileW(hfile, &File);
+	while (FindNextFileW(hfile, &File) != NULL)
+	{
+		names[i] = File;
+		name[i] = names[i].cFileName;
+		size[i] = names[i].nFileSizeLow;
+		i++;
+	}
+	N = i;
+
+	
+	int* size_copy = (int*)malloc(MAX_PATH * sizeof(int));
+	wchar_t** name_copy = (wchar_t**)malloc(MAX_PATH * sizeof(wchar_t*));
+	do
+	{
+		for (i = 0; i < N; i++)
+		{
+			size_copy[i] = size[i];
+			name_copy[i] = name[i];
+		}
+		printf("Choose the type of sorting: 1-selection sorting, 2-insertion sorting, 3-merge sorting  ");
+		scanf("%d", &number);
+
+		start = clock();
+		if ((number < 1) || (number > 3))
+		{
+			printf("Invalid number.  \n");
+			continue;
+		}
+		else if (number == 1)
+		{
+			Choice(size_copy, name_copy, N);
+		}
+		else if (number == 2)
+		{
+			Insert(size_copy, name_copy, N);
+		}
+		else if (number == 3)
+		{
+			merge_sort(size_copy, name_copy, N);
+		}
+		end = clock();
+		if ((number>=1) || (number<=3))
+			output(size_copy, name_copy, N);
+		elapsed = ((double)(end - start)) / CLOCKS_PER_SEC;
+		printf("\nTime: %.10f seconds\n", elapsed);
+		printf("\n");
+		printf("If you want to finish, write 0, if not - 1:  ");
+		scanf("%d", &flag);
+	} while (flag == 1);
+	
+	return 0;
+}
+
+
+void output(int size_copy[], wchar_t** name_copy[], int N)
+{
+	int i = 0;
+	for (i = 0; i < N; i++)
+	{
+		printf("\n%-60S ", name_copy[i]);
+		printf("%d ", size_copy[i]);
+	}
+}
+
+
+
+void Choice(int a[], wchar_t** b[], int N)  //сортировка выбором  (2)
+{
+	int i, j, min, ind, tmp;
+	wchar_t* t = (wchar_t*)malloc(MAX_PATH * sizeof(wchar_t));
+	for (i = 0; i < N; i++)
+	{
+		min = a[i];
+		ind = i;
+		for (j = i + 1; j < N; j++)
+			if (a[j] < min)
+			{
+				min = a[j];
+				ind = j;
+			}
+		tmp = a[ind];
+		a[ind] = a[i];
+		a[i] = tmp;
+
+		t = b[ind];
+		b[ind] = b[i];
+		b[i] = t;
+	}
+}
+
+
+void Insert(int a[], wchar_t** b[], int N)  //сортировка вставками  (3)
+{
+	int i, j, tmp, k;
+	wchar_t* t = (wchar_t*)malloc(MAX_PATH * sizeof(wchar_t));
+	for (i = 1; i < N; i++)
+	{
+		tmp = a[i];
+		t = b[i];
+		j = i - 1;
+		while ((j >= 0) && (a[j] > tmp))
+		{
+			a[j + 1] = a[j];
+			b[j + 1] = b[j];
+			j--;
+
+		}
+		a[j + 1] = tmp;
+		b[j + 1] = t;
+
+	}
+}
+
+
+
+
+void merge(int get[], wchar_t** name[], int l, int mid, int r)   //функция слияния
+{
+	int k = 0, i = 0, j = 0;
+	int N = mid - l;
+	int M = r - mid + 1;
+	int* a = (int*)malloc((N) * sizeof(int));
+	for (i = l; i < mid; i++)
+	{
+		a[i-l] = get[i];
+	}
+	int* b = (int*)malloc((M) * sizeof(int));
+	for (i = mid; i < r+1; i++)
+	{
+		b[i - mid] = get[i];
+	}
+	int* c = (int*)malloc((r-l+1) * sizeof(int));
+
+	
+	wchar_t** name_a = (wchar_t**)malloc(N * sizeof(wchar_t*));
+	for (i = l; i < mid; i++)
+	{
+		name_a[i - l] = name[i];
+
+	}
+	
+	wchar_t** name_b = (wchar_t**)malloc(M * sizeof(wchar_t*));
+	for (i = mid; i < r + 1; i++)
+	{
+		name_b[i - mid] = name[i];
+	}
+	wchar_t** name_c = (wchar_t**)malloc((r - l + 1) * sizeof(wchar_t*));
+	
+	i = 0;
+	while ((i < N) && (j < M))
+	{
+		if (a[i] < b[j]) {
+			c[k] = a[i];
+			name_c[k] = name_a[i];
+			k++;
+			i++;
+		}
+		else
+		{
+			c[k] = b[j];
+			name_c[k] = name_b[j];
+			k++;
+			j++;
+		}
+	}
+	while (i < N)
+	{
+		c[k] = a[i];
+		name_c[k] = name_a[i];
+		k++;
+		i++;
+	}
+	while (j < M)
+	{
+		c[k] = b[j];
+		name_c[k] = name_b[j];
+		k++;
+		j++;
+	}
+
+	for (i = l; i < r + 1; i++)
+	{
+		get[i] = c[i - l];
+		name[i] = name_c[i - l];
+	}
+}
+
+
+void merge_sort(int a[], wchar_t** name[], int N)
 {
 
+	merge_sort_lr(a, name, 0, N - 1);
+}
+
+void merge_sort_lr(int a[], wchar_t** name[], int l, int r)
+{
+	int mid;
+	if (l >= r)
+		return;
+	mid = l + (r - l) / 2;
+	merge_sort_lr(a, name, l, mid);
+	merge_sort_lr(a, name, mid + 1, r);
+	merge(a, name, l, mid+1, r);
 }
