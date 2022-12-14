7d4ee15f4deee6a4fd6ab29bc72c4b30cf38d3db
Tanya-Yasakova <yasakova_04@mail.ru>
2022-12-14 17:02:49 +0300

    fix file

--- YasakovaTE/Practice5/Practice5/Source.c
+++ YasakovaTE/Practice5/Practice5/Source.c
@@ -21,18 +21,81 @@ void merge_sort(int a[], wchar_t** name[], int N);
 void output(int size[], wchar_t** name[], int N);
 void merge_sort_lr(int a[], wchar_t** name[], int l, int r);
 void merge(int get[], wchar_t** name[], int l, int mid, int r);
+int form_array(int size[], wchar_t** name[], wchar_t** path[]);						
 
 
 
 int main()
 {
+	printf("2 \n");
 	int i = 0, N = 0, number, flag = 1;
-	char* a = (char*)malloc(MAX_PATH);
 	int* size = (int*)malloc(MAX_PATH * sizeof(int));
 	wchar_t** name = (wchar_t**)malloc(MAX_PATH * sizeof(wchar_t*));
 	wchar_t* path = (wchar_t*)malloc(MAX_PATH * sizeof(wchar_t));
-	printf("Enter the path to the directory (Example: D:\\photo\\1\)  \n");
+	N=form_array(size, name, path);
+	sorting(size, name, N);
+	return 0;
+}
+
+
+
+int sorting(int size[], wchar_t** name[], int N)
+{
+	int i, number, flag;
+	long long time1, time2;
+	int* size_copy = (int*)malloc(MAX_PATH * sizeof(int));
+	wchar_t** name_copy = (wchar_t**)malloc(MAX_PATH * sizeof(wchar_t*));
+	do
+	{
+		for (i = 0; i < N; i++)
+		{
+			size_copy[i] = size[i];
+			name_copy[i] = name[i];
+		}
+		printf("Choose the type of sorting: 1-selection sorting, 2-insertion sorting, 3-merge sorting  ");
+		scanf("%d", &number);
+
+		if ((number < 1) || (number > 3))
+		{
+			printf("Invalid number.  \n");
+			continue;
+		}
+		else if (number == 1)
+		{
+			QueryPerformanceCounter(&time1);
+			Choice(size_copy, name_copy, N);
+			QueryPerformanceCounter(&time2);
+		}
+		else if (number == 2)
+		{
+			QueryPerformanceCounter(&time1);
+			Insert(size_copy, name_copy, N);
+			QueryPerformanceCounter(&time2);
+		}
+		else if (number == 3)
+		{
+			QueryPerformanceCounter(&time1);
+			merge_sort(size_copy, name_copy, N);
+			QueryPerformanceCounter(&time2);
+		}
+		if ((number >= 1) || (number <= 3))
+			output(size_copy, name_copy, N);
+		printf("\nThe time it took to sort the bubble = %lld milliseconds\n\n", time2 - time1);
+		printf("\n");
+		printf("If you want to finish, write 0, if not - 1:  ");
+		scanf("%d", &flag);
+	} while (flag == 1);
+
+}
+
+
+
+int form_array(int size[], wchar_t** name[], wchar_t** path[])
+{
 	
+	char* a = (char*)malloc(MAX_PATH);
+	int i = 0, N = 0;;
+	printf("Enter the path to the directory (Example: D:\\photo\\1\)  \n");
 	scanf("%s", a);
 	strcat(a, "\\*.*");
 	mbstowcs(path, a, strlen(a) + 1);
@@ -47,50 +110,11 @@ int main()
 		i++;
 	}
 	N = i;
-
-	
-	int* size_copy = (int*)malloc(MAX_PATH * sizeof(int));
-	wchar_t** name_copy = (wchar_t**)malloc(MAX_PATH * sizeof(wchar_t*));
-	long long time1, time2;
-	do
-	{
-		for (i = 0; i < N; i++)
-		{
-			size_copy[i] = size[i];
-			name_copy[i] = name[i];
-		}
-		printf("Choose the type of sorting: 1-selection sorting, 2-insertion sorting, 3-merge sorting  ");
-		scanf("%d", &number);
-
-		QueryPerformanceCounter(&time1);
-		if ((number < 1) || (number > 3))
-		{
-			printf("Invalid number.  \n");
-			continue;
-		}
-		else if (number == 1)
-		{
-			Choice(size_copy, name_copy, N);
-		}
-		else if (number == 2)
-		{
-			Insert(size_copy, name_copy, N);
-		}
-		else if (number == 3)
-		{
-			merge_sort(size_copy, name_copy, N);
-		}
-		QueryPerformanceCounter(&time2);
-		if ((number>=1) || (number<=3))
-			output(size_copy, name_copy, N);
-		printf("\nThe time it took to sort the bubble = %lld milliseconds\n\n", time2 - time1);
-		printf("\n");
-		printf("If you want to finish, write 0, if not - 1:  ");
-		scanf("%d", &flag);
-	} while (flag == 1);
-	
-	return 0;
+	return N;
 }
+
+
+
 
 
 void output(int size_copy[], wchar_t** name_copy[], int N)
