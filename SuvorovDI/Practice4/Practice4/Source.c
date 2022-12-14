4b3870944b5a102bc19968c44aae96909c317b46
SuvorovDm-1 <dim-suv@mail.ru>
2022-12-14 15:32:14 +0300

    'TheLastChanges'

--- SuvorovDI/Practice4/Practice4/Source.c
+++ SuvorovDI/Practice4/Practice4/Source.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 #define TOTAL_N 12
 
 char* names[TOTAL_N] = { "Milk 1L", "Bread", "Butter", "Chokolate", "Cheese", "Ice-cream", "Chicken 800g", "Beef 1kg", "Crisps", "Juice 1L", "Sugar 1kg", "Salt 1kg" };
 int prices[TOTAL_N] = { 86, 45, 110, 114, 120, 65, 220, 300, 82, 99, 50, 45 };
 int discounts[TOTAL_N] = { 5, 3, 20, 25, 0, 5, 10, 5, 5, 5, 0, 5 };
-char barcodes[TOTAL_N][5] = { "0001", "00010", "0011", "0100", "0101", "0110", "0111", "1000", "1001", "1010", "1011", "1100" };
+char barcodes[TOTAL_N][5] = { "0001", "0010", "0011", "0100", "0101", "0110", "0111", "1000", "1001", "1010", "1011", "1100" };
 
 
 int check_exists_bar(char curr[]) {
@@ -33,8 +34,8 @@ void print_cheque(int count[], double curr_price_whoutDisc, double t_curr_price)
 		}
 	}
 	printf("------------------------------------------------------------------------------\n");
-	printf("\nTotal cost without discount:  %.2lf ,  BUT in general you have saved %.2lf rubles (i.e. %.*lf percent)\n", 2, curr_price_whoutDisc, economy, perc_economy);
-	printf("To be paid:  %.*lf  reubles\n", 2, t_curr_price);
+	printf("\nTotal cost without discount:  %.2lf ,  BUT in general you have saved %.2lf rubles (i.e. %.2lf percent)\n", curr_price_whoutDisc, economy, perc_economy);
+	printf("To be paid:  %.2lf  reubles\n", t_curr_price);
 }
 
 // calculates the count of each product and the total price
@@ -52,31 +53,32 @@ void print_prod_info(int bar) {
 
 int get_and_check_barcode() {
 	char curr_bar[5], clean, count_of_ch = 0, c_extra_ch = 0, ind_prod = -1;
-	int i, n = TOTAL_N;
+	int i, n = TOTAL_N, len;
 	printf("Enter barcode or 'C'\n");
 	gets(curr_bar);
+	len = strlen(curr_bar);
 	/*while ((clean = getchar()) != '\n') {
 		c_extra_ch++;
 	}*/
 
 	// User wants to get cheque
-	if (!strcmp(curr_bar, "0")) {
+	if (strcmp(curr_bar, "C") == 0) {
 		return -2;
 	}
 
 	// check the barcode by 3 parameters:
 
-	for (i = 0; i < 4; i++) { // check for invalid characters
+	for (i = 0; i < len; i++) { // check for invalid characters
 		if (!((curr_bar[i] == '0') || (curr_bar[i] == '1'))) {
 			printf("The barcodes in our supermarket consist only of '0' and '1'\n\n");
 			return -1;
 		}
 	}
 
-	if (c_extra_ch > 0) { // the barcode is too long -- it's bad
-		printf("You have been entered wrong-length barcode, you should enter barcode which length is 4!!!\n\n");
-		return -1;
-	}
+	//if (c_extra_ch > 0) { // the barcode is too long -- it's bad
+	//	printf("You have been entered wrong-length barcode, you should enter barcode which length is 4!!!\n\n");
+	//	return -1;
+	//}
 
 	ind_prod = check_exists_bar(curr_bar); // -1 if such a barcode doesn't exist
 	if (ind_prod == -1) {
