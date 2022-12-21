1d45d2cc9224efd72478179096ecd5cc44770a77
MaxikGuy <S_Maksssim@mail.ru>
2022-12-22 00:50:13 +0300

    Practice4 update

--- SavchenkoMP/Practice4/Practice4/Practice4.c
+++ SavchenkoMP/Practice4/Practice4/Practice4.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 
 #define N_ARR 10
-#define N_CODE 4
+#define N_CODE 5
 #define N_NAME 6
 
 /*
@@ -12,20 +12,9 @@ Bread, Milk, Water, Tomato, Pickle, Salt, Pepper, Shugar, Beer, Jagermeister
 
 int i, j, total_cost = 0;
 
-//char codes[N_ARR][N_CODE] = {
-//	"0001", "1213", "2424", "3776", "4637", "5180", "6878", "7352", "8545", "9999"
-//};
-
-char code0[] = "0001";
-char code1[] = "1213";
-char code2[] = "2424";
-char code3[] = "3776";
-char code4[] = "4637";
-char code5[] = "5180";
-char code6[] = "6878";
-char code7[] = "7352";
-char code8[] = "8545";
-char code9[] = "9999";
+char codes[N_ARR][N_CODE] = {
+	"0001", "1213", "2424", "3776", "4637", "5180", "6878", "7352", "8545", "9999"
+};
 
 char name[N_ARR][N_NAME] = {
 	"Bread ", "Milk  ", "Water ", "Tomato", "Pickle", "Salt  ", "Pepper", "Spices", "Beer  ", "Jagerm"
@@ -50,27 +39,11 @@ int chr_int(char x) {
 		return -1;
 }
 
-/*
-int chr_int(char x) {
-	switch (x) {
-	case '0': {return 0; break; }
-	case '1': {return 1; break; }
-	case '2': {return 2; break; }
-	case '3': {return 3; break; }
-	case '4': {return 4; break; }
-	case '5': {return 5; break; }
-	case '6': {return 6; break; }
-	case '7': {return 7; break; }
-	case '8': {return 8; break; }
-	case '9': {return 9; break; }
-	default: return -1;
-	}
-}*/
-
 int check(char* c) { // 1 - in array; 0 - NOT in array
-	if (!strcmp(c, code0) || !strcmp(c, code1) || !strcmp(c, code2) || !strcmp(c, code3) || !strcmp(c, code4) ||
-		!strcmp(c, code5) || !strcmp(c, code6) || !strcmp(c, code7) || !strcmp(c, code8) || !strcmp(c, code9))
-		return 1;
+	int i;
+	for (i = 0; i < N_ARR; i++)
+		if (!(strncmp(c, codes[i], N_CODE)))
+			return 1;
 	return 0;
 }
 
@@ -95,7 +68,7 @@ int main() {
 
 	printf("| NAME   | COST | DISC. | COST_DISC. |\n");
 
-	
+
 	do {
 		inp = (char*)malloc(10);
 		gets(inp);
@@ -110,7 +83,7 @@ int main() {
 
 		free(inp);
 	} while (flag);
-	
+
 
 	//The_check:
 	printf("\n\n=====================================================================\n");
