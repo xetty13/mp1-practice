fe3675e6d289c4fcc0b09289f6272a09c68ab4e8
MishaRysev <r0botbob@yandex.ru>
2023-02-15 12:25:03 +0300

    Fixed

--- RysevMD/Practice2_0/Practice2_0/main.c
+++ RysevMD/Practice2_0/Practice2_0/main.c
@@ -6,10 +6,14 @@ int main() {
 	Matrix *res, *m1, *m2, *res_dt;
 
 	allocate_matrix(&m1, 3);
-	allocate_matrix(&m2, 3);
 	fill_matrix(m1, 3);
+
+	allocate_matrix(&m2, 3);
 	fill_matrix(m2, 3);
 	
+	res_dt = add_dot(m1, 5.0f);
+	print_matrix(res_dt, 3);
+	free_matrix(&res_dt);
 
 	res_dt = multiple_dot(m1, 2.0f);
 	print_matrix(res_dt, 3);
@@ -19,7 +23,6 @@ int main() {
 	print_matrix(res, 3);
 	free_matrix(&res);
 
-	printf("\n");
 	res = multiple_matrix(m1, m2);
 	print_matrix(res, 3);
 	free_matrix(&res);
--- RysevMD/Practice2_0/Practice2_0/matrix.c
+++ RysevMD/Practice2_0/Practice2_0/matrix.c
@@ -15,42 +15,61 @@ void free_matrix(Matrix** matr) {
 void fill_matrix(Matrix* matr, int n) {
 	int i;
 	for (i = 0; i < n * n; i++) {
-		scanf("%f", &(matr->x[i * n + i]));
+		scanf("%f", &(matr->x[i]));
 	}
 }
 void print_matrix(Matrix* matr, int n) {
 	int i;
 	for (i = 0; i < n * n; i++) {
-		printf("%.1f ", matr->x[i * n + i]);
+		printf("%.1f ", matr->x[i]);
 		if ((i + 1) % n == 0) {
-			printf("\n");
+			printf("\n  ");
 		}
 	}
+	printf("\n");
 }
 
-Matrix* multiple_dot(Matrix* matr, float a) {
+Matrix* add_dot(Matrix* matr, float a){
+	printf("\nadd dot:\n\n  ");
 	Matrix* res;
 	int i;
 	allocate_matrix(&res, 3);
-	for (i = 0; i < (res->n * res->n); i++) {
-		res->x[i * res->n + i] = matr->x[i * matr->n + i] * a;
+	int n = res->n;
+	for (i = 0; i < n * n; i++) {
+		res->x[i] = (matr->x[i]) + a;
+	}
+	return res;
+}
+Matrix* multiple_dot(Matrix* matr, float a) {
+	printf("multiple dot:\n\n  ");
+	Matrix* res;
+	int i;
+	allocate_matrix(&res, 3);
+	int n = res->n;
+	for (i = 0; i < n * n; i++) {
+		res->x[i] = (matr->x[i]) * a;
 	}
 	return res;
 }
 Matrix* add_matrix(Matrix* matr1, Matrix* matr2) {
+	printf("add matrix:\n\n  ");
 	Matrix* res;
-	int i;
+	int i, j;
 	if (matr1->n != matr2->n) {
 		printf("matrices must be of the same order");
 		return NULL;
 	}
 	allocate_matrix(&res, 3);
-	for (i = 0; i < res->n * res->n; i++) {
-		res->x[i * res->n + i] = matr1->x[i * matr1->n + i] + matr2->x[i * matr2->n + i];
+	int n = res->n;
+	for (i = 0; i < n; i++) {
+		for (j = 0; j < n; j++) {
+			res->x[i * n + j] = (matr1->x[i * n + j]) + (matr2->x[i * n + j]);
+		}
 	}
 	return res;
 }
 Matrix* multiple_matrix(Matrix* matr1, Matrix* matr2) {
+	printf("multiple_matrix:\n\n  ");
 	Matrix* res;
 	int i, j, k;
 	if (matr1->n != matr2->n) {
@@ -58,11 +77,13 @@ Matrix* multiple_matrix(Matrix* matr1, Matrix* matr2) {
 		return NULL;
 	}
 	allocate_matrix(&res, matr1->n);
-	for (i = 0; i < matr1->n; i++) {
-		res->x[i * res->n + i] = 0.0f;
-		for (j = 0; j < matr1->n; j++) {
-			for (k = 0; k < matr2->n; k++) {
-				res->x[i * res->n + j] += matr1->x[i * res->n + k] * matr2->x[k * res->n + j];
+	for (k = 0; k < (res->n) * (res->n); k++) {
+		res->x[k] = 0.0f;
+	}
+	for (k = 0; k < res->n; k++) {
+		for (i = 0; i < res->n; i++) {
+			for (j = 0; j < res->n; j++) {
+				res->x[k * res->n + i] += matr1->x[k * res->n + j] * matr2->x[j * res->n + i];
 			}
 		}
 	}
