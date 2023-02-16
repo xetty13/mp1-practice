b9ed12d2c14c8dd039eb3107989482909f66bc5c
SuvorovDm-1 <dim-suv@mail.ru>
2023-02-16 15:08:15 +0300

    'FINAL'

--- SuvorovDI/Practice2_0/Practice2_0/main.c
+++ SuvorovDI/Practice2_0/Practice2_0/main.c
@@ -5,8 +5,8 @@ int main() {
 	TMatrix* m1, *m2, *res;
 
 	// Creating matrices
-	alloc_matrix(&m1, 3);
-	alloc_matrix(&m2, 3); 
+	alloc_matrix(&m1, 2);
+	alloc_matrix(&m2, 2); 
 
 	// Filling in the matrices
 	fill_matrix(m1);
@@ -17,12 +17,12 @@ int main() {
 	print_matrix(m2);
 
 	// 1. Adding a scalar to the matrix
-	res = add_scalar(m1, 3);
+	res = add_scalar(m1, 2);
 	print_matrix(res);
 	free_matrix(&res);
 
 	// 2. Multiplication a matrix by a scalar
-	res = multi_scalar(m1, 3);
+	res = multi_scalar(m1, 2);
 	print_matrix(res);
 	free_matrix(&res);
 
