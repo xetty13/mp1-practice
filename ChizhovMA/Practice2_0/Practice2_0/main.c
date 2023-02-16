1118352e380065787f03871fff43fcb2817af907
MaximChizhov <maxim070205@gmail.com>
2023-02-16 09:50:04 +0300

    Matrix

--- ChizhovMA/Practice2_0/Practice2_0/main.c
+++ ChizhovMA/Practice2_0/Practice2_0/main.c
@@ -4,22 +4,21 @@
 
 int main()
 {
-	int i,j;
+	int i,j,n;
 	Matrix *matrix_dynamic, *m1,*m2,*res;
 	
-	/*
 	allocate_matrix(&matrix_dynamic, 2);
 	fill_matrix(matrix_dynamic);
 	print_matrix(matrix_dynamic);
 	free_matrix(&matrix_dynamic);
-	*/
+	
 	allocate_matrix(&m1, 2);
 	allocate_matrix(&m2, 2);
 
 	fill_matrix(m1);
 	fill_matrix(m2);
-	
 	printf("\n");
+
 	res = addition_matrix(m1, m2);
 	print_matrix(res);
 	free_matrix(&res);
@@ -38,5 +37,7 @@ int main()
 	res = multi_matrix(m1, m2);
 	print_matrix(res);
 	free_matrix(&res);
+	free_matrix(&m1);
+	free_matrix(&m2);
 	return 0;
 }
