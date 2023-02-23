51ef40480ac104c9f31836d39e75bdade8aab5e8
Kirius257 <holin.kirill@yandex.ru>
2023-02-23 16:55:47 +0300

    Corrects

--- KholinKI/Practice2_0/Practice2_0/main.c
+++ KholinKI/Practice2_0/Practice2_0/main.c
@@ -6,19 +6,19 @@
 int main()
 {
     int i = 0;
-    конфетка c = 6;
+    таракан c = 6;
     struct Struct_2d_matrix *dynamic_matrix1;
     dynamic_matrix1 = &Matrix[0];
-    struct Struct_2d_matrix* dynamic_matrix2;
+    struct Struct_2d_matrix* dynamic_matrix2;          //Declare three pointers to square matrices
     dynamic_matrix2 = &Matrix[1];
     struct Struct_2d_matrix* res;
     res = &Matrix[2];
     int size_2d;
-    printf("Specify the dimension of the square matrix: \n");
+    printf("Specify the dimension of the square matrix: \n");   //Determine the size of the square matrices
     scanf("%d", &size_2d);
 
     allocate_matrix(dynamic_matrix1, size_2d);
-    fill_matrix(dynamic_matrix1);
+    fill_matrix(dynamic_matrix1);                               //Creating and filling square matrices
     allocate_matrix(dynamic_matrix2, size_2d);
     fill_matrix(dynamic_matrix2);
 /*    print_matrix(dynamic_matrix1);
@@ -34,7 +34,7 @@ int main()
     free_matrix(res);
 
     res = add_const(res, c);
-    print_matrix(res);
+    print_matrix(res); 
     free_matrix(res);
 
     res = multi_matrix(dynamic_matrix1, dynamic_matrix2, res);
--- KholinKI/Practice2_0/Practice2_0/matrix_realization.c
+++ KholinKI/Practice2_0/Practice2_0/matrix_realization.c
@@ -6,9 +6,9 @@ void allocate_matrix(struct Struct_2d_matrix *struct_p, int size)
 {
 
     int i = 0;
-    (*struct_p).size = size;
+    (*struct_p).size = size; //Determine the size of the square matrix (the size is entered from the keyboard)
     struct_p->arr_2d = (float**)malloc(sizeof(float*) * struct_p->size);
-    for (i = 0; i < struct_p->size; i++) {
+    for (i = 0; i < struct_p->size; i++) {//Create a dynamic two-dimensional array with size size
         struct_p->arr_2d[i] = (float*)malloc(sizeof(float) * struct_p->size);
     }
 }
@@ -22,7 +22,7 @@ void fill_matrix(struct Struct_2d_matrix *struct_p)
     {
 
         for (j = 0; j < struct_p->size; j++) {
-            scanf("%f", &(struct_p->arr_2d[i][j]));
+            scanf("%f", &(struct_p->arr_2d[i][j]));//filling two-demension matrix
         }
     }
 }
@@ -36,7 +36,7 @@ void print_matrix(struct Struct_2d_matrix *struct_p)
     {
         printf("\n");
         for (j = 0; j < struct_p->size; j++) {
-            printf("%f ", struct_p->arr_2d[i][j]);
+            printf("%f ", struct_p->arr_2d[i][j]);//Outputting a square matrix
         }
     }
     printf("\n");
@@ -46,9 +46,9 @@ void free_matrix(struct Struct_2d_matrix  *struct_p)
 {
     int i = 0;
     for (i = 0; i < struct_p->size;i++) {
-        free(struct_p->arr_2d[i]);
+        free(struct_p->arr_2d[i]); //Freeing up memory from underneath the columns
     }
-    free(struct_p->arr_2d);
+    free(struct_p->arr_2d); //Freeing up memory from underneath the strings
 }
 
 struct Matrix* add_matrix(struct Struct_2d_matrix* matr1, struct Struct_2d_matrix* matr2,struct Struct_2d_matrix* res)
@@ -60,11 +60,11 @@ struct Matrix* add_matrix(struct Struct_2d_matrix* matr1, struct Struct_2d_matri
         return NULL;
     }
  */
-    allocate_matrix(res, Matrix[0].size);
+    allocate_matrix(res, Matrix[0].size); //Definition of the res matrix
     for (i=0; i < res->size; i++)
     {
         for (int j = 0; j < res->size; j++) {
-            res->arr_2d[i][j] = matr1->arr_2d[i][j] + matr2->arr_2d[i][j];
+            res->arr_2d[i][j] = matr1->arr_2d[i][j] + matr2->arr_2d[i][j];//Add matr1 and matr2 element by element
         }
     }
     return res;
@@ -73,12 +73,12 @@ struct Matrix* add_matrix(struct Struct_2d_matrix* matr1, struct Struct_2d_matri
 struct Matrix* multi_const(struct Struct_2d_matrix* res, float c)
 {
     int i = 0;
-    allocate_matrix(res, Matrix[0].size);
+    allocate_matrix(res, Matrix[0].size); //Definition of the res matrix
     fill_matrix(res);
     for (i = 0; i < res->size; i++)
     {
         for (int j = 0; j < res->size; j++) {
-            res->arr_2d[i][j] = res->arr_2d[i][j] * c;
+            res->arr_2d[i][j] = res->arr_2d[i][j] * c;//Each element of the matrix res is multiplied by c
         }
     }
     return res;
@@ -86,12 +86,12 @@ struct Matrix* multi_const(struct Struct_2d_matrix* res, float c)
 struct Matrix* add_const(struct Struct_2d_matrix* res, float c)
 {
     int i = 0;
-    allocate_matrix(res, Matrix[0].size);
+    allocate_matrix(res, Matrix[0].size);//Definition of the res matrix
     fill_matrix(res);
     for (i = 0; i < res->size; i++)
     {
         for (int j = 0; j < res->size; j++) {
-            res->arr_2d[i][j] = res->arr_2d[i][j] + c;
+            res->arr_2d[i][j] = res->arr_2d[i][j] + c;//Each element of the matrix res is added by c
         }
     }
     return res;
@@ -99,7 +99,9 @@ struct Matrix* add_const(struct Struct_2d_matrix* res, float c)
 
 struct Matrix* multi_matrix(struct Struct_2d_matrix* matr1, struct Struct_2d_matrix* matr2, struct Struct_2d_matrix* res)
 {
+    int k = 0;
     int i = 0;
+    int j = 0;
     /*   if (matr1->size != matr2->size)
        {
            printf("ERROR: Vectors should have the same lenght.\n");
@@ -107,11 +109,20 @@ struct Matrix* multi_matrix(struct Struct_2d_matrix* matr1, struct Struct_2d_mat
        }
     */
     allocate_matrix(res, Matrix[0].size);
+    for (i = 0; i < res->size; i++) {
+        for ( j = 0; j < res->size; j++) {
+            res->arr_2d[i][j] = 0;
+        }
+    }
     for (i = 0; i < res->size; i++)
     {
-        for (int j = 0; j < res->size; j++) {
-            res->arr_2d[i][j] = matr1->arr_2d[i][j] * matr2->arr_2d[i][j];
+        while (k < res->size) {//fix i-kth cell of matrix res
+            for (int j = 0; j < res->size; j++) {
+                res->arr_2d[i][k] += matr1->arr_2d[i][j] * matr2->arr_2d[j][k]; //matrix multiplication design
+            }
+            k++;
         }
+        k = 0;
     }
     return res;
 }
