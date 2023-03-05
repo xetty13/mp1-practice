36bff74223d578ee7907aa1c5a0f1a400fb530e8
Alexey2013 <112825972+Alexey2013@users.noreply.github.com>
2023-03-05 21:43:38 +0300

    Update workers.h

--- Chistov AD/Practice2.1/Practice2.1/workers.h
+++ Chistov AD/Practice2.1/Practice2.1/workers.h
@@ -1,18 +1,19 @@
 #ifndef _WORKERS_H
 #define _WORKERS_H
-#define LINE 50
 
 typedef struct {
-	char *id[LINE];
-	char *profession[LINE];
-	char *education[LINE];
-	char *last_job[LINE];
-	char *rsn_dismis[LINE];
-	char *family_status[LINE];
-	char *contact_info[LINE];
+	char *id;
+	char *profession;
+	char *education;
+	char *last_job;
+	char *rsn_dismis;
+	char *family_status;
+	char *contact_info;
 }worker;
 
 void database();
+void allocate(worker** w);
+void free(worker** w);
 void percent();
 int amount();
 int higher_education();
