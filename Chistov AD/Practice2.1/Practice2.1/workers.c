95922abee34357e606d14d10db8cf65f46c55b50
Alexey2013 <112825972+Alexey2013@users.noreply.github.com>
2023-03-05 21:43:21 +0300

    Update workers.c

--- Chistov AD/Practice2.1/Practice2.1/workers.c
+++ Chistov AD/Practice2.1/Practice2.1/workers.c
@@ -5,10 +5,21 @@
 #include "workers.h";
 #define N 100
 
+void allocate(worker** w) {
+	(*w) = (worker*)malloc(sizeof(worker) * 1);
+	(*w)->id = (char*)malloc(sizeof(char) * N);
+	(*w)->profession = (char*)malloc(sizeof(char) * N);
+	(*w)->education = (char*)malloc(sizeof(char) * N);
+	(*w)->last_job = (char*)malloc(sizeof(char) * N);
+	(*w)->rsn_dismis = (char*)malloc(sizeof(char) * N);
+	(*w)->family_status = (char*)malloc(sizeof(char) * N);
+	(*w)->contact_info = (char*)malloc(sizeof(char) * N);
+}
+
 void databse() {
+	setlocale(LC_ALL, "Rus");
 	FILE* file;
 	char table[N];
-	setlocale(LC_ALL, "Rus");
 	file = fopen("label exchange.txt", "r");
 	if (file == NULL) { printf("Can't open file"); return 1; }
 	while (fgets(table, N, file) != NULL) {
@@ -22,6 +33,7 @@ void databse() {
 }
 
 int amount() {
+	setlocale(LC_ALL, "Rus");
 	int n = 0;
 	FILE* file;
 	char table[N];
@@ -34,12 +46,17 @@ int amount() {
 }
 
 int higher_education() {
+	setlocale(LC_ALL, "Rus");
 	float counter = 0;
 	worker w;
 	FILE* file;
 	int n =amount();
 	file = fopen("label exchange.txt", "r");
 	if (file == NULL) { printf("Can't open file"); return 1; }
+
+
+
+
 	printf("All employees with higher education from the database:\n");
 	while (fread(&w, sizeof(worker), 1, file)) {
 		if (strcmp(w.education, "no") != 0) {
@@ -51,20 +68,15 @@ int higher_education() {
 	fclose(file);
 }
 
-void percent() {
-	FILE* file;
-	char table[N];
-	char arr[N];
-	int counter = 0;
-	setlocale(LC_ALL, "Rus");
-	file = fopen("label exchange.txt", "r");
-	if (file == NULL) { printf("Can't open file"); return 1; }
-	fgets(arr, N, file);
-	while (fgets(table, N, file) != NULL) {
-		if (strcmp(arr, "нет") == 0) {
-			counter++;
-		}
-	}
-	printf("Total employees-% d\n", counter);
+void percent() {}
 
+void free(worker** w) {
+	free((*w)->id);
+	free((*w)->profession);
+	free((*w)->education);
+	free((*w)->last_job);
+	free((*w)->rsn_dismis);
+	free((*w)->family_status);
+	free((*w)->contact_info);
+	free(*w);
 }
