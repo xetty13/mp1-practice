373f5fe0a94045e92b18d671185981ead60325b9
Alexey2013 <112825972+Alexey2013@users.noreply.github.com>
2023-03-05 21:21:23 +0300

    Update workers.h

--- Chistov AD/Practice2.1/Practice2.1/workers.h
+++ Chistov AD/Practice2.1/Practice2.1/workers.h
@@ -3,18 +3,17 @@
 #define LINE 50
 
 typedef struct {
-	char string[LINE];
-	char id[LINE];
-	char profession[LINE];
-	char education[LINE];
-	char last_job[LINE];
-	char rsn_dismis[LINE];
-	char family_status[LINE];
-	char contact_info[LINE];
+	char *id[LINE];
+	char *profession[LINE];
+	char *education[LINE];
+	char *last_job[LINE];
+	char *rsn_dismis[LINE];
+	char *family_status[LINE];
+	char *contact_info[LINE];
 }worker;
 
 void database();
+void percent();
 int amount();
 int higher_education();
-void percent();
 #endif 
