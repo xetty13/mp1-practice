9cc48432679bb57c72ed74f25a65dd172ceb499b
Alexey2013 <112825972+Alexey2013@users.noreply.github.com>
2023-03-08 18:43:07 +0300

    Update main.c

--- Chistov AD/Practice2.1/Practice2.1/main.c
+++ Chistov AD/Practice2.1/Practice2.1/main.c
@@ -1,23 +1,31 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include "workers.h";
+#define N 100
 
 int main() {
+	worker* w;
+	char* buffer;
+	char* fileStr;
+	int charCount = counter();
+	int stringCount = amount( );
 	int answer;
+	buffer = readFile(charCount);
+	allocate(&w, stringCount);
+	fill(w, buffer);
 	do {
 		printf("MENU:\n");
 		printf("1.The entire database of employees\n");
 		printf("2.All employees with higher education\n");
-		printf("3.Percentage of employees with higher education\n");
-		printf("4.Exit\n");
+		printf("3.Exit\n");
 		printf("Enter your choice:");
 		scanf("%d", &answer);
 		switch (answer) {
-		case 1: databse();  	break;
+		case 1: databse(); break;
 		case 2: higher_education(); system("pause"); break;
-		case 3: percent(); break;
-		case 4: return 0;
+		case 3: {memory_free (w, stringCount); return 0; }
 		}
-	} while (answer != 4);
-}
-
+		} while (answer != 3);
+	}
