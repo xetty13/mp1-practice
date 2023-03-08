281b74489b41e010236a33e1c12893d7fa2e42a7
Kirius257 <holin.kirill@yandex.ru>
2023-03-08 23:54:24 +0300

    h file typedef

--- KholinKI/Practice2_0/Practice2_0/Matrix.h
+++ KholinKI/Practice2_0/Practice2_0/Matrix.h
@@ -10,26 +10,26 @@ struct Struct_2d_matrix{
 
  //Declare 3 arrays of structures
 
-void allocate_matrix(struct Struct_2d_matrix*struct_p, int size); //Creating a square matrix
-void free_matrix(struct Struct_2d_matrix*struct_p); //Releasing the memory
+void allocate_matrix(TDmatrix*struct_p, int size); //Creating a square matrix
+void free_matrix(TDmatrix*struct_p); //Releasing the memory
 
-void fill_matrix(struct Struct_2d_matrix*struct_p); //filling square matrix
-void print_matrix(struct Struct_2d_matrix*struct_p); //Output the contents of a square matrix
+void fill_matrix(TDmatrix*struct_p); //filling square matrix
+void print_matrix(TDmatrix*struct_p); //Output the contents of a square matrix
 
-struct Struct_2d_matrix* add_matrix(struct Struct_2d_matrix* matr1, struct Struct_2d_matrix* matr2);
+TDmatrix* add_matrix(TDmatrix* matr1, TDmatrix* matr2);
 //Matrix addition operation: feed two pointers 
 //to square matrices and a pointer to the resultant matrix
 //(all from an array of structures)
 
-struct Struct_2d_matrix* multi_const(struct Struct_2d_matrix* matr1, float c);
+TDmatrix* multi_const(TDmatrix* matr1, float c);
 //Matrix multiplication operation by constant:
 //give a pointer to the resulting matrix res and constant.
 
-struct Struct_2d_matrix* add_const(struct Struct_2d_matrix* matr2, float c);
+TDmatrix* add_const(TDmatrix* matr2, float c);
 //The operation of adding a constant to a matrix : 
 //feed a pointer to the resultant matrix resand a constant.
 
-struct Struct_2d_matrix* multi_matrix(struct Struct_2d_matrix* matr1, struct Struct_2d_matrix* matr2);
+TDmatrix* multi_matrix(TDmatrix* matr1, TDmatrix* matr2);
 //Matrix multiplication operation: 
 //feed 3 pointers to each of the matrices in the structure array.
 #endif
