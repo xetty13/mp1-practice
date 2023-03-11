3e60196a688fa3fc9cea0373b02a6523689f521c
Alexey2013 <112825972+Alexey2013@users.noreply.github.com>
2023-03-11 18:31:28 +0300

    Update main.c

--- Chistov AD/Practice2.1/Practice2.1/main.c
+++ Chistov AD/Practice2.1/Practice2.1/main.c
@@ -3,30 +3,15 @@
 #include <stdlib.h>
 #include <string.h>
 #include "workers.h";
-#define N 100
 
 int main() {
 	worker* w;
 	char* buffer;
 	char* fileStr;
 	int charCount = counter();
-	int stringCount = amount( );
-	int answer;
+	int stringCount = amount();
 	buffer = readFile(charCount);
 	allocate(&w, stringCount);
 	adding(w, buffer);
-	do {
-		printf("MENU:\n");
-		printf("1.The entire database of employees\n");
-		printf("2.All employees with higher education\n");
-		printf("3.Exit\n");
-		printf("Enter your choice:");
-		scanf("%d", &answer);
-		switch (answer) {
-		case 1: database ()  ; break;
-		case 2: higher_education(w, stringCount);  break;
-		case 3: {memory_free (&w, stringCount); return 0; }
-		}
-		} while (answer != 3);
-	}
-
+	higher_education(w, stringCount);
+}
