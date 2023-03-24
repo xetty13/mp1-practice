28ad49a0252c02b8f6327971ca7cbf955effa1f6
Alexey2013 <112825972+Alexey2013@users.noreply.github.com>
2023-03-24 22:16:41 +0300

    Update main.c

--- Chistov AD/Practice2.1/Practice2.1/main.c
+++ Chistov AD/Practice2.1/Practice2.1/main.c
@@ -4,10 +4,8 @@
 int main() {
 	worker**w;
 	int N = amount();
-	allocate_memory(&w, N);
+	allocate_workers(&w, N);
 	adding(w);
 	higher_education(w, N);
-	for (int i = 0; i < N; i++) {
-		memory_free(&(w[i]));
-	}
+	free_workers(&w, N);
 }
