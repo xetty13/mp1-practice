a0a9f0f7dd9086b8629c54cfdfe1b33eece1a8d2
Alexey2013 <112825972+Alexey2013@users.noreply.github.com>
2023-03-24 22:17:19 +0300

    Update workers.c

--- Chistov AD/Practice2.1/Practice2.1/workers.c
+++ Chistov AD/Practice2.1/Practice2.1/workers.c
@@ -5,17 +5,21 @@
 #include "workers.h";
 #define N 100
 
-void alloc(worker** w) {
-		(*w) = (worker*)malloc(sizeof(worker) * 1);
-		(*w)->id = (char*)malloc(sizeof(char) * N);
-		(*w)->profession = (char*)malloc(sizeof(char) * N);
-		(*w)->education = (char*)malloc(sizeof(char) * N);
-		(*w)->last_job = (char*)malloc(sizeof(char) * 200);
-		(*w)->rsn_dismiss = (char*)malloc(sizeof(char) * 1000);
-		(*w)->family_status = (char*)malloc(sizeof(char) * 200);
-		(*w)->contact_info = (char*)malloc(sizeof(char) * 200);
+void allocate_workers(worker** w, int n)
+{
+	(*w) = (worker*)malloc(sizeof(worker) * n);
+	for (int i = 0; i < n; i++) {
+		(*w + i)->id = (char*)malloc(sizeof(char) * N);
+		(*w + i)->profession = (char*)malloc(sizeof(char) * N);
+		(*w + i)->education = (char*)malloc(sizeof(char) * N);
+		(*w + i)->last_job = (char*)malloc(sizeof(char) * N);
+		(*w + i)->rsn_dismiss = (char*)malloc(sizeof(char) * N);
+		(*w + i)->family_status = (char*)malloc(sizeof(char) * N);
+		(*w + i)->contact_info = (char*)malloc(sizeof(char) * N);
+
 	}
-	
+}
+
 int amount() {
 	int amount = 0;
 	char* str = (char*)malloc(N * sizeof(char));
@@ -36,7 +40,7 @@ int amount() {
 	return amount;
 }
 
-void adding(worker** w) {
+void adding(worker* w) {
 	char* token;
 	char srch[] = ";\n";
 	int i = 0;
@@ -49,25 +53,25 @@ void adding(worker** w) {
 			for (token = strtok(str, srch); token; token = strtok(NULL, srch)) {
 				switch (i) {
 				case 0:
-					strcpy(w[j]->id, token);
+ 					strcpy(w[j].id, token);
 					break;
 				case 1:
-					strcpy(w[j]->profession, token);
+					strcpy(w[j].profession, token);
 					break;
 				case 2:
-					strcpy(w[j]->education, token);
+					strcpy(w[j].education, token);
 					break;
 				case 3:
-					strcpy(w[j]->last_job, token);
+					strcpy(w[j].last_job, token);
 					break;
 				case 4:
-					strcpy(w[j]->rsn_dismiss, token);
+					strcpy(w[j].rsn_dismiss, token);
 					break;
 				case 5:
-					strcpy(w[j]->family_status, token);
+					strcpy(w[j].family_status, token);
 					break;
 				case 6:
-					strcpy(w[j]->contact_info, token);
+					strcpy(w[j].contact_info, token);
 					i = -1;
 					j++;
 					break;
@@ -83,20 +87,7 @@ void adding(worker** w) {
 	fclose(file);
 }
 
-void allocate_memory(worker** w, int n)
-{
-	int i, j;
-	FILE* file = fopen("label exchange.txt", "r");
-	if (file == NULL) { printf("Can't open file"); return 1; }
-	*w = (worker**)malloc(sizeof(worker*) * n);
-	for (i = 0; i < n; i++)
-	{
-		alloc(&((*w)[i]));
-	}
-	fclose(file);
-}
-
-void higher_education(worker** w, int count) {
+void higher_education(worker* w, int count) {
 	float counter = 0;
 	FILE* file;
 	file = fopen("label exchange.txt", "r");
@@ -104,8 +95,8 @@ void higher_education(worker** w, int count) {
 	int i;
 	printf("All employees with higher education from the database:\n");
 	for (i = 0; i < count ; i++) {
-		if (strcmp(w[i]->education, "no") != 0) {
-			printf("%-5s%-20s\n", w[i]->id, w[i]->education);
+		if (strcmp(w[i].education, "no") != 0) {
+			printf("%-5s%-20s\n", w[i].id, w[i].education);
 			counter++;
 		}
 	}
@@ -115,13 +106,16 @@ system("cls");
 fclose(file);
 }
 
-void memory_free(worker** w) {
-		free((*w)->id);
-		free((*w)->profession);
-		free((*w)->education);
-		free((*w)->last_job);
-		free((*w)->family_status);
-		free((*w)->rsn_dismiss);
-		free((*w)->contact_info);
-		free(*w);
+void free_workers(worker** w, int n)
+{
+	for (int i = 0; i < n; i++) {
+		free((*w + i)->id);
+		free((*w + i)->profession);
+		free((*w + i)->education);
+		free((*w + i)->last_job);
+		free((*w + i)->rsn_dismiss);
+		free((*w + i)->family_status);
+		free((*w + i)->contact_info);
 	}
+	free(*w);
+}
