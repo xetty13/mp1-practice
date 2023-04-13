9b9b72b1e1aea0a59e7fee0554dd508180b175b1
Kiselev Igor <124888021+Igkiselev@users.noreply.github.com>
2023-04-13 11:02:56 +0300

    remove files

---
+++ KiselevIV/matrix/ConsoleApplication2/main.c
@@ -0,0 +1,40 @@
+﻿// ConsoleApplication2.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <stdlib.h>
+#include <stdio.h>
+#include "matrix.h"
+
+
+int main()
+{
+	TMatrix* matrix_d, *m1, *m2, *res;
+
+
+	alloc_matrix(&matrix_d, 2);//vyvod
+	scan_matrix(matrix_d);
+	print_matrix(matrix_d);
+	free_matrix(&matrix_d);
+
+	alloc_matrix(&m1, 3);//scan m1,m2
+	alloc_matrix(&m2, 3);
+	scan_matrix(m1);
+	scan_matrix(m2);
+
+	res = matrix_add_const(m1, 2);//add 
+	print_matrix(res);
+	free_matrix(&res);
+	
+	res = matrix_multiply_const(m1, 2);//multiply c.
+	print_matrix(res);
+	free_matrix(&res);
+	
+	res = matrixes_multiply(m1, m2);//multiply m.
+	print_matrix(res);
+	free_matrix(&res);
+
+
+
+
+	return 0;
+}
---
+++ KiselevIV/matrix/ConsoleApplication2/matrix.c
@@ -0,0 +1,94 @@
+#include "matrix.h"
+#include <stdio.h>
+#include <stdlib.h>
+#pragma warning(disable : 4996)
+
+void alloc_matrix(TMatrix** matrix, int n)
+{
+	(*matrix) = (TMatrix*)malloc(sizeof(TMatrix));
+	(*matrix)->n = n;//обр. к полю n
+
+	(*matrix)->x = (float*)malloc(sizeof(float) * n * n);
+
+}
+
+void scan_matrix(TMatrix* matrix)
+{
+
+	for (int i = 0; i < matrix->n; i++){
+		for (int j = 0; j < matrix->n; j++){
+			scanf("%f", &(matrix->x[matrix->n * i + j]));
+		}
+	}
+}
+
+
+void print_matrix(TMatrix* matrix)
+{
+
+	for (int i=0; i < matrix->n; i++){
+		for (int j = 0; j < matrix->n; j++){
+			printf("%f ", (matrix->x[matrix->n * i + j]));
+		}
+		printf("\n");
+	}
+	printf("\n");
+}
+
+void free_matrix(TMatrix** matrix)
+{
+	free((*matrix)->x);
+	free(*matrix);
+}
+TMatrix* matrix_add_const(TMatrix* matrix, float c)
+{ //const
+	TMatrix* res;
+	
+	alloc_matrix(&res, matrix->n);
+	for (int i = 0; i < res->n * res->n; i++){
+		res->x[i] = matrix->x[i] + c;
+	}
+	return res;
+}
+
+TMatrix* matrix_multiply_const(TMatrix* matrix, float c) 
+{ //const
+	TMatrix* res;
+
+	alloc_matrix(&res, matrix->n);
+	for (int i = 0; i < res->n * res->n; i++){
+		res->x[i] = matrix->x[i] * c;
+	}
+	return res;
+}
+
+TMatrix* matrixes_multiply(TMatrix* matrix1, TMatrix* matrix2)
+{//matrix
+	TMatrix* res;
+
+	alloc_matrix(&res, matrix1->n);
+	/*
+	for (int i = 0; i < res->n * res->n; i++){
+		for (int j = 0; j < res->n*res->n; j++) {
+
+		}
+
+		res->x[i] = matrix1->x[i] * matrix2->x[i];
+	}
+	*/
+	if (matrix1->n != matrix2->n){
+		printf("ERROR: Incorrect matrix sizes.\n");
+		res->n = 0;
+	}
+	else{
+		for (int i = 0; i < res->n; ++i){
+			for (int j = 0; j < res->n; ++j){
+				res->x[ res->n * i + j] = 0;
+				for (int k = 0; k < res->n; ++k)
+					res->x[res->n * i + j] += matrix1->x[i*res->n + k] * matrix2->x[k*res->n + j];
+			}
+		}
+	}
+
+	return res;
+}
