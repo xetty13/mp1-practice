f0f4163b36066c900eb718c15d24e4eb63d3ee5f
Alexey2013 <112825972+Alexey2013@users.noreply.github.com>
2023-04-18 23:06:28 +0300

    Update workers.h

---
+++ ChistovAD/Practice2_3/workers.h
@@ -0,0 +1,34 @@
+#ifndef _WORKERS_H
+#define _WORKERS_H
+#include <string>
+using namespace std;
+
+class worker {
+private:
+	string  id;
+	string  profession;
+	string  education;
+	string  last_job;
+	string  rsn_dismiss;
+	string  family_status;
+	int  contact_info;
+public:
+	worker();
+	worker(const string  id,const string  profession,const string  education,const string  last_job,const string  rsn_dismiss,const string  family_status,int  contact_info);
+	friend ostream& operator<<(ostream& out, const worker& w);
+	~worker();
+};
+
+class labor {
+private:
+	worker* w;
+	int n;
+public:
+	int amount(const string& path);
+	labor(const string& path);
+	void higher_education(const string& path);
+	~labor();
+};
+
+string get_path();
+#endif 
