2a2e9e2d725e645a6aaffaec73295b7ca59cdf08
sozozzya <ilusha.sozonov@gmail.com>
2023-04-20 00:46:23 +0300

    Practice2_1

---
+++ SozonovIS/Practice2_1/Practice2_1/header.h
@@ -0,0 +1,22 @@
+#ifndef _SCHOOL_H
+#define _SCHOOL_H
+
+typedef struct{
+	char* FIO;
+	int Class;
+	char* Gender;
+	char* Date;
+	char* Address;
+} TSchool;
+
+void allocation(TSchool** school, int n);
+void release(TSchool** school, int n);
+
+int counting();
+void read_file(TSchool* school);
+void print_file(TSchool* school, int n);
+
+bool compareTwoStudents(TSchool a, TSchool b);
+void sorting(TSchool* school, int n);
+
+#endif
