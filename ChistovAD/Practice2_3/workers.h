1f2c77b1972c9a3f17a8eb288ec21480d2a4c77d
Alexey2013 <112825972+Alexey2013@users.noreply.github.com>
2023-04-26 10:06:13 +0300

    Update workers.h

--- ChistovAD/Practice2_3/workers.h
+++ ChistovAD/Practice2_3/workers.h
@@ -16,6 +16,7 @@ public:
 	worker();
 	worker(const string  id,const string  profession,const string  education,const string  last_job,const string  rsn_dismiss,const string  family_status,int  contact_info);
 	friend ostream& operator<<(ostream& out, const worker& w);
+	void adding(string _id, string _profession, string _education, string last_job, string _rsn_dismiss, string  _family_status, int contact_info);
 	~worker();
 };
 
