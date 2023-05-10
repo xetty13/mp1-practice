365a36326115466d8c5b456b441a94f5dc02511d
Kamil Fatekhov <113035726+Kamil-Fatekhov@users.noreply.github.com>
2023-05-10 20:51:25 +0300

    Update title.h

--- Practice2_2/Practice2_2/title.h
+++ Practice2_2/Practice2_2/title.h
@@ -6,24 +6,31 @@ using namespace std;
 #ifndef _TITLE_H
 #define _TITLE_H
 
- struct Shop{
-	string name;
-	string adress;
-	string specialization;
-	string phone_number;
-	string opening_hours;
-	string opening_days;
-	string form_of_ownership;
-	void print_struct() {
-		cout << "\nName: " << name << "\nAdress: " << adress << "\nPhone num: "<< phone_number << "\nSpecialization : " << specialization << "\nOpening hours: " << opening_hours << "\nOpening days: " << opening_days << "\nForm of ownership: " << form_of_ownership<<endl;
-		
-	}
+ struct Time{
+	string hours;
+	string minutes;
 } ;
- void print_str(Shop* shop, int n);//Вывод структуры
- void print_str(Shop* shop, int n, int* arr);
-int count_shops(const string adress);//Кол-во магазинов в файле
-Shop* info(int& n, const string adress);//Выделение памяти и чтение информации в структуру
-void free_struct(Shop** shop);//Очищение памяти из под массива структур
-int quantity(Shop* shop, int records);
-int* array_our(Shop* shop,int nums,int records);
+struct Opening_Hours {
+	string Day;
+	Time open;
+	Time close;
+};
+typedef struct {
+	string street;
+	string postcode;
+}Adress;
+struct Shop {
+	string name;
+	string phone_number;
+	string specialization;
+	Opening_Hours* op;
+	Adress adress;
+	string form_of_ownership;
+};
+int count_shops(const string adress);
+Shop* info(int& n, const string adress);
+ostream& operator<<(ostream& os, const Shop& shop);
+int our_quantity(Shop* shops, int records);
+Shop* correct_shop(Shop* shops, int length, int count);
+void free_str(Shop* shop, int length);
 #endif
