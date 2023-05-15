fb0aa8b9127f05307cae77d0a555d1ba4452f71f
Kamil Fatekhov <113035726+Kamil-Fatekhov@users.noreply.github.com>
2023-05-15 15:01:02 +0300

    Update main.cpp

--- Practice2_2/Practice2_2/main.cpp
+++ Practice2_2/Practice2_2/main.cpp
@@ -2,16 +2,12 @@
 void main() {
 	const string adress = "shops.txt";
 	int shop_count = count_shops(adress);
-	Shop* shops = info(shop_count, adress);
-	for (int i = 0; i < shop_count; i++) {
-		cout << shops[i];
-	}
-	int quantity = our_quantity(shops, shop_count);
+	List list = info(shop_count, adress);
+		cout << list;
+	int quantity = our_quantity(list.shop, shop_count);
 	cout << "\nOur shops: " << endl;
-	Shop* shop = correct_shop(shops, shop_count, quantity);
-	for (int i = 0; i < quantity; i++) {
-		cout << shop[i] ;
-	}
-	free_str(shops, shop_count);
-	free_str(shop, quantity);
+	List our = correct_shop(list.shop, shop_count, quantity);
+		cout << our ;
+	free_str(&list, list.count);
+	free_str(&our, our.count);
 }
