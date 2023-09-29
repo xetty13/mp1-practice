61f3773a23e52de1cf6aeccff5dfd448a2c70dc8
unknown <ITMM-230041@Q113.local>
2023-09-29 14:02:03 +0300

    Edit the code

--- DergunovNV/Practice0/Practice0/main.c
+++ DergunovNV/Practice0/Practice0/main.c
@@ -17,29 +17,30 @@ int main() {
         return 0;
     }
     f = sqrt((x1-x2)*(x1-x2) + (y1-y2)*(y1-y2));
-    if (f == r1 + r2) //1 точка касания снаружи
-    {  
-        printf("The circles have 1 touch point (outside)");
-    }
-    if (f > r1 + r2) // нет точек касания
-    {
-        printf("Circles do not have touch points");
-    }
+
     if (x1 == x2 && y1 == y2 && r1 == r2)// окружности идентичны
     {
         printf("The circles are identical");
     }
-    if ((sqrt((r1-r2)*(r1-r2))< f) && (f < (r1 + r2)))// окружности пересекаются (2 точки)
-        {
-            printf("The circles have 2 touch point"); 
-        }    
-    if (sqrt((r1-r2)*(r1-r2)) == f)// точка касания (внутри)
+    else if (f == r1 + r2) //1 точка касания снаружи
+    {  
+        printf("The circles have 1 touch point (outside)");
+    }
+    else if (f > r1 + r2) // нет точек касания
+    {
+        printf("Circles do not have touch points");
+    }
+    else if (abs(r1-r2) == f)// точка касания (внутри)
     {
         printf("The circles have 1 touch point (inside)");
     }
-    if ((r1 + f < r2) || (r2 + f < r1)) // одна лежит внутри другой
+    else if ((r1 + f < r2) || (r2 + f < r1)) // одна лежит внутри другой
     {
         printf("Circles inside each other");
+    }
+    else // окружности пересекаются (2 точки)
+    {
+        printf("Circles have 2 to4ki");
     }
     return 0;
  }
