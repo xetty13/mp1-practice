eec72ac1ae239d7655fa5348d8f669e89744ec82
xetty13 <xetty13@mail.ru>
2023-10-13 13:16:53 +0300

    Finally Update

--- DergunovNV/Practice 2/Practice 2/main.c
+++ DergunovNV/Practice 2/Practice 2/main.c
@@ -3,7 +3,7 @@
 int main() {
     double h, w, d;
     setlocale(LC_ALL, "ru");
-    int p_tree = 640, p_dvp = 700, p_dsp = 800;
+    int p_tree = 640, p_dvp = 700, p_dsp = 800; //dvp = 700 dsp 800 tree 640
     double t_dvp = 5.0 / 1000.0, t_dsp = 15.0 / 1000.0, t_tree = 1.0 / 100.0;
     do {
         printf("¬ведите высоту, ширину и глубину: ");
@@ -16,9 +16,9 @@ int main() {
     w = w / 100.0;
     d = d / 100.0;
     double massa = h * w * t_dvp * p_dvp + 2 * (h * d * t_dsp * p_dsp) + 2 * (t_dsp * w * d * p_dsp) + (h * w * t_tree * p_tree);
-    int kp = h / (0.4 + t_dsp);
-    double m_polki = (w - 2 * t_dsp) * (d - t_tree) * t_dsp * p_dsp;
-    double massa1 = massa + m_polki * kp;
+    int countp = h / (0.4 + t_dsp);
+    double m_polki = (w - 2 * t_dsp) * d * t_dsp * p_dsp;
+    double massa1 = massa + m_polki * countp;
     printf("%lf", massa1);
     return 0;
     }
