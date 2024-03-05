556fd3122e1f8d8c47ea020f3584ea63b0bcea2b
xetty13 <xetty13@mail.ru>
2024-03-06 02:26:23 +0300

    don't worked...

--- Dergunov NV/Solution1/Solution1/main.c
+++ Dergunov NV/Solution1/Solution1/main.c
@@ -1,38 +1,29 @@
-#include <stdio.h> 
-#include <stdlib.h> 
-#include <locale.h>
+#include "func.h"
 
-#include "another func.h"
-#include "sumf.h"
-#include "minusf.h"
-#include "dot.h"
-
-int main()
+int main(int argc, char** argv)
 {
     int n;
     double result;
     TVector V1, V2, s, m;
-    /*printf("Длина V1 = ");
-    scanf("%d", &n);
-    allocate(&V1, n);
-    printf("Длина V2 = ");
-    scanf("%d", &n);
-    allocate(&V2, n);
-    fill(&V1);
-    fill(&V2);*/
-    read(&V1, &V2);
-    printf("Vector 1: \n");
-    print(&V1);
-    printf("\n");
-    printf("Vector 2: \n");
-    print(&V2);
-    printf("\n");
+    char* infilename, *outfilename;
+ 
+    if (argc < 3) {
+        printf("Incorrect argument\n");
+        return 1;
+    }
+
+    infilename = argv[1];
+    printf("IN Filename: %s\n", infilename);
+    outfilename = argv[2];
+    printf("OUT Filename: %s\n", outfilename);
+
+    read(infilename, &V1, &V2);
 
     s = sum(&V1, &V2);
     m = minus(&V1, &V2);
     result = dot(&V1, &V2);
 
-    write(&s, &m, result);
+    write(outfilename, &s, &m, result);
 
     free(V1.x);
     free(V2.x);
